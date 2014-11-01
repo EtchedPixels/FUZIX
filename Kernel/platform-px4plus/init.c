282bc5307e688bc99ef055fb7337ecc3aef19ae7
Alan Cox <alan@etchedpixels.co.uk>
2014-11-01 15:45:18 +0000

    px4 init: Remember to set the video base !

--- Kernel/platform-px4plus/init.c
+++ Kernel/platform-px4plus/init.c
@@ -8,10 +8,13 @@
 #include <devlpr.h>
 #include <devtty.h>
 
+__sfr __at 0x08 vadr;
+
 void device_init(void)
 {
   int i;
   /* Add 4 swaps (128K) to use the entire RAM drive */
   for (i = 0; i < MAX_SWAPS; i++)
     swapmap_add(i);
+  vadr = 0x58;	/* Must match kernel.def */  
 }
