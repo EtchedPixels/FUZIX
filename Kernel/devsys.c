85651b11555ce4f6762db6bf5829100d2e885c7b
Alan Cox <alan@etchedpixels.co.uk>
2014-11-21 15:01:59 +0000

    devsys: note a Z80 assumption to fix

--- Kernel/devsys.c
+++ Kernel/devsys.c
@@ -52,6 +52,7 @@ int sys_write(uint8_t minor, uint8_t rawflag, uint8_t flag)
   case 2:
     return udata.u_count;
   case 1:
+    /* FIXME: this needs to be a per CPU value */
     if (udata.u_offset < 0x0100) {
       udata.u_error = EPERM;
       return -1;
