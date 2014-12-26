c872bf3e822dbfc35d703deebf2843a4ef7fa834
Alan Cox <alan@linux.intel.com>
2014-12-26 20:11:09 +0000

    syscall_name: add argument counts to the tables
    We will need these to automate creation of some types of call stubs

--- Kernel/include/syscall_name.h
+++ Kernel/include/syscall_name.h
@@ -62,3 +62,66 @@ char *syscall_name[NR_SYSCALL] = {
 	"nice",
 	"_sigdisp",
 };
+
+int syscall_args[NR_SYSCALL] = {
+	1, //_exit
+	3, //open
+	1, //close
+	2, //rename
+	3, //mknod
+	2, //link
+	1, //unlink
+	3, //read
+	3, //write
+	3, //_lseek
+	1, //chdir
+	0, //sync
+	2, //access
+	2, //chmod
+	3, //chown
+	2, //_stat
+	2, //_fstat
+	1, //dup
+	0, //getpid
+	0, //getppid
+	0, //getuid
+	1, //umask
+	2, //_getfsys
+	3, //execve
+	2, //_getdirent
+	1, //setuid
+	1, //setgid
+	2, //_time
+	1, //stime
+	3, //ioctl
+	1, //brk
+	1, //sbrk
+	0, //fork
+	2, //mount
+	1, //umount
+	3, //signal
+	2, //dup2
+	1, //_pause
+	1, //alarm
+	2, //kill
+	1, //pipe
+	0, //getgid
+	1, //times
+	2, //utime
+	0, //geteuid
+	0, //getegid
+	1, //chroot
+	3, //fcntl
+	1, //fchdir
+	2, //fchmod
+	3, //fchown
+	2, //mkdir
+	1, //rmdir
+	0, //setpgrp
+	2, //_uname
+	3, //waitpid
+	4, //_profil
+	3, //uadmin
+	1, //nice
+	2  //_sigdisp
+};
