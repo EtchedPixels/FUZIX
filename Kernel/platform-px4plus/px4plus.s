c3215b3eb5bf2fbaf9a9a7f68f51829ed227ff70
Alan Cox <alan@linux.intel.com>
2014-12-27 15:39:16 +0000

    timer: remove more dead references

--- Kernel/platform-px4plus/px4plus.s
+++ Kernel/platform-px4plus/px4plus.s
@@ -16,7 +16,6 @@
             .globl init_early
             .globl init_hardware
             .globl _program_vectors
-            .globl _system_tick_counter
 
 	    .globl map_kernel
 	    .globl map_process
