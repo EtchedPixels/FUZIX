9c745212af19a4a490acc1663fd6b9b22b35f113
Alan Cox <alan@linux.intel.com>
2014-12-29 23:33:31 +0000

    utils: from minixspace this time
    Note: sed gives sdcc the shits. Build send with 100 not 20000 or you'll be
    waiting all week

---
+++ Applications/util/tee.c
@@ -0,0 +1,96 @@
+/*
+  Copyright (c) 1987,1997, Prentice Hall
+  All rights reserved.
+  
+  Redistribution and use of the MINIX operating system in source and
+  binary forms, with or without modification, are permitted provided
+  that the following conditions are met:
+  
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+  
+     * Redistributions in binary form must reproduce the above
+       copyright notice, this list of conditions and the following
+       disclaimer in the documentation and/or other materials provided
+       with the distribution.
+  
+     * Neither the name of Prentice Hall nor the names of the software
+       authors or contributors may be used to endorse or promote
+       products derived from this software without specific prior
+       written permission.
+  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS, AUTHORS, AND
+  CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+  IN NO EVENT SHALL PRENTICE HALL OR ANY AUTHORS OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/  
+/* tee - pipe fitting			Author: Paul Polderman */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define	MAXFD	18
+#define CHUNK_SIZE	4096
+
+int fd[MAXFD];
+
+int main(int argc, char *argv[])
+{
+  char iflag = 0, aflag = 0;
+  char buf[CHUNK_SIZE];
+  int i, s, n;
+
+  argv++;
+  --argc;
+  while (argc > 0 && argv[0][0] == '-') {
+	switch (argv[0][1]) {
+	    case 'i':		/* Interrupt turned off. */
+		iflag++;
+		break;
+	    case 'a':		/* Append to outputfile(s), instead of
+			 * overwriting them. */
+		aflag++;
+		break;
+	    default:
+		fprintf(stderr,"Usage: tee [-i] [-a] [files].\n");
+		exit(1);
+	}
+	argv++;
+	--argc;
+  }
+  fd[0] = 1;			/* Always output to stdout. */
+  for (s = 1; s < MAXFD && argc > 0; --argc, argv++, s++) {
+	if (aflag && (fd[s] = open(*argv, O_RDWR)) >= 0) {
+		lseek(fd[s], 0L, SEEK_END);
+		continue;
+	} else {
+		if ((fd[s] = creat(*argv, 0666)) >= 0) continue;
+	}
+	fprintf(stderr,"Cannot open output file: ");
+	fprintf(stderr,*argv);
+	fprintf(stderr,"\n");
+	exit(2);
+  }
+
+  if (iflag) signal(SIGINT, SIG_IGN);
+
+  while ((n = read(0, buf, CHUNK_SIZE)) > 0) {
+	for (i = 0; i < s; i++) write(fd[i], buf, n);
+  }
+
+  for (i = 0; i < s; i++)	/* Close all fd's */
+	close(fd[i]);
+  return(0);
+}
