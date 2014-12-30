fe54cedff45fd93f53e8a1bebcbd056014696125
Alan Cox <alan@linux.intel.com>
2014-12-30 11:09:59 +0000

    utils: Add logname

---
+++ Applications/util/logname.c
@@ -0,0 +1,18 @@
+/* See README.ajr / COPYING */
+
+#include <unistd.h>
+#include <string.h>
+#include <pwd.h>
+#include <sys/types.h>
+
+void main(int argc, char *argv[])
+{
+	struct passwd * upw = getpwuid(getuid());
+
+	if (upw) {
+		write(STDOUT_FILENO,upw->pw_name,strlen(upw->pw_name));
+		write(STDOUT_FILENO,"\n",1);
+		exit (0);
+	}
+	exit (1);
+}
