ad8bb114fb69b32fe4cbab0c367dcb8e573a7858
Alan Cox <alan@linux.intel.com>
2015-01-02 22:28:08 +0000

    err.h: include stdarg

---
+++ Library/include/err.h
@@ -0,0 +1,17 @@
+#ifndef _ERR_H
+#define _ERR_H
+
+#include <stdarg.h>
+/* BSD err functionality */
+
+extern void err(int eval, const char *fmt, ...);
+extern void errx(int eval, const char *fmt, ...);
+extern void warn(const char *fmt, ...);
+extern void warnx(const char *fmt, ...);
+
+extern void verr(int eval, const char *fmt, va_list args);
+extern void verrx(int eval, const char *fmt, va_list args);
+extern void vwarn(const char *fmt, va_list args);
+extern void vwarnx(const char *fmt, va_list args);
+
+#endif
