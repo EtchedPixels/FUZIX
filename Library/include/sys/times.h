a486bb6ce6d62bf10d380db40f852fdb9f30bebb
Alan Cox <alan@linux.intel.com>
2015-01-02 10:09:18 +0000

    times: correct returned type to be clock_t
    From: Sergio L. Pascual <https://github.com/slp/FUZIX>

--- Library/include/sys/times.h
+++ Library/include/sys/times.h
@@ -3,11 +3,11 @@
 
 /* User's structure for times() system call */
 struct tms {
-	time_t	tms_utime;
-	time_t	tms_stime;
-	time_t	tms_cutime;
-	time_t	tms_cstime;
-	time_t	tms_etime;	/* Elapsed real time */
+	clock_t	tms_utime;
+	clock_t	tms_stime;
+	clock_t	tms_cutime;
+	clock_t	tms_cstime;
+	clock_t	tms_etime;	/* Elapsed real time */
 };
 
 extern int times(struct tms *tms);
