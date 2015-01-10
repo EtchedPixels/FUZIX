95d9d1c870133bbe5c9839b0a38947861cdf7dab
Alan Cox <alan@linux.intel.com>
2015-01-10 00:40:04 +0000

    6502: stubs for usermem

--- Kernel/usermem_std-6502.s
+++ Kernel/usermem_std-6502.s
@@ -1,6 +1,17 @@
 	.include "platform/kernel.def"
         .include "kernel02.def"
 
+	.export __uget, __ugetc, __ugetw, __ugets
+	.export __uput, __uputc, __uputw, __uzero
 ;
-;	Uses the C helpers
+;	TO DO: Possibly easier to write them in C with just the _c helpers
+;	but for speed would be best the block ones are asm.
 ;
+__uget:		rts
+__ugetc:	rts
+__ugetw:	rts
+__ugets:	rts
+__uput:		rts
+__uputc:	rts
+__uputw:	rts
+__uzero:	rts
