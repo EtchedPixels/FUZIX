25de9c9996547af38dcb1d7955a838f15f4267aa
Will Sowerbutts <will@sowerbutts.com>
2015-01-10 01:11:37 +0000

    Revert "Kernel: Update n8vem-mark4, p112 platform device tables with new flush functions"
    This reverts commit 7f11d30050416cf836a964844b5465b85087c6eb.

--- Kernel/platform-p112/devices.c
+++ Kernel/platform-p112/devices.c
@@ -10,12 +10,12 @@
 
 struct devsw dev_tab[] =  /* The device driver switch table */
 {
-/*   open	    close	read		write		ioctl	    flush */
-  {  blkdev_open,   no_close,	blkdev_read,	blkdev_write,	no_ioctl,   blkdev_flush },	/* 0: /dev/hd -- standard block device interface */
-  {  no_open,	    no_close,	no_rdwr,	no_rdwr,	no_ioctl,   no_flush },		/* 1: unused slot */
-  {  tty_open,	    tty_close,	tty_read,	tty_write,	tty_ioctl,  no_flush },		/* 2: /dev/tty -- serial ports */
-  {  no_open,	    no_close,	no_rdwr,	no_rdwr,	no_ioctl,   no_flush },		/* 3: unused slot */
-  {  no_open,	    no_close,	sys_read,	sys_write,	sys_ioctl,  no_flush },		/* 4: /dev/mem etc	System devices (one offs) */
+/*   open	    close	read		write		ioctl */
+  {  blkdev_open,   no_close,	blkdev_read,	blkdev_write,	no_ioctl },	/* 0: /dev/hd -- standard block device interface */
+  {  no_open,	    no_close,	no_rdwr,	no_rdwr,	no_ioctl },	/* 1: unused slot */
+  {  tty_open,	    tty_close,	tty_read,	tty_write,	tty_ioctl },	/* 2: /dev/tty -- serial ports */
+  {  no_open,	    no_close,	no_rdwr,	no_rdwr,	no_ioctl },	/* 3: unused slot */
+  {  no_open,	    no_close,	sys_read,	sys_write,	sys_ioctl  },	/* 4: /dev/mem etc	System devices (one offs) */
 };
 
 bool validdev(uint16_t dev)
