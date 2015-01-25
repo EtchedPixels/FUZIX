8887a9209c52134195c6680642d3dda2797d5716
Alan Cox <alan@linux.intel.com>
2015-01-25 20:28:10 +0000

    zx128: fix devtty header
    Lots more needed to start getting this port on the road usefully

--- Kernel/platform-zx128/devtty.h
+++ Kernel/platform-zx128/devtty.h
@@ -5,9 +5,9 @@ void tty_pollirq(void);
 static void keydecode(void);
 
 #define KEY_ROWS	8
-#define KEY_COLS	8
+#define KEY_COLS	5
 extern uint8_t keymap[8];
-extern uint8_t keyboard[8][8];
-extern uint8_t shiftkeyboard[8][8];
+extern uint8_t keyboard[8][5];
+extern uint8_t shiftkeyboard[8][5];
 
 #endif
