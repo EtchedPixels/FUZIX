3162c6814748a3d2e5b4f5ba8f518a413f9d4d22
Alan Cox <alan@linux.intel.com>
2015-01-30 00:23:05 +0000

    devsys: fix types
    This doesn't matter for 16bit but will shortly

--- Kernel/include/devsys.h
+++ Kernel/include/devsys.h
@@ -1,3 +1,3 @@
 extern int sys_read(uint8_t minor, uint8_t rawflag, uint8_t flag);
 extern int sys_write(uint8_t minor, uint8_t rawflag, uint8_t flag);
-extern int sys_ioctl(uint8_t minor, uint16_t request, char *data);
+extern int sys_ioctl(uint8_t minor, uarg_t request, char *data);
