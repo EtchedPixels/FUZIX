7cee511157acca6c1f0e3095c51f5c0b6042f4af
Alan Cox <alan@linux.intel.com>
2015-02-12 01:48:26 +0000

    zx128: update to match changes in blkdev and devide

--- Kernel/platform-zx128/devices.c
+++ Kernel/platform-zx128/devices.c
@@ -6,23 +6,28 @@
 #include <vt.h>
 #include <devmdv.h>
 #include <devide.h>
+#include <blkdev.h>
 
 struct devsw dev_tab[] =  /* The device driver switch table */
 {
+#ifdef CONFIG_MDV
   /* 0: /dev/fd		Floppy disc block devices, or microdrive etc */
   {  mdv_open,     mdv_close,    mdv_read,   mdv_write,   no_ioctl },
+#else
+  {  no_open,      no_close,     no_rdwr,   no_rdwr,   no_ioctl },
+#endif
 #ifdef CONFIG_IDE
   /* 1: /dev/hd		Hard disc block devices */
-  {  devide_open,  no_close, devide_read,   devide_write,   no_ioctl },
+  {  blkdev_open,  no_close,     blkdev_read,   blkdev_write,   blkdev_ioctl },
 #else
-  {  no_open,  no_close, no_rdwr,   no_rdwr,   no_ioctl },
+  {  no_open,      no_close,     no_rdwr,       no_rdwr,        no_ioctl },
 #endif
   /* 2: /dev/tty	TTY devices */
-  {  tty_open,     tty_close,   tty_read,  tty_write,  vt_ioctl },
+  {  tty_open,	   tty_close,    tty_read,      tty_write,     vt_ioctl },
   /* 3: /dev/lpr	Printer devices */
-  {  no_open,     no_close,   no_rdwr,   no_rdwr,  no_ioctl  },
+  {  no_open,      no_close,     no_rdwr,       no_rdwr,       no_ioctl  },
   /* 4: /dev/mem etc	System devices (one offs) */
-  {  no_open,      no_close,    sys_read, sys_write, sys_ioctl  },
+  {  no_open,      no_close,     sys_read,      sys_write,     sys_ioctl  },
   /* Pack to 7 with nxio if adding private devices and start at 8 */
 };
 
