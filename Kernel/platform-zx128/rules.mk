fcfd98a65fd8418cd7dd45198cd5ef39828a3105
Alan Cox <alan@linux.intel.com>
2015-02-12 22:12:10 +0000

    zx128: rules.mk for banked

--- Kernel/platform-zx128/rules.mk
+++ Kernel/platform-zx128/rules.mk
@@ -1,1 +1,11 @@
+#
+#	ZX128 uses banked kernel images
+#
 export CPU = z80
+CROSS_CCOPTS += --external-banker
+#
+# Tell the core code we are using the banked helpers
+#
+export BANKED=-banked
+#
+export CROSS_CC_SEG3=--codeseg CODE3
