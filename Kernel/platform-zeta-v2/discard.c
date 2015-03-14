0a63518e55a1a9b8d6e7aec6fd0f973c3b8c84ef
Sergey Kiselev <skiselev@gmail.com>
2015-03-13 23:18:49 -0700

    zeta-v2: implement UART auto detection; enable FIFO on UARTs that support it; move device initialization code to _DISCARD

--- Kernel/platform-zeta-v2/discard.c
+++ Kernel/platform-zeta-v2/discard.c
@@ -2,25 +2,32 @@
 #include <kdata.h>
 #include <printf.h>
 #include <devtty.h>
+#include <ds1302.h>
 #include "config.h"
 
 void pagemap_init(void)
 {
-    int i;
+	int i;
 
-    /* ZETA SBC V2 has RAM in the top 512 KiB of physical memory
-     * corresponding pages are 32-63 (page size is 16 KiB)
-     * Pages 32-34 are used by the kernel
-     * Page 35 is the common area
-     * Pages starting from DEV_RD_START are used by RAM disk
-     */
-    for (i = 32 + 4; i < DEV_RD_START; i++)
-        pagemap_add(i);
+	/* ZETA SBC V2 has RAM in the top 512 KiB of physical memory
+	 * corresponding pages are 32-63 (page size is 16 KiB)
+	 * Pages 32-34 are used by the kernel
+	 * Page 35 is the common area
+	 * Pages starting from DEV_RD_START are used by RAM disk
+	 */
+	for (i = 32 + 4; i < DEV_RD_START; i++)
+		pagemap_add(i);
 
-    /* finally add the common area */
-    pagemap_add(32 + 3);
+	/* finally add the common area */
+	pagemap_add(32 + 3);
 }
 
 void map_init(void)
 {
+}
+
+void device_init(void)
+{
+	ds1302_init();
+	uart0_init();
 }
