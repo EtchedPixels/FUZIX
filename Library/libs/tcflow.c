3af5f3b288df39452b3498c99ea4e3ed520d4cde
Faisal Abbas <90.abbasfaisal@gmail.com>
2015-03-15 13:05:26 +0000

    tcflow.c: tcflow implementation

--- Library/libs/tcflow.c
+++ Library/libs/tcflow.c
@@ -4,7 +4,24 @@
 
 int tcflow(int fd, int action)
 {
-	/* TODO */
-	errno = EINVAL;
-	return -1;
+	struct termios term;
+	cc_t c;
+
+	switch (action) {
+		case TCOOFF:
+			return ioctl(fd, TIOCOSTOP, 0);
+		case TCOON:
+			return ioctl(fd, TIOCOSTART, 0);
+		case TCIOFF:
+		case TCION:
+			if (tcgetattr(fd, &term) == -1)
+				return -1;
+			c = term.c_cc[action == TCIOFF ? VSTOP : VSTART];
+			if (c != _POSIX_VDISABLE && (write(fd, &c, sizeof(c))) == -1)
+				return -1;
+			return 0;
+		default:
+			errno = EINVAL;
+			return -1;
+	}
 }
