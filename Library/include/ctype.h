45838a23478e1d2e82564952269f4f1add8b8a74
David Given <dg@cowlark.com>
2015-03-19 23:00:06 +0100

    Add some missing prototypes; ensure that the functions are only inlined on supported compilers (i.e. not cc65).

--- Library/include/ctype.h
+++ Library/include/ctype.h
@@ -77,11 +77,12 @@ extern int iscntrl(int c);
 extern int isdigit(int c);
 extern int isgraph(int c);
 extern int islower(int c);
-extern int isoctal(int c);
 extern int ispunct(int c);
 extern int isspace(int c);
 extern int isupper(int c);
 extern int isxdigit(int c);
+extern int tolower(int c);
+extern int toupper(int c);
 
 #define isdecimal(c) isdigit(c)
 
