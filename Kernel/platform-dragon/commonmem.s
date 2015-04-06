2479cfb828a60a226d4a46e48a79f6341d25c8d3
Tormod Volden <debian.tormod@gmail.com>
2015-04-06 11:32:51 +0200

    dragon: Remove misplaced comment in commonmem.s
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

--- Kernel/platform-dragon/commonmem.s
+++ Kernel/platform-dragon/commonmem.s
@@ -1,8 +1,4 @@
 ;
-;	Put the udata at the start of common. We have four 16K banks so we
-; keep the non .common kernel elements below C000 and then keep bank 3 as a
-; true common bank
-;
         .module commonmem
 
         ; exported symbols
