ebf1150ab629a20276138ff0effd844873c4b1df
Will Sowerbutts <will@sowerbutts.com>
2015-05-10 15:07:02 +0100

    zeta-v2: Fix boot from kernel in ROM. Do not change tty1 UART parameters unless booting from ROM.

--- Kernel/platform-zeta-v2/config.h
+++ Kernel/platform-zeta-v2/config.h
@@ -110,7 +110,7 @@
 
 	/* UART0 as the console */
 	#define BOOT_TTY (512 + 1)
-	#define TTY_INIT_BAUD B115200
+	#define TTY_INIT_BAUD B38400
 #endif
 
 #define TTYDEV   BOOT_TTY /* Device used by kernel for messages, panics */
