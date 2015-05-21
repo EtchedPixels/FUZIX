d3ddb9098d2f4e3dd37543e72cb131101847d6d6
Alan Cox <alan@linux.intel.com>
2015-05-21 23:36:22 +0100

    mv: note removal of stdio

--- Applications/util/mv.c
+++ Applications/util/mv.c
@@ -3,7 +3,7 @@
  * Permission is granted to use, distribute, or modify this source,
  * provided that this copyright notice remains intact.
  *
- * Most simple built-in commands are here.
+ * stdio stripped out Alan Cox 2015
  */
 
 #include <stdio.h>
