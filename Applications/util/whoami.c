452933e8cb905185993056f8708a6e3f5330981e
Alan Cox <alan@linux.intel.com>
2015-05-21 23:38:33 +0100

    whoami: remove stdio

--- Applications/util/whoami.c
+++ Applications/util/whoami.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <pwd.h>
 
@@ -10,7 +11,8 @@ int main(int argc, const char *argv[])
     uid = geteuid();
     pw = getpwuid(uid);
     if (pw) {
-	printf("%s\n", pw->pw_name);
+	write(1, pw->pw_name, strlen(pw->pw_name));
+	write(1, "\n", 1);
 	return 0;
     }
 
