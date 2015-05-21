28c3eea1ee164a3d7a1c19cc047221529310c656
Alan Cox <alan@linux.intel.com>
2015-05-21 23:37:12 +0100

    pwd: remove stdio

--- Applications/util/pwd.c
+++ Applications/util/pwd.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(int argc, const char *argv[])
 {
     char buf[512];
     
-    if (getcwd(buf, 512) == NULL) {
-        fprintf(stderr, "pwd: cannot get current directory\n");
-        return -1;
+    if (getcwd(buf, sizeof(buf)) == NULL) {
+        write(2, "pwd: cannot get current directory\n", 34);
+        return 1;
     }
-    printf("%s\n", buf);
+    write(1, buf, strlen(buf));
+    write(1, "\n", 1);
     return 0;
 }
