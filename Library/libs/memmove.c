b44cb3d818ccf2576188233e5be78a025c3ac5c2
Tormod Volden <debian.tormod@gmail.com>
2015-05-22 22:20:16 +0200

    Fix memmove breakage
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

--- Library/libs/memmove.c
+++ Library/libs/memmove.c
@@ -1,9 +1,11 @@
 /* From dLibs 1.20 but ANSIfied */
 
-void *memmove(void *dest, const void *source, size_t len)
+#include <types.h>
+
+void *memmove(void *dest, const void *src, size_t len)
 {
 	uint8_t *dp = dest;
-	uint8_t *sp = src;
+	const uint8_t *sp = src;
 	
 	if (sp < dp) {
 		dp += len;
