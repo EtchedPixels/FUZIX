b6adf3ac59b1536c82ba29029631d657150353c9
Tormod Volden <debian.tormod@gmail.com>
2015-05-23 20:07:30 +0200

    6809: Implement setjmp/longjmp
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

---
+++ Library/libs/setjmp_6809.s
@@ -0,0 +1,34 @@
+; setjmp / longjmp for 6809 FUZIX
+; Copyright 2015 Tormod Volden
+
+	; exported
+	.globl _setjmp
+	.globl _longjmp
+
+	.area .text
+
+; int setjmp(jmp_buf)
+_setjmp:
+	ldd ,s		; return address
+	sty ,x++
+	stu ,x++
+	sts ,x++
+	std ,x
+	ldx #0
+	rts
+
+; void longjmp(jmp_buf, int)
+_longjmp:
+	; read back Y,U,S and return address
+	ldd 2,s		; second argument
+	bne @nz		; must not be 0
+	incb
+@nz	ldy ,x++
+	ldu ,x++
+	lds ,x++	; points to clobbered return address
+	ldx ,x
+	stx ,s		; restore return address
+	; return given argument to setjmp caller
+	tfr d,x
+	rts
+
