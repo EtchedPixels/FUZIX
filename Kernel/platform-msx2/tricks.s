c88bce9076e914f38fb53d32154a3ba1672db41f
Alan Cox <alan@linux.intel.com>
2015-05-30 19:36:15 +0100

    msx2: correct for new style IRQ handling
    Untested - needs the Makefiles fixing for the fact bootrom.bin has no
    rule to create it

--- Kernel/platform-msx2/tricks.s
+++ Kernel/platform-msx2/tricks.s
@@ -8,7 +8,6 @@
         .globl _getproc
         .globl _trap_monitor
         .globl trap_illegal
-        .globl _inint
         .globl _switchout
         .globl _switchin
         .globl _doexec
@@ -51,10 +50,6 @@ _switchout:
         push ix
         push iy
         ld (U_DATA__U_SP), sp ; this is where the SP is restored in _switchin
-
-        ; set inint to false
-        xor a
-        ld (_inint), a
 
         ; find another process to run (may select this one again)
         call _getproc
@@ -113,9 +108,9 @@ _switchin:
         pop hl ; return code
 
         ; enable interrupts, if the ISR isn't already running
-        ld a, (_inint)
+        ld a, (U_DATA__U_ININTERRUPT)
         or a
-        ret z ; in ISR, leave interrupts off
+        ret nz ; in ISR, leave interrupts off
         ei
         ret ; return with interrupts on
 
