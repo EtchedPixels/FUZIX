bc1105048d48012bfe7d52b41c9391c6d4110766
Alan Cox <alan@linux.intel.com>
2015-06-04 19:45:16 +0100

    dragon-nx-32: enable IDE probing

--- Kernel/platform-dragon-nx32/devices.c
+++ Kernel/platform-dragon-nx32/devices.c
@@ -9,15 +9,17 @@
 #include <vt.h>
 #include <graphics.h>
 #include <devtty.h>
+#include <blkdev.h>
+#include <devide.h>
 
 struct devsw dev_tab[] =  /* The device driver switch table */
 {
 // minor    open         close        read      write       ioctl
 // -----------------------------------------------------------------
-  /* 0: /dev/fd		Floppy disc block devices  */
+  /* 0: /dev/hd		Hard disc block devices (SD and IDE) */
+  {  blkdev_open,  no_close,	blkdev_read,	blkdev_write,	blkdev_ioctl },
+  /* 1: /dev/fd		Floppy disc block devices  */
   {  fd_open,     no_close,    fd_read,   fd_write,   no_ioctl },
-  /* 1: /dev/hd		Hard disc block devices (absent) */
-  {  nxio_open,     no_close,    no_rdwr,   no_rdwr,   no_ioctl },
   /* 2: /dev/tty	TTY devices */
   {  tty_open,     tty_close,   tty_read,  tty_write,  gfx_ioctl },
   /* 3: /dev/lpr	Printer devices */
@@ -41,7 +43,8 @@ bool validdev(uint16_t dev)
     else
         return true;
 }
+
 void device_init(void)
 {
+    devide_init();
 }
-
