4a6aba04e187b045102b2de9deeb08fd02da25bb
Alan Cox <alan@linux.intel.com>
2015-06-04 23:02:55 +0100

    dragon-nx32: report cartridge hashes and MPI (needs xroar fixes)
    This is the start of an small experiment to see if we can autodetect some
    cartridges by spotting their ROMs.

--- Kernel/platform-dragon-nx32/main.c
+++ Kernel/platform-dragon-nx32/main.c
@@ -2,10 +2,14 @@
 #include <timer.h>
 #include <kdata.h>
 #include <printf.h>
+#include <device.h>
 #include <devtty.h>
 
 uint8_t membanks;
 uint8_t system_id;
+uint8_t cartslots = 1;
+uint16_t carthash[4];
+uint8_t bootslot = 0;
 
 void platform_idle(void)
 {
@@ -14,6 +18,9 @@ void platform_idle(void)
 void do_beep(void)
 {
 }
+
+
+/* ------------- Below bits can all move to discard if needed ------------ */
 
 /*
  * Map handling: We have flexible paging. Each map table consists of a set of pages
@@ -33,6 +40,24 @@ void pagemap_init(void)
 #endif
 }
 
+static const char *sysname[] = {"Dragon", "COCO", "COCO3", "Unknown"};
+
 void map_init(void)
 {
+	uint8_t i;
+
+	kprintf("%s system.\n", sysname[system_id]);
+	if (mpi_present()) {
+		kputs("MPI cartridge detected.\n");
+		cartslots = 4;
+		bootslot = mpi_set_slot(0);
+	}
+	for (i = 0; i < cartslots; i++) {
+		mpi_set_slot((i << 4) | i);
+		carthash[i] = cart_hash();
+		kprintf("%d: %x %c\n",
+			i, carthash[i],
+			i == bootslot ? '*':' ');
+	}
+	mpi_set_slot(bootslot);
 }
