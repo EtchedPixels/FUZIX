b9a20463deb461d4a18d6ada101aa4b8c4065c39
David Given <dg@cowlark.com>
2015-06-27 13:42:30 +0200

    User processes start running; they can call back to the kernel via the syscall interface.

--- Kernel/lowlevel-msp430x.c
+++ Kernel/lowlevel-msp430x.c
@@ -1,6 +1,7 @@
 #include <kernel.h>
 #include <kdata.h>
 #include <printf.h>
+#include <stdarg.h>
 #include "iomacros.h"
 #include "intrinsics.h"
 
@@ -11,7 +12,40 @@ __interrupt void interrupt_handler(void)
 
 void doexec(uaddr_t start_addr)
 {
-	kprintf("exec %lx\n", start_addr);
+	di();
+	udata.u_insys = 0;
+
+	asm volatile(
+		"mov %0, sp\n"
+		"eint\n"
+		"br %1\n"
+		:
+		: "g" (udata.u_isp),
+		  "g" (start_addr));
+}
+
+__attribute__ ((naked)) void unix_syscall_entry(void)
+{
+	asm volatile (
+		"mov.b r11, %0\n"
+		"mov r12, %1\n"
+		"mov r13, %2\n"
+		"mov r14, %3\n"
+		"mov r15, %4\n"
+		"jmp unix_syscall_main\n"
+		: "=m" (udata.u_callno),
+		  "=m" (udata.u_argn),
+		  "=m" (udata.u_argn1),
+		  "=m" (udata.u_argn2),
+		  "=m" (udata.u_argn3)
+		:
+	);
+}
+
+void unix_syscall_main(void)
+{
+	kprintf("syscall %d(%x, %x, %x, %x)!\n", udata.u_callno,
+		udata.u_argn, udata.u_argn1, udata.u_argn2, udata.u_argn3);
 	for (;;);
 }
 
