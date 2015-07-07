960582d309cd2f974c93934d4a33dddcf04a4ca8
David Given <dg@cowlark.com>
2015-07-07 21:11:22 +0200

    Fix longjmp and setjmp to actually work.

--- Library/libs/longjmp_msp430.s
+++ Library/libs/longjmp_msp430.s
@@ -3,17 +3,17 @@
 	.type longjmp, @function
 	.func longjmp
 longjmp:
-	mov	r15, r13 ; param 0: address of jmp_buf
-	mov	r14, r15 ; param 1; return value
-	mov	@r13+, r1
-	mov	@r13+, r2
-	mov	@r13+, r4
-	mov	@r13+, r5
-	mov	@r13+, r6
-	mov	@r13+, r7
-	mov	@r13+, r8
-	mov	@r13+, r9
-	mov	@r13+, r10
-	mov	@r13+, r11
-	br @r13
+	mov	r12, r14 ; param 0: address of jmp_buf
+	mov	r13, r12 ; param 1; return value
+	mov	@r14+, r1
+	mov	@r14+, r2
+	mov	@r14+, r4
+	mov	@r14+, r5
+	mov	@r14+, r6
+	mov	@r14+, r7
+	mov	@r14+, r8
+	mov	@r14+, r9
+	mov	@r14+, r10
+	mov	@r14+, r11
+	br @r14
 .endfunc
