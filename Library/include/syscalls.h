442fd50a41a293a133aa165b102ca4ae7eb2570c
Alan Cox <alan@linux.intel.com>
2015-10-02 13:17:08 +0100

    fork(): flag day time - add flags, pointer argument as _fork and wrap it
    Ready for doing faster/cleverer forky things

--- Library/include/syscalls.h
+++ Library/include/syscalls.h
@@ -117,7 +117,7 @@ extern int setgid(gid_t gid);
 extern int ioctl(int fd, int request,...);
 extern int brk(void *addr);
 extern void *sbrk(intptr_t increment);
-extern pid_t fork(void);
+extern pid_t _fork(uint16_t flags, void *addr);
 extern int mount(const char *dev, const char *path, int flags);
 extern int umount(const char *dev);
 extern sighandler_t signal(int signum, sighandler_t sighandler);
