bac30d42fb2b9c5852e26213be778c70f7d11e47
Alan Cox <alan@linux.intel.com>
2015-10-09 16:18:17 +0100

    zx128: platform_ide.h

--- Kernel/platform-zx128/config.h
+++ Kernel/platform-zx128/config.h
@@ -1,16 +1,4 @@
-/* Simple IDE interface */
 #define CONFIG_IDE
-#define IDE_REG_DATA		0xA3
-#define IDE_REG_ERROR		0xA7
-#define IDE_REG_FEATURES	0xA7
-#define IDE_REG_SEC_COUNT	0xAB
-#define IDE_REG_LBA_0		0xAF
-#define IDE_REG_LBA_1		0xB3
-#define IDE_REG_LBA_2		0xB7
-#define IDE_REG_LBA_3		0xBB
-#define IDE_REG_DEVHEAD		0xBB
-#define IDE_REG_STATUS		0xBF
-#define IDE_REG_COMMAND		0xBF
 
 /* Enable to make ^Z dump the inode table for debug */
 #undef CONFIG_IDUMP
