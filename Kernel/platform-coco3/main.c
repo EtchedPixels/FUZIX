e967405e3d6c3b37e8051316b5a6fe179c7c2a69
Brett Gordon <beretta42@gmail.com>
2015-10-22 02:24:08 -0400

    coco3: discard: import of more dragon code

--- Kernel/platform-coco3/main.c
+++ Kernel/platform-coco3/main.c
@@ -5,6 +5,26 @@
 #include <devtty.h>
 
 
+struct blkbuf *bufpool_end = bufpool + NBUFS;
+
+void platform_discard(void)
+{
+	extern uint8_t discard_size;
+	bufptr bp = bufpool_end;
+
+	kprintf("%d buffers reclaimed from discard\n", discard_size);
+	
+	bufpool_end += discard_size;
+
+	memset( bp, 0, discard_size * sizeof(struct blkbuf) );
+
+	for( bp = bufpool + NBUFS; bp < bufpool_end; ++bp ){
+		bp->bf_dev = NO_DEVICE;
+		bp->bf_busy = BF_FREE;
+	}
+}
+
+
 void platform_idle(void)
 {
 }
