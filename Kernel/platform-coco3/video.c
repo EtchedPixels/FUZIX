4cb0f23499ffc75dada845d9fac5b456408af9d9
Brett Gordon <beretta42@gmail.com>
2015-11-01 08:40:38 -0500

    coco3: vt: bug fix: use wider counter for line clearing.

--- Kernel/platform-coco3/video.c
+++ Kernel/platform-coco3/video.c
@@ -61,11 +61,11 @@ void plot_char(int8_t y, int8_t x, uint16_t c)
 
 void clear_lines(int8_t y, int8_t ct)
 {
+	uint16_t wc= ct * VT_WIDTH;
 	map_for_video();
 	uint16_t *s = (uint16_t *)char_addr(y, 0);
-	ct *= VT_WIDTH;
 	uint16_t w = ' ' * 0x100 + curpty->attr;
-	for( ; ct ; ct-- )
+	for( ; wc ; wc-- )
 		*s++=w;
 	map_for_kernel();
 }
