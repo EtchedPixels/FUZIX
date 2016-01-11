8dfe4ec4a6adac6708221256ffcc8b82a5aa988d
Alan Cox <alan@linux.intel.com>
2016-01-11 19:07:12 +0000

    net_at: header for silly stack

---
+++ Kernel/include/net_at.h
@@ -0,0 +1,10 @@
+/* Callbacks */
+extern void netat_hangup(void);
+extern void netat_event(void);
+/* Platform provided */
+extern void netat_nowake(void);
+extern void netat_wake(void);
+extern uint8_t netat_byte(void);
+extern void netat_poll(void);
+extern void netat_outbyte(uint8_t);
+extern uint8_t netat_ready(void);
