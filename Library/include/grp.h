86858a473caa49f0c0eb0a0b4c43b98453db7c82
Alan Cox <alan@linux.intel.com>
2016-01-16 15:26:16 +0000

    grp.h: setgroups is a syscall it shouldn't be here
    Fixes the Dragon NX build failure. Reported by Tony Jewell

--- Library/include/grp.h
+++ Library/include/grp.h
@@ -25,7 +25,6 @@ extern struct group *getgrnam __P((const char * name));
 
 extern struct group * fgetgrent __P((FILE * file));
 
-extern int setgroups __P((size_t n, int *groups));
 extern int initgroups __P((const char * user, gid_t gid));
 
 extern struct group * __getgrent __P((int grp_fd));
