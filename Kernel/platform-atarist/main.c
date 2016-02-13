624139822e194dd62da7f233f8aa3615222d60dc
Alan Cox <alan@linux.intel.com>
2016-02-13 21:17:40 +0000

    flat_mem: start adjusting atarist as a prototype user of flat_mem

--- Kernel/platform-atarist/main.c
+++ Kernel/platform-atarist/main.c
@@ -19,21 +19,48 @@ void do_beep(void)
 
 void pagemap_init(void)
 {
+	vmmu_init();
 }
 
 void map_init(void)
 {
 }
 
-unsigned char vt_mangle_6847(unsigned char c)
-{
-	if (c >= 96)
-		c -= 32;
-	c &= 0x3F;
-	return c;
-}
-
 u_block uarea_block[PTABSIZE];
 uaddr_t ramtop;
 
 void *screenbase;
+uint8_t *membase = 0x10000;	/* GUESS FIXME */
+uint8_t *memtop;
+
+/*
+ *	We can do our fork handling in C for once. The only oddity here is
+ *	the fixups to run parent first and avoid needless memory thrashing
+ */
+void dofork(ptptr p)
+{
+	/* Child and parent udata pointers */
+	u_block *uc = uarea_block + p - ptab;
+	u_block *up = udata_ptr;
+	uint32_t *csp = uc + 1;
+	uint32_t *psp = up->u_sp;
+	/* Duplicate the memory maps */
+	if (pagemap_fork(p))
+		return -1;
+	/* Duplicate the udata */
+	memcpy(&uc->u_d, &up->u_d, sizeof(struct u_data))
+	/* Use the child udata for initializing the child */
+	udata_ptr = uc;
+	newproc(p);
+	udata_ptr = up;
+	/* And return as the parent. The child will return via the
+	   fork return path */
+	*--csp = fork_return;
+	uc->u_sp = csp;
+	/* Copy the saved register state over - must match switchin */
+	memcpy(csp - 14, psp - 14, 4 * 14);
+	/* Return as the parent and run it first (backwards to most ports) */
+	p->p_status = P_READY;
+	udata.u_ptab->p_status = P_RUNNING;
+	return p->p_pid;
+}
