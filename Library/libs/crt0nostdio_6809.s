8a44a71f3d7094d0ccc632096edbe6a83bb0c51a
Tormod Volden <debian.tormod@gmail.com>
2016-05-28 17:32:47 +0200

    Library: Add stdio-free crt0 for 6809
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

---
+++ Library/libs/crt0nostdio_6809.s
@@ -0,0 +1,40 @@
+		.module crt0
+
+		.globl _main
+		.globl _exit
+		.globl _environ
+		.globl ___argv
+
+		.area .header
+
+start:		jmp start2
+		.db 'F'
+		.db 'Z'
+		.db 'X'
+		.db '1'
+		.db __sectionbase_.header__/256	; page to load at
+		.dw 0				; chmem ("0 - 'all'")
+		.dw __sectionlen_.text__	; gives us code size info
+		.dw __sectionlen_.data__	; gives us data size info
+		.dw __sectionlen_.bss__		; bss size info
+		.dw 0			; spare
+
+		.area .text
+
+start2:
+		; we don't clear BSS since the kernel already did
+
+		; pass environ, argc and argv to main
+		; pointers and data stuffed above stack by execve()
+		leax 4,s
+		stx _environ
+		leax 2,s
+		stx ___argv
+		puls x			; argc
+		ldy #_exit		; return vector
+		pshs y
+		jmp _main		; go
+
+		.area .data
+
+_environ:	.dw 0
