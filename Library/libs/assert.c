ed07bd48b03c02ede2e8e381231b08dfda451c64
Alan Cox <alan@linux.intel.com>
2016-06-17 23:34:20 +0100

    libc: remove \r\n markers from assert.c and tidy

--- Library/libs/assert.c
+++ Library/libs/assert.c
@@ -1,18 +1,19 @@
 /* Copyright (C) 1996 Robert de Bath <rdebath@cix.compulink.co.uk>
  * This file is part of the Linux-8086 C library and is distributed
  * under the GNU Library General Public License.
- */  
+ */
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
 
-void __errput(char *str) 
+void __errput(const char *str)
 {
 	write(2, str, strlen(str));
 }
 
-void __assert(char *assertion, char *filename, int linenumber) 
+void __assert(const char *assertion, const char *filename,
+	      const int linenumber)
 {
 	__errput("Failed '");
 	__errput(assertion);
@@ -22,4 +23,4 @@ void __assert(char *assertion, char *filename, int linenumber)
 	__errput(_itoa(linenumber));
 	__errput(".\n");
 	abort();
-} 
+}
