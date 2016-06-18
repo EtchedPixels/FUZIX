8a9dce7c1a87d2d94616e6d16abca5fc3eb24ebc
Neal Andrew Crook <neal@pippaluk.org.uk>
2016-06-18 23:18:47 +0100

    Tweaks to allow library to build on 6809

--- Library/libs/tzset.c
+++ Library/libs/tzset.c
@@ -1,5 +1,5 @@
-/*************************** TZSET ************************************/  
-    
+/*************************** TZSET ************************************/
+
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
@@ -8,22 +8,22 @@ char *tzname[2] = { "GMT", "\0\0\0" };
 
 int daylight;
 long timezone;
- 
+
 /* tzset expects fo find a environment string of the form TZ=...
  * ??? need to correct!
- */ 
-void tzset(VOID)
+ */
+void tzset(void)
 {
-	char *tz = getenv("TZ");
+        char *tz = getenv("TZ");
 
-	if (tz == NULL) {
-		memcpy(tzname[1], "GMT", 3);
-		timezone = 0 * 60 * 60L;	/* London */
-	} else {
-		int v;
-		
-		memcpy(tzname[1], tz, 3);
-		v = atoi(tz + 3);
-		timezone = -((v / 100) * 60 + (v % 100)) * 60L;
-	}
-} 
+        if (tz == NULL) {
+                memcpy(tzname[1], "GMT", 3);
+                timezone = 0 * 60 * 60L;        /* London */
+        } else {
+                int v;
+
+                memcpy(tzname[1], tz, 3);
+                v = atoi(tz + 3);
+                timezone = -((v / 100) * 60 + (v % 100)) * 60L;
+        }
+}
