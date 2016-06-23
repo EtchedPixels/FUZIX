750eb2123c11725a5f8f8753dbf41d2bff16bf64
Brett Gordon <beretta42@gmail.com>
2016-06-23 13:47:31 -0400

    dwtime: add header. remove defunct drivewire.h (in Kernel/include now)

---
+++ Kernel/platform-coco3/dwtime.h
@@ -0,0 +1,7 @@
+/* Defines for the dwtime RTC module */
+
+/* call to initialize dwtime subsystem
+   Warning: this funtion is in the discard section, so please call it
+   before freeing .discard
+*/
+int dwtime_init( void );
