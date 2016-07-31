d049176e8cdb8e8285b5fffa8b6b9d0239184ffb
Alexander Tsidaev <a.tsidaev@gmail.com>
2016-07-31 23:11:12 +0500

    zx128: adding Disciple lowlevel functions prototypes and libz80 reference

---
+++ Kernel/platform-zx128/disciple.h
@@ -0,0 +1,8 @@
+#ifndef __DISCIPLE_DOT_H__
+#define __DISCIPLE_DOT_H__
+
+/* low level interface */
+uint16_t fd_reset(uint8_t *driveptr);
+uint16_t fd_operation(uint8_t *driveptr);
+
+#endif /* __DISCIPLE_DOT_H__ */
