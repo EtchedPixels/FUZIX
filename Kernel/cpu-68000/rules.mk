360feed6f12b0fb40446d681e379dc620b1f3ad6
Alan Cox <alan@linux.intel.com>
2016-10-20 23:12:45 +0100

    68000: switch to gcc 6.2.0

--- Kernel/cpu-68000/rules.mk
+++ Kernel/cpu-68000/rules.mk
@@ -1,5 +1,5 @@
-export CROSS_LD=m68k-linux-gnu-ld
-export CROSS_CC = m68k-linux-gnu-gcc
+export CROSS_LD=m68k-uclinux-ld
+export CROSS_CC = m68k-uclinux-gcc
 # Do not use the Fedora gcc 5.3.1. It miscompiles stuff badly.
 export CROSS_CCOPTS=-c -Os -fno-strict-aliasing -fomit-frame-pointer -fno-builtin -Wall -m68000 -I$(ROOT_DIR)/cpu-68000 -I$(ROOT_DIR)/platform-$(TARGET) -I$(ROOT_DIR)/include
 export CROSS_AS=$(CROSS_CC) $(CROSS_CCOPTS) #-Wa,-M
