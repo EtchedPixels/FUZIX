abc63619a34a432b4f62a0daa5b812beb9dcd565
Alan Cox <alan@linux.intel.com>
2016-11-05 12:23:40 +0000

    coco2cart: COCO2 with cartridge
    While fiddling with COCO2 might as well clone it and add the cartridge version
    I made the notes for ages ago.
    
    Untested and will need some work.

---
+++ Kernel/platform-coco2cart/config.h
@@ -0,0 +1,61 @@
+/* Enable to make ^Z dump the inode table for debug */
+#undef CONFIG_IDUMP
+/* Enable to make ^A drop back into the monitor */
+#undef CONFIG_MONITOR
+/* Profil syscall support (not yet complete) */
+#undef CONFIG_PROFIL
+/* Multiple processes in memory at once */
+#undef CONFIG_MULTI
+/* Single tasking - for now while we get it booting */
+#undef CONFIG_SINGLETASK
+/* Pure swap */
+#define CONFIG_SWAP_ONLY
+#define CONFIG_SPLIT_UDATA
+#define UDATA_BLKS 1
+
+#define CONFIG_BANKS	1
+/* And swapping */
+#define SWAPDEV     0x0		/* Uses part of IDE slice 0 */
+#define SWAP_SIZE   0x40	/* 32K in 512 byte blocks */
+#define SWAPBASE    0x8000	/* We swap the lot, including stashed uarea */
+#define SWAPTOP     0xFE00	/* so it's a round number of 512 byte sectors */
+#define UDATA_SIZE  0x0200	/* one block */
+#define MAX_SWAPS   32
+
+/* Permit large I/O requests to bypass cache and go direct to userspace */
+#define CONFIG_LARGE_IO_DIRECT
+
+/* Video terminal, not a serial tty */
+#define CONFIG_VT
+#define CONFIG_FONT8X8
+#define CONFIG_FONT8X8SMALL
+
+extern unsigned char vt_mangle_6847(unsigned char c);
+#define VT_MAP_CHAR(x)	vt_mangle_6847(x)
+
+/* Vt definitions */
+#define VT_WIDTH	32
+#define VT_HEIGHT	24
+#define VT_RIGHT	31
+#define VT_BOTTOM	23
+#define VT_INITIAL_LINE	0
+
+#define VIDEO_BASE	0x0200
+
+#define TICKSPERSEC 50   /* Ticks per second */
+#define PROGBASE    0x8000  /* also data base */
+#define PROGLOAD    0x8000  /* also data base */
+#define PROGTOP     0xF000  /* Top of program */
+
+/* We need a tidier way to do this from the loader */
+#define CMDLINE	NULL	  /* Location of root dev name */
+
+/* Device parameters */
+#define NUM_DEV_TTY 2
+#define NDEVS    2        /* Devices 0..NDEVS-1 are capable of being mounted */
+                          /*  (add new mountable devices to beginning area.) */
+#define TTYDEV   513	 /* Device used by kernel for messages, panics */
+#define NBUFS    6       /* Number of block buffers */
+#define NMOUNTS	 2	  /* Number of mounts at a time */
+#define swap_map(x)	((uint8_t *)(x))
+
---
+++ Kernel/platform-coco2cart/main.c
@@ -0,0 +1,51 @@
+#include <kernel.h>
+#include <timer.h>
+#include <kdata.h>
+#include <printf.h>
+#include <device.h>
+#include <devtty.h>
+#include <carts.h>
+#include <blkdev.h>
+
+uint8_t membanks;
+uint8_t system_id;
+uint8_t cartslots = 1;
+uint8_t carttype[4];
+uint8_t bootslot = 0;
+uint16_t swap_dev;
+
+void platform_idle(void)
+{
+}
+
+uint8_t platform_param(char *p)
+{
+    return 0;
+}
+
+void do_beep(void)
+{
+}
+
+void platform_discard(void)
+{
+}
+
+/* Find a cartridge or it's slot */
+int cart_find(int id)
+{
+	int i;
+	for (i = 0; i < id; i++) {
+		if (carttype[i] == id)
+			return i;
+	}
+	return -1;
+}
+
+unsigned char vt_mangle_6847(unsigned char c)
+{
+	if (c >= 96)
+		c -= 32;
+	c &= 0x3F;
+	return c;
+}
