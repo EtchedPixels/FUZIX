e883a108db3610d1c7e8da5149f52fefe4325051
Alan Cox <alan@linux.intel.com>
2016-11-19 11:42:53 +0000

    execl: 6502 support

--- Library/libs/execl.c
+++ Library/libs/execl.c
@@ -2,23 +2,23 @@
  *
  * function(s)
  *	  execl - load and run a program
- */  
-    
+ */
+
 #include <unistd.h>
 #include <paths.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
-    
+
 /* Find file in pathes:
  * 1. /name or ./name or ../name is already qualified names
  * 2. else search in all pathes described in env var PATH (if this
  *    var is not exist, _PATH_DEFPATH is used)
  * 3. else search in current directory
  * 4. else return NULL (execve() interpretes NULL as non existent file!)
- */ 
-const char *_findPath(const char *path) 
+ */
+const char *_findPath(const char *path)
 {
 	char *p;
 	const char *envp;
@@ -26,11 +26,11 @@ const char *_findPath(const char *path)
 	if (*path == '/' || /* qualified name */ *path == '.')
 		return path;
 
-	/* search for pathes list */ 
+	/* search for pathes list */
 	if ((envp = getenv("PATH")) == NULL)
 		envp = _PATH_DEFPATH;
 
-	/* lookup all pathes */ 
+	/* lookup all pathes */
 	while (*envp) {
 		p = name;
 		while (*envp && (*p = *envp++) != ':') {
@@ -55,27 +55,65 @@ const char *_findPath(const char *path)
 #ifndef __CC65__
 
 /* FIXME: The 6502 calling sequence means these need a different implementation */
-int execl(const char *pathP, const char *arg0, ...) 
+int execl(const char *pathP, const char *arg0, ...)
 {
 	return execve(pathP, &arg0, environ);
 }
 
-int execlp(const char *pathP, const char *arg0, ...) 
+int execlp(const char *pathP, const char *arg0, ...)
 {
 	return execve(_findPath(pathP), &arg0, environ);
 }
 #else
-int execl(const char *pathP, const char *arg0, ...) 
+
+/* This gets to be overexciting with the backward argument handling */
+#include <stdarg.h>
+
+int execl(const char *pathP, const char *arg0, ...)
 {
-	/* BUG: this should be ENOSYS, but Fuzix doesn't have that yet */
-	errno = EINVAL;
+	va_list ptr;
+	const char *arg[32];
+	const char **p = arg;
+
+	va_start(ptr, arg0);
+
+	*p++ = arg0;
+
+	while (p != &arg[32]) {
+		*p = va_arg(ptr, const char *);
+		if (*p++ == NULL) {
+			va_end(ptr);
+			return execve(pathP, (void *) arg,
+				      (void *) environ);
+		}
+	}
+	va_end(ptr);
+	errno = E2BIG;
 	return -1;
 }
 
-int execlp(const char *pathP, const char *arg0, ...) 
+
+int execlp(const char *pathP, const char *arg0, ...)
 {
-	/* BUG: this should be ENOSYS, but Fuzix doesn't have that yet */
-	errno = EINVAL;
+	va_list ptr;
+	const char *arg[32];
+	const char **p = arg;
+
+	va_start(ptr, arg0);
+
+	*p++ = arg0;
+
+	while (p != &arg[32]) {
+		*p = va_arg(ptr, const char *);
+		if (*p++ == NULL) {
+			va_end(ptr);
+			return execve(_findPath(pathP), (void *) arg,
+				      (void *) environ);
+		}
+	}
+	va_end(ptr);
+	errno = E2BIG;
 	return -1;
 }
+
 #endif
