9bb917670daf950a11f19c795c6fef7ce395dd0f
Will Sowerbutts <will@sowerbutts.com>
2016-12-28 17:24:50 +0000

    zeta-v2: Save a few bytes

--- Kernel/platform-zeta-v2/zeta-v2.s
+++ Kernel/platform-zeta-v2/zeta-v2.s
@@ -316,9 +316,7 @@ _program_vectors:
 map_process_always:
 	push hl
 	ld hl,#U_DATA__U_PAGE
-	call map_process_2
-	pop hl
-	ret
+        jr map_process_2_pophl_ret
 
 ;=========================================================================
 ; map_process - map process or kernel pages
@@ -338,9 +336,7 @@ map_process:
 map_kernel:
 	push hl
 	ld hl,#_kernel_pages
-	call map_process_2
-	pop hl
-	ret
+        jr map_process_2_pophl_ret
 
 ;=========================================================================
 ; map_process_2 - map process or kernel pages
@@ -377,6 +373,7 @@ map_process_2:
 map_restore:
 	push hl
 	ld hl,#map_savearea
+map_process_2_pophl_ret:
 	call map_process_2
 	pop hl
 	ret
