7a2d731f3ea03178e81cf1d754b9074016c0958a
Will Sowerbutts <will@sowerbutts.com>
2017-01-06 19:25:36 +0000

    Kernel: devide: Improve drive detection and timeout code.

--- Kernel/dev/devide.c
+++ Kernel/dev/devide.c
@@ -22,7 +22,7 @@ bool devide_wait(uint8_t bits)
     uint8_t status;
     timer_t timeout;
 
-    timeout = set_timer_ms(500);
+    timeout = set_timer_sec(20);
 
     while(true){
         status = devide_readb(ide_reg_status);
@@ -30,7 +30,10 @@ bool devide_wait(uint8_t bits)
         if((status & (IDE_STATUS_BUSY | IDE_STATUS_ERROR | bits)) == bits)
             return true;
 
-        if((status & (IDE_STATUS_BUSY | IDE_STATUS_ERROR)) == IDE_STATUS_ERROR){
+        if(((status & (IDE_STATUS_BUSY | IDE_STATUS_ERROR)) == IDE_STATUS_ERROR) || /* error */
+           (status == 0x00) || /* zeta-v2 PPIDE: status=0x00 indicates no slave drive present */
+           (status == 0xFF) || /* zeta-v2 PPIDE: status=0xFF indicates neither master nor slave drive present */
+           (status == 0x87)){  /* n8vem-mark4:   status=0x87 indicates neither master nor slave drive present */
             kprintf("ide error, status=%x\n", status);
             return false;
         }
