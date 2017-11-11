6cc0fb2beefc9ea0ab4f98356e4ef7710479bf9f
Alan Cox <alan@linux.intel.com>
2017-11-11 21:49:02 +0000

    utils add 'clear'
    Not having this was driving me slowly nuts 8)

---
+++ Applications/V7/cmd/clear.c
@@ -0,0 +1,42 @@
+static char *sccsid = "@(#)clear.c	4.1 (Berkeley) 10/1/80";
+/* load me with -ltermlib */
+/* #include <retrofit.h> on version 6 */
+/*
+ * clear - clear the screen
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <termcap.h>
+
+char	PC;
+
+int tputchar(int c)
+{
+	putchar(c);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	char *cp = getenv("TERM");
+	char clbuf[20];
+	char pcbuf[20];
+	char *clbp = clbuf;
+	char *pcbp = pcbuf;
+	char *clear;
+	char buf[1024];
+	char *pc;
+
+	if (cp == (char *) 0)
+		exit(1);
+	if (tgetent(buf, cp) != 1)
+		exit(1);
+	pc = tgetstr("pc", &pcbp);
+	if (pc)
+		PC = *pc;
+	clear = tgetstr("cl", &clbp);
+	if (clear)
+		tputs(clear, tgetnum("li"), tputchar);
+	exit (clear != (char *) 0);
+}
