70f7d96114ddec35928be3df437119436870624a
Alan Cox <alan@linux.intel.com>
2017-11-12 21:06:56 +0000

    mkfs: use O_SYNC

--- Applications/util/mkfs.c
+++ Applications/util/mkfs.c
@@ -174,7 +174,7 @@ void mkfs(uint16_t fsize, uint16_t isize)
     printf("Done.\n");
 }
 
-void printopts( )
+void printopts(void)
 {
 	fprintf( stderr, "usage: mkfs [options] device isize fsize\n");
 	exit(-1);
@@ -224,7 +224,7 @@ int main(int argc, char *argv[])
     if (!yes())
 	exit(-1);
 
-    dev = open(argv[optind], O_RDWR);
+    dev = open(argv[optind], O_RDWR|O_SYNC);
     if (dev < 0) {
         fprintf(stderr, "mkfs: can't open device %s\n", argv[optind]);
         exit(-1);
