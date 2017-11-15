32fae56cdd5507107140bcd867204899c03403f2
Alan Cox <alan@linux.intel.com>
2017-11-15 21:09:50 +0000

    rtc: Headers for RTC in kernel and user

---
+++ Library/include/sys/rtc.h
@@ -0,0 +1,20 @@
+#ifndef _SYS_RTC_H
+#define _SYS_RTC_H
+
+struct cmos_rtc
+{
+	uint8_t type;
+	union {
+		uint8_t bytes[8];
+		time_t clock;
+	} data;
+};
+
+#define CMOS_RTC_BCD	0
+/* BCD encodded YYYY MM DD HH MM SS */
+#define CMOS_RTC_DEC	1
+/* Byte encoded the same. Year is 2 bytes little endian */
+#define CMOS_RTC_TIME	2
+/* Unix time format */
+
+#endif
