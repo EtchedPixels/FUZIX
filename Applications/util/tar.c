0f8ddede4768c96e5cdef812719f228d13a16682
Brett Gordon <beretta42@gmail.com>
2018-04-08 12:41:29 -0400

    tar: make parent directories

--- Applications/util/tar.c
+++ Applications/util/tar.c
@@ -435,6 +435,23 @@ static void list(void)
 	}			/* block while */
 }
 
+/* Attempt to make all parent directories */
+static void makeparents(char *path)
+{
+	static char c[100];
+	char *s = path;
+	int l;
+	while (1){
+		char *e = index(s,'/');
+		if (!e)
+			return;
+		l = e - path;
+		memcpy(c,path,l);
+		c[l] = 0;
+		mkdir(c,0755);
+		s = e + 1;
+	}
+}
 
 /* Extract all file in archive */
 static void extract(char *argv[])
@@ -493,6 +510,8 @@ static void extract(char *argv[])
 		printheader();
 
 		uflag = !strncmp(h.ustar, "ustar", 5);
+
+		makeparents(h.name);
 
 		switch (h.type) {
 		case '1':	/* a hard link */
