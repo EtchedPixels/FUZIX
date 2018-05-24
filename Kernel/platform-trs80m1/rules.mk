f315487416abc0658152cd2203625e5f8073c7a2
Alan Cox <alan@linux.intel.com>
2018-05-25 00:26:00 +0100

    trs80m1: constants can't go in DISCARD2
    Otherwise they can't be accessed by kprintf!

--- Kernel/platform-trs80m1/rules.mk
+++ Kernel/platform-trs80m1/rules.mk
@@ -16,5 +16,5 @@ export CROSS_CC_SYS2=--codeseg CODE1
 export CROSS_CC_SYS3=--codeseg CODE1
 export CROSS_CC_SYS4=--codeseg CODE2
 export CROSS_CC_SYS5=--codeseg CODE2
-export CROSS_CC_SEGDISC=--codeseg DISCARD2 --constseg DISCARD2
+export CROSS_CC_SEGDISC=--codeseg DISCARD2
 
