b9fc07f0eee5edf419d9ad31fe7e16136844ef67
Alan Cox <alan@linux.intel.com>
2018-05-24 15:28:15 +0100

    trs80: introduce a model type

---
+++ Kernel/platform-trs80m1/trs80.h
@@ -0,0 +1,30 @@
+
+extern uint8_t trs80_model;
+#define TRS80_MODEL1	0
+#define TRS80_MODEL3	1
+#define LNW80		2	/* Need to handle LNW80 model II.. */
+#define VIDEOGENIE	3	/* But colour genie may be too different.. */
+
+
+/*
+ *	Differences versus model 1
+ *
+ *	LNW80		Port 0xFE control for memory, extra graphics modes,
+ *			(not currently supported). 4MHz CPU mode. Memory
+ *			expansions include the selector.
+ *	LNW80 II	Adds 4 banks of graphics memory it seems. Also adds
+ *			port 0x1f which controls low system RAM
+ *			(bit 1 - ROM off if set, bit 2 blocks IRQ if set)
+ *	VideoGenie	Printer at 0xFD not memory mapped. The earlier
+ *			expansion unit is a 3 slot S100 backplane so can be
+ *			fitted with banked S100 memory (only the upper 32K
+ *			seems to map to the S100). Some had lower case.
+ *	Model 3		Floppy moves to 0xF0-F4/F7 except for status, Sound
+ *			click moves to 0x90 printer to 0xFB. 0xEF control
+ *			register, NMI control, Interrupt mask register.
+ *			Interrupt latch doesn't ack timer and is inverted.
+ *			Lower case !!
+ *
+ *	Note that the Colour Genie is a very different beast.
+ *	
+ */
