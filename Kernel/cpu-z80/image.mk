a464a0c7accb9b51397632718db61e0b00f48c3e
Alan Cox <alan@linux.intel.com>
2018-06-25 22:09:52 +0100

    z80: use visualize in the build

--- Kernel/cpu-z80/image.mk
+++ Kernel/cpu-z80/image.mk
@@ -1,5 +1,7 @@
 tools/analysemap: tools/analysemap.c
 
+tools/visualize: tools/visualize.c
+
 tools/bihx: tools/bihx.c
 
 tools/binmunge: tools/binmunge.c
@@ -28,11 +30,12 @@ tools/makejv3: tools/makejv3.c
 fuzix.ihx: target $(OBJS) platform-$(TARGET)/fuzix.lnk tools/bankld/sdldz80
 	$(CROSS_LD) -n -k $(LIBZ80) -f platform-$(TARGET)/fuzix.lnk
 
-fuzix.bin: fuzix.ihx tools/bihx tools/analysemap tools/memhogs tools/binman tools/bintomdv tools/binmunge tools/bin2sna tools/bin2z80 cpm-loader/cpmload.bin tools/flat2z80 tools/makejv3
+fuzix.bin: fuzix.ihx tools/bihx tools/analysemap tools/memhogs tools/binman tools/bintomdv tools/binmunge tools/bin2sna tools/bin2z80 cpm-loader/cpmload.bin tools/flat2z80 tools/makejv3 tools/visualize
 	-cp hogs.txt hogs.txt.old
 	tools/memhogs <fuzix.map |sort -nr >hogs.txt
 	head -5 hogs.txt
 	tools/bihx fuzix.ihx
 	tools/binprep
 	+$(MAKE) -C platform-$(TARGET) image
+	tools/visualize < fuzix.map
 
