b7b3ae7510ae13512c157030470b98aa9d4cafee
Alan Cox <alan@linux.intel.com>
2018-07-07 00:41:38 +0100

    trs80: remove prototypes for deleted code

--- Kernel/platform-trs80/devgfx.h
+++ Kernel/platform-trs80/devgfx.h
@@ -4,9 +4,5 @@
 #include <graphics.h>
 
 extern int gfx_ioctl(uint8_t minor, uarg_t arg, char *ptr);
-extern void video_cmd(uint8_t *ptr);
-extern void video_read(uint8_t *ptr);
-extern void video_write(uint8_t *ptr);
-extern void video_exg(uint8_t *ptr);
 
 #endif
