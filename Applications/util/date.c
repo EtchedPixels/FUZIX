d3f8ac6f3d5cb35b5963bae2153c49e31b49a7ef
Alan Cox <alan@linux.intel.com>
2018-07-22 00:35:11 +0100

    date: rewrite and add manual page
    We still need to support custom formats but that needs the fancy time format
    stuff we don't yet have.

--- Applications/util/date.c
+++ Applications/util/date.c
@@ -1,15 +1,40 @@
 #include <time.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+static void usage(void)
+{
+    write(2, "date: [-u]\n", 11);
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[])
 {
-    time_t now;
-    const char *p;
+    time_t t;
+    struct tm *tm;
+    const char *ts, *z;
+    int utc = 0;
+    
+    if (argc > 1 && strcmp(argv[1], "-u") == 0) {
+        utc = 1;
+        argc--;
+    }
+    if (argc != 1)
+        usage();
 
-    now = time(NULL);
-    p = ctime(&now);
-    write(1, p, strlen(p));
-
+    time(&t);
+    if (utc)
+        tm = gmtime(&t);
+    else
+        tm = localtime(&t);
+    ts = asctime(tm);
+    /* Up to the time zone */
+    write(1, ts, 20);
+    /* Zone */
+    z = tzname[tm->tm_isdst];
+    write(1, z, strlen(z));
+    /* Year */
+    write(1, ts + 19, 6);
     return 0;
 }
