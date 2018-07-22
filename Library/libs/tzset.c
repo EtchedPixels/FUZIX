d3366c6eba6beba243ee12554d20eec5ae12a3c7
Alan Cox <alan@linux.intel.com>
2018-07-22 13:54:48 +0100

    tzset: first cut at a fullish parser

--- Library/libs/tzset.c
+++ Library/libs/tzset.c
@@ -12,6 +12,13 @@ char *tzname[2] = { tzbuf0, tzbuf1  };
 
 int daylight;
 long timezone;
+static long dstzone;
+static long dstime[2];	/* Shift for winter/summer */
+static uint16_t dsday;
+static uint8_t dsdow;
+static uint8_t dswom;
+static uint8_t dsmon;
+static uint8_t dsmode;
 
 static uint8_t dpair(char **p, unsigned long *v, uint16_t mul)
 {
@@ -81,26 +88,89 @@ static char *parse_tzspec(char *in, long *l)
  *	POSIX then allows a very complex specification of exactly when time
  *	switches in terms of dates, days of week, last thursday and so on...
  *
- *	FIXME: The spec says hh:mm:ss, the real world appears fine with the
- *	hours bit being a single digit.
  */
 
+static char *tzunit(char *in, char *tzstr, long *p, int required)
+{
+        char *pe = tzstr + 5;
+        while(*in && isalpha(*in)) {
+                if (tzstr < pe)
+                        *tzstr++ = *in;
+                in++;
+        }
+        *pe = 0;
+        pe = parse_tzspec(in, p);
+        /* It's ok not to have the numbers bit the second time and
+           it implies one hour positive */
+        if (pe == NULL) {
+                if (required)
+                        return NULL;
+                else {
+                        *p = timezone + 3600;
+                        return in;
+                }
+        }
+        return pe;
+        
+}
+
+char *parse_dst(char *p, int n)
+{
+	char *np;
+
+        /* If we want to support the rest of the POSIX stuff we now need to
+           parse the start/time and end/time fields. These are not simple
+           Jn Julian date n (day 1-365, leap years ignored)
+           n  Julian date n (day 0-365, leap years included)
+           Mm.w.d day d of week w of month m week 5 always means 'last day d
+              of the month'
+           And of course we have to do something with the data!
+         */
+        if (*p == 'J') {
+		p++;
+		dsmode = 1;
+		dsday = strtoul(p, &p, 0);
+	} else if (*p != 'M') {
+		dsmode = 0;
+		dsday = strtoul(p, &p, 0);
+	} else {
+		/* The complicated one */
+		/* TODO */
+	}
+	if (*p != '/') {
+		dstime[n] = 2 * 3600;	/* 02:00 is the default */
+		return p;
+	}
+	np = parse_tzspec(p, &dstime[n]);
+	if (np == NULL) {
+		dstime[n] = 0L;
+		np = p;
+	}
+	return p;
+}	
+
+        
 void tzset(void)
 {
         char *tz = getenv("TZ");
-        char *p, *pe;
+        char *p;
 
         memcpy(tzbuf0, "GMT", 4);
         timezone = 0 * 60 * 60L;        /* London */
         if (tz == NULL)
                 return;
-        p = tzbuf0;
-        pe = p + 5;
-        while(*tz && isalpha(*tz)) {
-                if (p < pe)
-                        *p++ = *tz;
-                tz++;
-        }
-        tzbuf0[5] = 0;
-        parse_tzspec(tz, &timezone);
+        p = tzunit(tz, tzbuf0, &timezone, 1);
+        /* Not valid */
+        if (p == NULL)
+                return;
+        if (isalpha(*p))
+                p = tzunit(p, tzbuf1, &dstzone, 0);
+        /* If no comma then we are not specifying when the zone changes,
+           and presumably we should look it up somewhere */
+        if (*p++ != ',')
+                return;
+	p = parse_dst(p, 0);
+	if (p == NULL || *p != ',')
+		return;
+	parse_dst(p, 1);
 }
