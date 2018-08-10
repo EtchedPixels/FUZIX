9ba3c3798c7448acb94ff47445e45f72e55f79ba
Alan Cox <alan@linux.intel.com>
2018-08-10 22:23:54 +0100

    trs80m1: build the IDE layer for this machine

---
+++ Kernel/platform-trs80m1/platform_ide.h
@@ -0,0 +1,10 @@
+#define IDE_DRIVE_COUNT	4
+#define IDE_REG_CS1_BASE	0x40
+#define IDE_8BIT_ONLY
+
+#define ide_select(x)
+#define ide_deselect()
+
+/* The transfer isn't non standard but the banked memory requirements are */
+
+#define IDE_NONSTANDARD_XFER
