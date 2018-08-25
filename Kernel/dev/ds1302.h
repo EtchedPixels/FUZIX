e2dc12a7310cc4ac67a639f56e2b39734e28e845
Alan Cox <alan@linux.intel.com>
2018-08-25 15:20:56 +0100

    ds1302: update header
    Export more bits people need outside of the driver

--- Kernel/dev/ds1302.h
+++ Kernel/dev/ds1302.h
@@ -4,11 +4,11 @@
 /* public interface */
 void ds1302_init(void);
 uint8_t platform_rtc_secs(void);
+void ds1302_read_clock(uint8_t *buffer, uint8_t length);
 
 #ifdef _DS1302_PRIVATE
 /* consult the DS1302 datasheet for data format;
    http://datasheets.maximintegrated.com/en/ds/DS1302.pdf table 3 */
-void ds1302_read_clock(uint8_t *buffer, uint8_t length);
 void ds1302_send_byte(uint8_t byte);
 
 uint8_t uint8_from_bcd(uint8_t value);
