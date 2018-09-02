8b4a239364886a4d8755729221a89b4889409b83
Alan Cox <alan@linux.intel.com>
2018-09-02 21:47:36 +0100

    Kernel: commit the rest of propio and ds1302 support for RBC
    SBCv2 should now build from git.

---
+++ Kernel/dev/propio2.h
@@ -0,0 +1,16 @@
+#ifndef _DEV_PROPIO2
+#define _DEV_PROPIO2
+/*
+ *	Propeller I/O 2 card interfaces
+ */
+
+extern void prop_tty_poll(uint8_t minor);
+extern void prop_tty_write(uint8_t c);
+extern uint8_t prop_tty_writeready(void);
+
+extern uint8_t prop_sd_probe(void);
+
+extern void platform_prop_sd_read(void);
+extern void platform_prop_sd_write(void);
+
+#endif
