ce1db8c8a328529b0f4d796bf6e1ae9f60885c74
Alan Cox <alan@linux.intel.com>
2018-09-08 01:13:16 +0100

    aes256: look for header the right way

--- Library/libs/aes256.c
+++ Library/libs/aes256.c
@@ -17,7 +17,7 @@
 *   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 *   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
-#include "aes256.h"
+#include <aes256.h>
 
 #define FD(x)  (((x) >> 1) ^ (((x) & 1) ? 0x8d : 0))
 
