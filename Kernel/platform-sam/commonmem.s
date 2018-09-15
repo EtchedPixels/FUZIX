570f4c344cd506496d51a0cdc430337a6468f1d9
Alan Cox <alan@linux.intel.com>
2018-09-16 00:44:05 +0100

    sam: fix comment

--- Kernel/platform-sam/commonmem.s
+++ Kernel/platform-sam/commonmem.s
@@ -1,6 +1,5 @@
 ;
-;	We have no real common on the TRS80so just tuck it up at the top of
-;	memory leaving room for the keyboard and video (3K)
+;	Standard common mem
 ;
         .module commonmem
 
