0f6ac19c39fa75894413b0796eea8cff0c6bbcd0
Alan Cox <alan@linux.intel.com>
2018-10-02 14:02:29 +0100

    68000: update to new tty masking

--- Kernel/platform-v68-banked/devtty.c
+++ Kernel/platform-v68-banked/devtty.c
@@ -16,6 +16,18 @@ struct s_queue ttyinq[NUM_DEV_TTY + 1] = {	/* ttyinq[0] is never used */
 	{tbuf1, tbuf1, tbuf1, TTYSIZ, 0, TTYSIZ / 2},
 };
 
+static tcflag_t console_mask[4] = {
+	_ISYS,
+	_OSYS,
+	_CSYS,
+	_LSYS
+};
+
+tcflag_t *termios_mask[NUM_DEV_TTY + 1] = {
+	NULL,
+	console_mask
+};
+
 /* Output for the system console (kprintf etc) */
 void kputchar(char c)
 {
