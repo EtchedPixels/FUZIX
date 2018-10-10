503dd672c40f16f162e28bb739797f1c9f5a287a
Alan Cox <alan@linux.intel.com>
2018-10-10 22:18:29 +0100

    chmem: merge in utils improvements

--- Standalone/chmem.c
+++ Standalone/chmem.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -10,6 +11,7 @@ int main(int argc, char *argv[])
   FILE *fp;
   unsigned char buf[10];
   unsigned short top;
+  uint8_t be;
 
   if (argc != 2 && argc != 3) {
     fprintf(stderr, "%s [executable] {size}\n", argv[0]);
@@ -28,13 +30,26 @@ int main(int argc, char *argv[])
     exit(1);
   }
   /* FIXME : add 6809 but remember its big endian! */
-  if ((buf[0] != 0x4C && buf[0] != 0xC3) || buf[3] != 'F' || buf[4] != 'Z'|| buf[5] != 'X' ||
+  if (buf[3] != 'F' || buf[4] != 'Z'|| buf[5] != 'X' ||
     buf[6] != '1') {
     fprintf(stderr, "%s: not a Fuzix binary format.\n", argv[1]);
     exit(1);
   }
+  if (buf[3] == 0x7E || buf[3] == 0x20)
+    be = 1;	/* 6809 */
+  else if (buf[3] == 0x4C || buf[3] == 0x38)
+    be = 0;	/* 6502 */
+  else if (buf[3] == 0xC3 || buf[3] == 0x18)
+    be = 0;	/* Z80 */
+  else {
+    fprintf(stderr, "%s: unknown Fuzix binary type.\n", argv[1]);
+    exit(1);
+  }
   if (argc == 2) {
-    top = buf[8] | (buf[9] << 8);
+    if (be)
+      top = buf[9] | (buf[8] << 8);
+    else
+      top = buf[8] | (buf[9] << 8);
     if (top)
       printf("Fuzix binary set at %d bytes.\n", top);
     else
@@ -46,8 +61,13 @@ int main(int argc, char *argv[])
     fprintf(stderr, "%s: invalid chmem value '%s'.\n", argv[0], argv[2]);
     exit(1);
   }
-  buf[8] = v & 0xFF;
-  buf[9] = v >> 8;
+  if (be) {
+    buf[8] = v >> 8;
+    buf[9] = v & 0xFF;
+  } else {
+    buf[8] = v & 0xFF;
+    buf[9] = v >> 8;
+  }
   rewind(fp);
   if(fwrite(buf, 10, 1, fp) != 1) {
     fprintf(stderr, "%s: write error.\n", argv[0]);
