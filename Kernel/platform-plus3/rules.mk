c1ba44f1af60697ca12994fef74aacb82c007a56
Alan Cox <alan@linux.intel.com>
2018-11-30 00:36:45 +0000

    plus3: remove old dead plus3 port work

--- Kernel/platform-plus3/rules.mk
+++
@@ -1,3 +0,0 @@
-#
-#	ZXSpectrum +3 uses unbanked kernel images (for now anyway)
-#
--- Kernel/platform-plus3/sysconfig.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SYSCONFIG_H
-#define _SYSCONFIG_H
-
-extern uint16_t sysconfig;
-
-#define CONF_PLUS3		0x0001		/* Plus3 (has 3DOS) */
-
-#endif
