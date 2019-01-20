3b8473ca788ed222b583bdcf45e9c314b9a69c9b
Alan Cox <alan@linux.intel.com>
2019-01-20 15:13:08 +0000

    util: chmem and size support for 32bit
    Also clean up the formatting and other details.

--- Applications/util/chmem.c
+++ Applications/util/chmem.c
@@ -4,74 +4,119 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#if defined(__m68k__)
+
+static void chmem_flat(FILE * fp, unsigned char *buf, int argc, char *argv[])
+{
+	uint32_t *hdr = (uint32_t *) buf;
+	uint32_t top;
+	unsigned int v;
+
+	if (ntohl(hdr[1]) != 4) {
+		fprintf(stderr, "%s: unsupported revision %d.\n", ntohl(hdr[1]));
+		exit(1);
+	}
+	if (argc == 2) {
+		top = ntohl(hdr[6]);
+		if (top)
+			printf("Flat binary set at %d bytes.\n", top);
+		else
+			printf("Flat binary, set to allocate all available.\n");
+		return;
+	}
+
+	if (sscanf(argv[2], "%u", &v) != 1) {
+		fprintf(stderr, "%s: invalid chmem value '%s'.\n", argv[0], argv[2]);
+		exit(1);
+	}
+	hdr[6] = htonl(v);
+	rewind(fp);
+	if (fwrite(buf, 32, 1, fp) != 1) {
+		fprintf(stderr, "%s: write error.\n", argv[0]);
+		exit(1);
+	}
+}
+#endif
+
+static void chmem_fzx1(FILE * fp, unsigned char *buf, int argc, char *argv[])
+{
+	unsigned int v;
+	unsigned short top;
+	uint8_t be;
+	if (buf[0] == 0x7E || buf[0] == 0x20)
+		be = 1;		/* 6809 */
+	else if (buf[0] == 0x4C || buf[0] == 0x38)
+		be = 0;		/* 6502 */
+	else if (buf[0] == 0xC3 || buf[0] == 0x18)
+		be = 0;		/* Z80 */
+	else {
+		fprintf(stderr, "%s: unknown Fuzix FZX1 binary type.\n", argv[1]);
+		exit(1);
+	}
+	if (argc == 2) {
+		if (be)
+			top = buf[9] | (buf[8] << 8);
+		else
+			top = buf[8] | (buf[9] << 8);
+		if (top)
+			printf("Fuzix binary set at %d bytes.\n", top);
+		else
+			printf("Fuzix binary, set to allocate all available.\n");
+		return;
+	}
+
+	if (sscanf(argv[2], "%u", &v) != 1 || v > 65536) {
+		fprintf(stderr, "%s: invalid chmem value '%s'.\n", argv[0], argv[2]);
+		exit(1);
+	}
+	if (be) {
+		buf[8] = v >> 8;
+		buf[9] = v & 0xFF;
+	} else {
+		buf[8] = v & 0xFF;
+		buf[9] = v >> 8;
+	}
+	rewind(fp);
+	if (fwrite(buf, 10, 1, fp) != 1) {
+		fprintf(stderr, "%s: write error.\n", argv[0]);
+		exit(1);
+	}
+}
 
 int main(int argc, char *argv[])
 {
-  unsigned int v;
-  FILE *fp;
-  unsigned char buf[10];
-  unsigned short top;
-  uint8_t be;
+	FILE *fp;
+	unsigned char buf[32];
 
-  if (argc != 2 && argc != 3) {
-    fprintf(stderr, "%s [executable] {size}\n", argv[0]);
-    exit(1);
-  }
-  if (argc == 2)
-    fp = fopen(argv[1], "r");
-  else
-    fp = fopen(argv[1], "r+");
-  if (fp == NULL) {
-    perror(argv[1]);
-    exit(1);
-  }
-  if (fread(buf, 10, 1, fp) != 1) {
-    fprintf(stderr, "%s: too short ?\n", argv[0]);
-    exit(1);
-  }
-  if (buf[3] != 'F' || buf[4] != 'Z'|| buf[5] != 'X' ||
-    buf[6] != '1') {
-    fprintf(stderr, "%s: not a Fuzix binary format.\n", argv[1]);
-    exit(1);
-  }
-  if (buf[0] == 0x7E || buf[0] == 0x20)
-    be = 1;	/* 6809 */
-  else if (buf[0] == 0x4C || buf[0] == 0x38)
-    be = 0;	/* 6502 */
-  else if (buf[0] == 0xC3 || buf[0] == 0x18)
-    be = 0;	/* Z80 */
-  else {
-    fprintf(stderr, "%s: unknown Fuzix binary type.\n", argv[1]);
-    exit(1);
-  }
-  if (argc == 2) {
-    if (be)
-      top = buf[9] | (buf[8] << 8);
-    else
-      top = buf[8] | (buf[9] << 8);
-    if (top)
-      printf("Fuzix binary set at %d bytes.\n", top);
-    else
-      printf("Fuzix binary, set to allocate all available.\n");
-    exit(0);
-  }
-      
-  if (sscanf(argv[2], "%u", &v) != 1 || v > 65536) {
-    fprintf(stderr, "%s: invalid chmem value '%s'.\n", argv[0], argv[2]);
-    exit(1);
-  }
-  if (be) {
-    buf[8] = v >> 8;
-    buf[9] = v & 0xFF;
-  } else {
-    buf[8] = v & 0xFF;
-    buf[9] = v >> 8;
-  }
-  rewind(fp);
-  if(fwrite(buf, 10, 1, fp) != 1) {
-    fprintf(stderr, "%s: write error.\n", argv[0]);
-    exit(1);
-  }
-  fclose(fp);
-  exit(0);
+	if (argc != 2 && argc != 3) {
+		fprintf(stderr, "%s [executable] {size}\n", argv[0]);
+		exit(1);
+	}
+	if (argc == 2)
+		fp = fopen(argv[1], "r");
+	else
+		fp = fopen(argv[1], "r+");
+	if (fp == NULL) {
+		perror(argv[1]);
+		exit(1);
+	}
+	if (fread(buf, 32, 1, fp) != 1) {
+		fprintf(stderr, "%s: too short ?\n", argv[0]);
+		exit(1);
+	}
+	if (memcmp(buf + 3, "FZX1", 4) == 0)
+		chmem_fzx1(fp, buf, argc, argv);
+#if defined(__m68k__)
+	else if (memcmp(buf, "bFLT", 4) == 0)
+		chmem_flat(fp, buf, argc, argv);
+#endif
+	else {
+		fprintf(stderr, "%s: not a Fuzix binary format.\n", argv[1]);
+		exit(1);
+	}
+	fclose(fp);
+	exit(0);
 }
--- Applications/util/size.c
+++ Applications/util/size.c
@@ -3,72 +3,122 @@
 #include <stdlib.h>
 #include <string.h>
 
-static unsigned int
-bufpair(int bigend, unsigned char *p, int n)
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+static int err = 0;
+static int head = 0;
+
+static unsigned int bufpair(int bigend, unsigned char *p, int n)
 {
-  if (bigend) {
-    /* mc6809, big endian */
-    return (p[n] << 8) | p[n+1];
-  }
-  return p[n] | (p[n+1] << 8);
+	if (bigend) {
+		/* mc6809, big endian */
+		return (p[n] << 8) | p[n + 1];
+	}
+	return p[n] | (p[n + 1] << 8);
+}
+
+#if defined(__m68k__)
+static void size_binflat(char *name, uint8_t * buf)
+{
+	uint32_t *hdr = (uint32_t *) buf;
+	uint32_t txtsz, datsz, bsssz, stksz;
+
+	if (ntohl(hdr[1]) != 4) {
+		fprintf(stderr, "%s: unknown binflat revision %d.\n", name, hdr[1]);
+		err = 1;
+		return;
+	}
+	txtsz = ntohl(hdr[3]);	/* Start of data, end of text */
+	datsz = ntohl(hdr[4]);	/* End of data, start of bss */
+	bsssz = ntohl(hdr[5]);	/* End of bss */
+	bsssz -= datsz;		/* Get the BSS size */
+	datsz -= txtsz;		/* And data size */
+
+	stksz = ntohl(hdr[6]);
+
+	if (head != 2) {
+		if (head == 1)
+			printf("\n");
+		printf("   text    data     bss    stack   size     hex filename\n");
+		head = 2;
+	}
+
+	printf("%7d %7d %7d %7d %7d %7x %s\n",
+		txtsz, datsz, bsssz, stksz,
+		txtsz + datsz + bsssz + stksz,
+		txtsz + datsz + bsssz + stksz,
+		name);
+}
+#endif
+
+static void size_fzx1(char *name, uint8_t * buf)
+{
+	unsigned int basepage = 0;
+	int endian;
+	unsigned int txtsz, datsz, bsssz;
+
+	if (buf[0] == 0xC3 || buf[0] == 0x18 || buf[0] == 0x4C || buf[0] == 0x38) {
+		/* Z-80 or 6502 */
+		endian = 0;
+		basepage = buf[7] << 8;
+	} else if (buf[0] == 0x7E || buf[0] == 0x20) {
+		/* 6809 */
+		endian = 1;
+	} else {
+		endian = -1;
+	}
+	if (endian == -1) {
+		fprintf(stderr, "%s: not a known FZX1 platform.\n", name);
+		err = 1;
+		return;
+	}
+	/* Text, data, BSS */
+	txtsz = bufpair(endian, buf, 10);
+	datsz = bufpair(endian, buf, 12);
+	bsssz = bufpair(endian, buf, 14);
+
+	if (head != 1) {
+		if (head == 2)
+			printf("\n");
+		printf(" base text data  bss   size  hex filename\n");
+		head = 1;
+	}
+	printf("%5x%5x%5x%5x%7d%5x %s\n", basepage, txtsz, datsz, bsssz, txtsz + datsz + bsssz, txtsz + datsz + bsssz, name);
 }
 
 int main(int argc, char *argv[])
 {
-  FILE *fp;
-  unsigned char buf[16];
-  unsigned int basepage;
-  int n, endian;
-  unsigned int txtsz, datsz, bsssz;
+	FILE *fp;
+	unsigned char buf[32];
+	int n;
 
-  if (argc < 2) {
-    fprintf(stderr, "%s [executable]\n", argv[0]);
-    exit(1);
-  }
-  for (n = 1; n < argc; n++) {
-    fp = fopen(argv[n], "r");
-    if (fp == NULL) {
-      perror(argv[n]);
-      exit(1);
-    }
-    if (fread(buf, 16, 1, fp) != 1) {
-      fprintf(stderr, "%s: too short ?\n", argv[0]);
-      exit(1);
-    }
+	if (argc < 2) {
+		fprintf(stderr, "%s [executable...]\n", argv[0]);
+		exit(1);
+	}
 
-    basepage = 0;
-    if (buf[0] == 0xC3 || buf[0] == 0x18 || buf[0] == 0x4C || buf[0] == 0x38) {
-	/* Z-80 or 6502 */
-	endian = 0;
-	basepage = buf[7] << 8;
-    } else if (buf[0] == 0x7E || buf[0] == 0x20) {
-	/* 6809 */
-	endian = 1;
-    } else {
-	endian = -1;
-    }
-    if ((endian == -1) ||
-	    (buf[3] != 'F') ||
-	    (buf[4] != 'Z') ||
-	    (buf[5] != 'X') ||
-	    (buf[6] != '1')) {
-	fprintf(stderr, "%s: not a Fuzix binary format.\n", argv[1]);
-	exit(1);
-    }
-    fclose(fp);
-    printf(" base text data  bss   size  hex filename\n");
 
-    /* Text, data, BSS */
-    txtsz = bufpair(endian, buf, 10);
-    datsz = bufpair(endian, buf, 12);
-    bsssz = bufpair(endian, buf, 14);
+	for (n = 1; n < argc; n++) {
+		fp = fopen(argv[n], "r");
+		if (fp == NULL) {
+			perror(argv[n]);
+			exit(1);
+		}
+		if (fread(buf, 32, 1, fp) != 1) {
+			fprintf(stderr, "%s: too short ?\n", argv[0]);
+			exit(1);
+		}
+		fclose(fp);
 
-    printf("%5x%5x%5x%5x%7d%5x %s\n",
-      basepage,
-      txtsz, datsz, bsssz,
-      txtsz+datsz+bsssz,
-      txtsz+datsz+bsssz,
-      argv[n]);
-  }
-  exit(0);
+		if (memcmp(buf + 3, "FZX1", 4) == 0)
+			size_fzx1(argv[n], buf);
+#if defined(__m68k__)
+		else if (memcmp(buf, "bFLT", 4) == 0)
+			size_binflat(argv[n], buf);
+#endif
+		else
+			fprintf(stderr, "%s: not a Fuzix binary format.\n", argv[n]);
+	}
+	exit(err);
 }
