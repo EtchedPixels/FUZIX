28fcd3ec900c7e0ff8cfb5294c012b31e2a332ce
Alan Cox <alan@linux.intel.com>
2019-02-04 18:37:49 +0000

    yaz180: initial bits

---
+++ Kernel/platform-yaz180/config.h
@@ -0,0 +1,51 @@
+/* Enable to make ^Z dump the inode table for debug */
+#undef CONFIG_IDUMP
+/* Enable to make ^A drop back into the monitor */
+#undef CONFIG_MONITOR
+/* Profil syscall support (not yet complete) */
+#undef CONFIG_PROFIL
+/* Multiple processes in memory at once */
+#define CONFIG_MULTI
+/* Single tasking */
+#undef CONFIG_SINGLETASK
+/* CP/M emulation */
+#undef CONFIG_CPM_EMU
+/* Fixed banking: 8 x 64K banks, top 4KB is shared with kernel, 60KB-62KB is user memory  */
+#define CONFIG_BANK_FIXED
+/* Permit large I/O requests to bypass cache and go direct to userspace */
+#define CONFIG_LARGE_IO_DIRECT(x)	1
+/* 8 60K banks, 1 is kernel */
+#define MAX_MAPS	8
+#define MAP_SIZE	PROGTOP    /* WRS: I feel this should be 60KB, but setting it so breaks pagemap_realloc() when exec calls it */
+
+/* Banks as reported to user space */
+#define CONFIG_BANKS	1
+
+#define TICKSPERSEC 40U     /* Ticks per second */
+#define PROGBASE    0x0000  /* also data base */
+#define PROGLOAD    0x0100  /* also data base */
+#define PROGTOP     0xF800  /* Top of program, base of U_DATA copy */
+#define KERNTOP     0xF000  /* Kernel has lower 60KB */
+#define PROC_SIZE   64      /* Memory needed per process */
+
+/* We need a tidier way to do this from the loader */
+#define CMDLINE	(0x0081)  /* Location of root dev name */
+#define BOOTDEVICENAMES "hd#"
+
+#define CONFIG_DYNAMIC_BUFPOOL /* we expand bufpool to overwrite the _DISCARD segment at boot */
+#define NBUFS    4        /* Number of block buffers, keep in line with space reserved in yaz180.s */
+#define NMOUNTS	 4	  /* Number of mounts at a time */
+
+/* Hardware parameters */
+#define Z180_IO_BASE       0x00
+
+#define MAX_BLKDEV 2	    /* 2 IDE drives */
+
+#define CONFIG_IDE
+#define CONFIG_PPIDE
+
+#define NUM_DEV_TTY 2
+/* ASCI0 as the console */
+#define TTYDEV   (512+1)  /* System console (used by kernel, init) */
+
+#define platform_copyright()
---
+++ Kernel/platform-yaz180/devtty.c
@@ -0,0 +1,91 @@
+#include <kernel.h>
+#include <kdata.h>
+#include <printf.h>
+#include <stdbool.h>
+#include <tty.h>
+#include <devtty.h>
+#include <z180.h>
+
+static char tbuf1[TTYSIZ];
+static char tbuf2[TTYSIZ];
+
+struct  s_queue  ttyinq[NUM_DEV_TTY+1] = {       /* ttyinq[0] is never used */
+    {   NULL,    NULL,    NULL,    0,        0,       0    },
+    {   tbuf1,   tbuf1,   tbuf1,   TTYSIZ,   0,   TTYSIZ/2 },
+    {   tbuf2,   tbuf2,   tbuf2,   TTYSIZ,   0,   TTYSIZ/2 },
+};
+
+static tcflag_t console_mask[4] = {
+	_ISYS,
+	_OSYS,
+	_CSYS,
+	_LSYS
+};
+
+/* TODO: stty support for  the Z180 ports */
+tcflag_t *termios_mask[NUM_DEV_TTY + 1] = {
+	NULL,
+	console_mask,
+	console_mask,
+};
+
+void tty_setup(uint8_t minor, uint8_t flags)
+{
+    minor;
+}
+
+/* For the moment */
+int tty_carrier(uint8_t minor)
+{
+    minor;
+    return 1;
+}
+
+void tty_pollirq_asci0(void)
+{
+    while(ASCI_STAT0 & 0x80)
+        tty_inproc(1, ASCI_RDR0);
+}
+
+void tty_pollirq_asci1(void)
+{
+    while(ASCI_STAT1 & 0x80)
+        tty_inproc(2, ASCI_RDR1);
+}
+
+void tty_putc(uint8_t minor, unsigned char c)
+{
+    switch(minor){
+        case 1:
+            while(!(ASCI_STAT0 & 2));
+            ASCI_TDR0 = c;
+            break;
+        case 2:
+            while(!(ASCI_STAT1 & 2));
+            ASCI_TDR1 = c;
+            break;
+    }
+}
+
+void tty_sleeping(uint8_t minor)
+{
+    minor;
+}
+
+void tty_data_consumed(uint8_t minor)
+{
+}
+
+ttyready_t tty_writeready(uint8_t minor)
+{
+    minor;
+    return TTY_READY_NOW;
+}
+
+/* kernel writes to system console -- never sleep! */
+void kputchar(char c)
+{
+    tty_putc(TTYDEV & 0xFF, c);
+    if(c == '\n')
+        tty_putc(TTYDEV & 0xFF, '\r');
+}
