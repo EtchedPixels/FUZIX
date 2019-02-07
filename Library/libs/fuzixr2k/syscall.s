428f8543fb898e11265f717354d5aa00d54a03a4
Alan Cox <alan@linux.intel.com>
2019-02-07 23:35:20 +0000

    rabbit: syscall stub

--- Library/libs/fuzixr2k/syscall.s
+++ Library/libs/fuzixr2k/syscall.s
@@ -1,15 +1,14 @@
 		.globl	__syscall
 		.globl	_errno
 
+;
+;	For Rabbit we use the new syscall ABI we plan to use for Z80.
+;
 __syscall:
-;		ex	(sp), hl		; hl is now return addr
-						; stack is syscall
-		ex	de, hl			; save return addr in de
-		rst	#0x28
-		ex	de, hl			; undo the magic
-;		ex	(sp), hl
-		ex	de, hl			; return with HL
-		ret	nc			; ok
-		ld	(_errno), hl		; error path
+		rst	0x28		; Needs to be an RST if we want
+					; to also run on R3000A so we get
+					; a system mode entry
+		ret	nc		; ok
+		ld	(_errno), hl	; error path
 		ld	hl, #0xffff
 		ret
