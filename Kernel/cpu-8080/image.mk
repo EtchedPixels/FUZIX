6dacbe23ce6cce210b0e6a0bd6937af42d4238ec
Alan Cox <alan@linux.intel.com>
2019-02-09 21:50:32 +0000

    8080: build the new tools

--- Kernel/cpu-8080/image.mk
+++ Kernel/cpu-8080/image.mk
@@ -1,2 +1,7 @@
-fuzix.bin: target $(OBJS)
+
+tools/8080map: tools/8080map.c
+
+tools/ack2kernel: tools/ack2kernel.c
+
+fuzix.bin: target $(OBJS) tools/8080map tools/ack2kernel
 	+$(MAKE) -C platform-$(TARGET) image
