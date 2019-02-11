7094f8fd242651ac58147010919fbbc41fe57cd6
Alan Cox <alan@linux.intel.com>
2019-02-11 23:30:13 +0000

    8080fixedbank: remove some debug

--- Kernel/lib/8080fixedbank.s
+++ Kernel/lib/8080fixedbank.s
@@ -6,17 +6,6 @@
 !	Fast copy 512 bytes from H to D
 !
 copy512:
-	in 21
-	adi 48
-	out 1
-	mvi a,":"
-	out 1
-	call outhl
-	xchg
-	mvi a,"-"
-	out 1
-	call outhl
-	xchg
 	mvi b,64
 copy8:
 	mov a,m
@@ -53,8 +42,4 @@ copy8:
 	inx d
 	dcr b
 	jnz copy8
-	mvi a,'='
-	out 1
-	lhld 0xe800
-	call outhl
 	ret
