1a75e2e125d95d0e5120d71b3cb1dc3b3e6a73b6
Alan Cox <alan@linux.intel.com>
2019-03-15 20:02:35 +0000

    trs80m1: fix banking to cover CODE4

--- Kernel/platform-trs80m1/rules.mk
+++ Kernel/platform-trs80m1/rules.mk
@@ -9,6 +9,7 @@ export BANKED=-banked
 export CROSS_CC_SEG1=--codeseg CODE1
 export CROSS_CC_SEG2=--codeseg CODE2
 export CROSS_CC_SEG3=--codeseg CODE1
+export CROSS_CC_SEG4=--codeseg CODE1
 export CROSS_CC_VIDEO=--codeseg CODE2
 #
 export CROSS_CC_SYS1=--codeseg CODE1
