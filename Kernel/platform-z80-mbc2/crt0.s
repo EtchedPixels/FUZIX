bd2c39ebf3bb3d4b5c5b1e938f8dae5601db8e4c
Alan Cox <alan@linux.intel.com>
2019-05-11 00:28:31 +0100

    z80-mbc2: A new port
    An initial port to the Z80MBC2 system.
    
    It's a bit basic because the I/O is a bit limited. Really the IOS side of
    it needs to grow a few things including testing for uart write blocking and
    timers.

---
+++ Kernel/platform-z80-mbc2/crt0.s
@@ -0,0 +1,74 @@
+	        ; Ordering of segments for the linker.
+	        .area _CODE
+	        .area _CODE2
+		.area _HOME
+	        .area _CONST
+	        .area _INITIALIZED
+	        .area _DATA
+	        .area _BSEG
+	        .area _BSS
+	        .area _HEAP
+	        .area _GSINIT
+	        .area _GSFINAL
+		.area _BUFFERS
+	        ; note that areas below here may be overwritten by the heap at runtime, so
+	        ; put initialisation stuff in here
+	        .area _INITIALIZER
+		.area _DISCARD
+	        .area _COMMONMEM
+
+        	; imported symbols
+        	.globl _fuzix_main
+	        .globl init_early
+	        .globl init_hardware
+	        .globl s__DATA
+	        .globl l__DATA
+	        .globl s__DISCARD
+	        .globl l__DISCARD
+	        .globl s__BUFFERS
+	        .globl l__BUFFERS
+	        .globl s__COMMONMEM
+	        .globl l__COMMONMEM
+	        .globl kstack_top
+		.globl map_kernel
+
+	        ; startup code
+	        .area _CODE
+
+		; Load at 0x0000
+		jp start
+		.ds 125
+;
+;	Leave low space for vectors (FIXME - pre init them)
+;
+start:
+		di
+		ld sp, #kstack_top
+		; move the common memory where it belongs    
+		ld hl, #s__DATA
+		ld de, #s__COMMONMEM
+		ld bc, #l__COMMONMEM
+		ldir
+		; then the discard
+		; Discard can just be linked in but is next to the buffers
+		ld de, #s__DISCARD
+		ld bc, #l__DISCARD
+		ldir
+		; then zero the data area
+		ld hl, #s__DATA
+		ld de, #s__DATA + 1
+		ld bc, #l__DATA - 1
+		ld (hl), #0
+		ldir
+;		Zero buffers area
+		ld hl, #s__BUFFERS
+		ld de, #s__BUFFERS + 1
+		ld bc, #l__BUFFERS - 1
+		ld (hl), #0
+		ldir
+		call init_early
+		call init_hardware
+		call _fuzix_main
+		di
+stop:		halt
+		jr stop
---
+++ Kernel/platform-z80-mbc2/main.c
@@ -0,0 +1,149 @@
+#include <kernel.h>
+#include <timer.h>
+#include <kdata.h>
+#include <printf.h>
+#include <devtty.h>
+#include <rtc.h>
+#include <mbc2.h>
+
+uint16_t ramtop = PROGTOP;
+uint16_t swap_dev = 0xFFFF;
+
+/*
+ *	This routine is called continually when the machine has nothing else
+ *	it needs to execute. On a machine with entirely interrupt driven
+ *	hardware this could just halt for interrupt.
+ */
+void platform_idle(void)
+{
+	/* Disable interrupts so we don't accidentally process a polled tty
+	   and interrupt call at once and make a mess */
+	irqflags_t irq = di();
+	sync_clock();
+	tty_poll();
+	/* Restore prior state. */
+	irqrestore(irq);
+}
+
+/*
+ *	This routine is called from the interrupt handler code to process
+ *	interrupts. All of the nasty stuff (register saving, bank switching,
+ *	reti instructions) is dealt with for you.
+ *
+ *	Most platforms would read something to identify the interrupt source
+ *	but in our case the only possible source is the serial uart.
+ */
+void platform_interrupt(void)
+{
+	tty_poll();
+}
+
+/* This points to the last buffer in the disk buffers. There must be at least
+   four buffers to avoid deadlocks. */
+struct blkbuf *bufpool_end = bufpool + NBUFS;
+
+/*
+ *	We pack discard into the memory image is if it were just normal
+ *	code but place it at the end after the buffers. When we finish up
+ *	booting we turn everything from the buffer pool to common into
+ *	buffers. This blows away the _DISCARD segment.
+ */
+void platform_discard(void)
+{
+	uint16_t discard_size = (uint16_t)udata - (uint16_t)bufpool_end;
+	bufptr bp = bufpool_end;
+
+	discard_size /= sizeof(struct blkbuf);
+
+	kprintf("%d buffers added\n", discard_size);
+
+	bufpool_end += discard_size;
+
+	memset( bp, 0, discard_size * sizeof(struct blkbuf) );
+
+	for( bp = bufpool + NBUFS; bp < bufpool_end; ++bp ){
+		bp->bf_dev = NO_DEVICE;
+		bp->bf_busy = BF_FREE;
+	}
+}
+
+#ifdef CONFIG_NO_CLOCK
+
+/*
+ *	Logic for tickless system. If you have an RTC with a timer tick
+ *	you can ignore this.
+ */
+
+static uint8_t newticks = 0xFF;
+static uint8_t oldticks;
+
+static uint8_t re_enter;
+
+/*
+ *	Hardware specific logic to get the seconds. We really ought to enhance
+ *	this to check minutes as well just in case something gets stuck for
+ *	ages.
+ */
+static void sync_clock_read(void)
+{
+	oldticks = newticks;
+	opcode = OP_GET_RTC;
+	newticks = opread;
+	/* Don't bother with the rest of the bytes */
+}
+
+/*
+ *	The OS core will invoke this routine when idle (via platform_idle) but
+ *	also after a system call and in certain other spots to ensure the clock
+ *	is roughly valid. It may be called from interrupts, without interrupts
+ *	or even recursively so it must protect itself using the framework
+ *	below.
+ *
+ *	Having worked out how much time has passed in 1/10ths of a second it
+ *	performs that may timer_interrupt events in order to advance the clock.
+ *	The core kernel logic ensures that we won't do anything silly from a
+ *	jump forward of many seconds.
+ *
+ *	We also choose to poll the ttys here so the user has some chance of
+ *	getting control back on a messed up process.
+ */
+void sync_clock(void)
+{
+	irqflags_t irq = di();
+	int16_t tmp;
+	if (!re_enter++) {
+		sync_clock_read();
+		if (oldticks != 0xFF) {
+			tmp = newticks - oldticks;
+			if (tmp < 0)
+				tmp += 60;
+			tmp *= 10;
+			while(tmp--) {
+				timer_interrupt();
+			}
+			platform_interrupt();
+		}
+	} 
+	re_enter--;
+	irqrestore(irq);
+}
+
+/*
+ *	This method is called if the kernel has changed the system clock. We
+ *	don't work out how much work we need to do by using it as a reference
+ *	so we don't care.
+ */
+void update_sync_clock(void)
+{
+}
+
+#else
+
+uint8_t platform_rtc_secs(void)
+{
+	opcode = OP_GET_RTC;
+	return opread;
+	/* Don't bother with the rest of the bytes */
+}
+
+#endif
