1788bb190e6e02d9046175c2d6f485bf4bd1373f
Alan Cox <alan@linux.intel.com>
2019-05-12 14:02:28 +0100

    syscall_exec16: fix warning if UFTSIZE is 16
    In that case we generate 1 << 15, which sdcc (arguably correctly) considers
    worth warning about as we shifted into the sign bit.

--- Kernel/syscall_exec16.c
+++ Kernel/syscall_exec16.c
@@ -6,7 +6,7 @@
 static void close_on_exec(void)
 {
 	/* Keep the mask separate to stop SDCC generating crap code */
-	uint16_t m = 1 << (UFTSIZE - 1);
+	uint16_t m = 1U << (UFTSIZE - 1);
 	int8_t j;
 
 	for (j = UFTSIZE - 1; j >= 0; --j) {
