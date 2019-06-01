a8dbe957be6313e6e41b67aef3390cc445d16638
Alan Cox <alan@linux.intel.com>
2019-06-01 01:24:19 +0100

    6809: add _fast_ types

--- Library/include/6809/stdint.h
+++ Library/include/6809/stdint.h
@@ -13,6 +13,12 @@ typedef signed char int8_t;
 typedef int16_t intptr_t;
 typedef uint16_t uintptr_t;
 
+typedef uint8_t uint_fast8_t;
+typedef int8_t int_fast8_t;
+typedef uint16_t uint_fast16_t;
+typedef int16_t int_fast16_t;
+typedef uint32_t uint_fast32_t;
+typedef int32_t int_fast32_t;
 #define __SIZE_T_DEFINED
 #define NO_64BIT
 
