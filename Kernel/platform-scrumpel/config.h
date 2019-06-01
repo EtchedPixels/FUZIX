4dca6a4f54e02c10cd4d48f1fd8d0658b7b764f2
Alan Cox <alan@linux.intel.com>
2019-06-01 16:27:04 +0100

    scrumpel: An initial sketch for Nick to work on

---
+++ Kernel/platform-scrumpel/config.h
@@ -0,0 +1,68 @@
+/* Enable to make ^Z dump the inode table for debug */
+#undef CONFIG_IDUMP
+/* Enable to make ^A drop back into the monitor */
+#undef CONFIG_MONITOR
+/* Profil syscall support (not yet complete) */
+#undef CONFIG_PROFIL
+/* Multiple processes in memory at once */
+#define CONFIG_MULTI
+/* Single tasking */
+#undef CONFIG_SINGLETASK
+/* Fixed banking: 8 x 64K banks, top 4KB is shared with kernel, 60KB-62KB is user memory  */
+#define CONFIG_BANK_FIXED
+/* Permit large I/O requests to bypass cache and go direct to userspace */
+#define CONFIG_LARGE_IO_DIRECT(x)	1
+/* 8 60K banks, 1 is kernel */
+#define MAX_MAPS	8
+#define MAP_SIZE	PROGTOP    /* WRS: I feel this should be 60KB, but setting it so breaks pagemap_realloc() when exec calls it */
+
+/* Banks as reported to user space */
+#define CONFIG_BANKS	1
+
+#define TICKSPERSEC 40U     /* Ticks per second */
+#define PROGBASE    0x0000  /* also data base */
+#define PROGLOAD    0x0100  /* also data base */
+#define PROGTOP     0xF600  /* Top of program, base of U_DATA */
+#define KERNTOP     0xF600  /* Kernel has lower 60KB */
+#define PROC_SIZE   64      /* Memory needed per process */
+
+#define CONFIG_IDE
+#define CONFIG_PPIDE
+
+/* We need a tidier way to do this from the loader */
+#define CMDLINE	(0x0081)  /* Location of root dev name */
+#define BOOTDEVICENAMES "hd#"
+
+#define CONFIG_DYNAMIC_BUFPOOL /* we expand bufpool to overwrite the _DISCARD segment at boot */
+#define NBUFS    5        /* Number of block buffers, keep in line with space reserved in scrumpel.s */
+#define NMOUNTS	 4	  /* Number of mounts at a time */
+
+/* Hardware parameters : internal hardware at 0x00-0x3F */
+#define Z180_IO_BASE       0x00
+
+#define MAX_BLKDEV 3	    /* 2 IDE drives, 1 SD drive */
+
+/* SD via bitbang */
+#define CONFIG_SD
+#define SD_DRIVE_COUNT 1
+#define SD_SPI_CALLTYPE __z88dk_fastcall
+
+#define NUM_DEV_TTY	2
+/* UART0 as the console */
+#define BOOT_TTY (512 + 1)
+#define TTY_INIT_BAUD B115200	/* Hardwired generally */
+
+#define TTYDEV   BOOT_TTY /* Device used by kernel for messages, panics */
+
+/* Z180 does not yet support swap - need to fix that */
+//#define SWAPDEV     (swap_dev)	/* A variable for dynamic, or a device major/minor */
+extern unsigned int swap_dev;
+#define SWAP_SIZE   0x7C 	/* 62K in blocks (prog + udata) */
+#define SWAPBASE    0x0000	/* start at the base of user mem */
+#define SWAPTOP	    0xF800	/* Swap out udata and program */
+#define MAX_SWAPS   16	    	/* We will size if from the partition */
+/* Swap will be set up when a suitably labelled partition is seen */
+#define CONFIG_DYNAMIC_SWAP
+#define swap_map(x)	((uint8_t *)(x))
+
+#define platform_copyright()		// for now
---
+++ Kernel/platform-scrumpel/scrumpel.s
@@ -0,0 +1,258 @@
+;
+;	Scrumpel support code. Based upon code by Will Sowerbutts
+;
+        .module sc111
+        .z180
+
+        ; exported symbols
+        .globl init_early
+        .globl init_hardware
+        .globl inchar
+        .globl outchar
+        .globl platform_interrupt_all
+        .globl _bufpool
+
+        ; imported symbols
+        .globl z180_init_hardware
+        .globl z180_init_early
+        .globl outhl
+        .globl outnewline
+
+        .include "kernel.def"
+        .include "../cpu-z180/z180.def"
+        .include "../kernel-z80.def"
+
+; -----------------------------------------------------------------------------
+; Buffers
+; -----------------------------------------------------------------------------
+        .area _BUFFERS
+_bufpool:
+        .ds (BUFSIZE * 5) ; adjust NBUFS in config.h in line with this
+
+; -----------------------------------------------------------------------------
+; Initialisation code
+; -----------------------------------------------------------------------------
+        .area _DISCARD
+
+init_early:
+	ret
+
+init_hardware:
+        ; enable ASCI interrupts
+        in0 a, (ASCI_STAT0)
+        or #0x08                ; enable ASCI0 receive interrupts
+        out0 (ASCI_STAT0), a
+        in0 a, (ASCI_ASEXT0)
+        and #0x7f               ; disable RDRF interrupt inhibit
+        out0 (ASCI_ASEXT0), a
+        in0 a, (ASCI_STAT1)
+        or #0x08                ; enable ASCI1 receive interrupts
+        out0 (ASCI_STAT1), a
+        in0 a, (ASCI_ASEXT1)
+        and #0x7f               ; disable RDRF interrupt inhibit
+        out0 (ASCI_ASEXT1), a
+
+        jp z180_init_hardware
+
+; -----------------------------------------------------------------------------
+; COMMON MEMORY BANK
+; -----------------------------------------------------------------------------
+        .area _COMMONMEM
+
+; outchar: Wait for UART TX idle, then print the char in A
+; destroys: AF
+outchar:
+        push bc
+        ld b, a
+        ; wait for transmitter to be idle
+ocloop: in0 a, (ASCI_STAT0)
+        bit 1, a        ; and 0x02
+        jr z, ocloop    ; loop while busy
+        ; now output the char to serial port
+        ld a, b
+        out0 (ASCI_TDR0), a
+        pop bc
+        ret
+
+; inchar: Wait for character on UART, return in A
+; destroys: AF
+inchar:
+        in0 a, (ASCI_STAT0)
+        rlca
+        jr nc, inchar
+        in0 a, (ASCI_RDR0)
+        ret
+
+platform_interrupt_all:
+        ret
+
+
+;
+;	Low level SPI bitbanging
+;
+;	Based on the Z80 PIO interface. Assumes that the SPI interface
+;	is wired to an SD card through the relevant voltage shifters and
+;	pullup.
+;
+
+	.globl _sd_spi_transmit_byte
+	; Send the byte in L, received data is ignored
+	.globl _sd_spi_receive_byte
+	; Receive a byte in L, send FF
+	; Optimized block transfer routines
+	.globl _sd_spi_tx_sector
+	.globl _sd_spi_rx_sector
+
+	; Other port bit status
+	.globl _spi_bits
+
+SPI_PORT	.equ	0xA0
+
+SPI_DATA	.equ	1
+SPI_CLOCK	.equ	2
+
+;
+;	The basic idea is that we set up de and hl to bit bang a 0 or 1
+;	c to point to the port so we can use out (c), and b is free
+;	for djnz.
+;
+sd_spi_set_regs:
+	ld a,(_spi_bits)
+	ld e,a
+	ld a,#SPI_DATA
+	or e
+	ld l,a
+	or #SPI_CLOCK
+	ld h,a
+	ld a,e
+	or #SPI_CLOCK
+	ld d,a
+	ld bc, #SPI_PORT
+	ret	
+
+_sd_spi_transmit_byte:
+	ld a,l
+	push af
+	call sd_spi_set_regs
+	pop af
+	call spi0_bitbang_tx
+	out (c),e
+	ret
+
+_sd_spi_receive_byte:
+	call sd_spi_set_regs
+	call spi0_bitbang_rx
+	out(c),e
+	ld l,d
+	ret
+
+_sd_spi_rx_sector:
+	exx
+	call sd_spi_set_regs
+	exx
+	ld b,#0
+spi_rx_loop:
+	exx
+	call spi0_bitbang_rx
+	ld a,d
+	exx
+	ld (hl),a
+	inc hl
+	exx
+	call spi0_bitbang_rx
+	ld a,d
+	exx
+	ld (hl),a
+	inc hl
+	djnz spi_rx_loop
+	exx
+	out (c),e
+	exx
+	ret
+
+_sd_spi_tx_sector:
+	exx
+	call sd_spi_set_regs
+	exx
+	ld b,#0
+spi_tx_loop:
+	ld a,(hl)
+	inc hl
+	exx
+	call spi0_bitbang_tx
+	exx
+	ld a,(hl)
+	inc hl
+	exx
+	call spi0_bitbang_tx
+	exx
+	djnz spi_tx_loop
+	exx
+	out (c),e
+	exx
+	ret
+
+;
+;	Now the scary stuff
+;
+
+spi0_bitbang_tx:
+	ld b,#8
+spi0_bit_tx:
+	rla
+	jp nc, spi0_tx0			; sending 0 or 1 ?
+	out (c),l
+	out (c),h
+	djnz spi0_bit_tx
+	ret
+spi0_tx0:
+	out (c),e
+	out (c),d
+	djnz spi0_bit_tx
+	ret
+
+spi0_bitbang_rx:
+	out (c),l		; 12		low | 1
+	out (c),h		; 12		high | 1
+	in d,(c)		; 12		get bit 0
+	out (c),l		; 12		low | 1
+	out (c),h		; 12		high | 1
+	in a,(c)		; 12
+	rra			; 4
+	rl d			; 8
+	out (c),l		; 12		low | 1
+	out (c),h		; 12		high | 1
+	in a,(c)		; 12		and bit 1
+	rra			; 4
+	rl d			; 8
+	out (c),l		; 12		low | 1
+	out (c),h		; 12		high | 1
+	in a,(c)		; 12		and bit 2
+	rra			; 4
+	rl d			; 8
+	out (c),l		; 12		low | 1
+	out (c),h		; 12		high | 1
+	in a,(c)		; 12		3
+	rra			; 4
+	rl d			; 8
+	out (c),l		; 12		low | 1
+	out (c),h		; 12		high | 1
+	in a,(c)		; 12		4
+	rra			; 4
+	rl d			; 8
+	out (c),l		; 12		low | 1
+	out (c),h		; 12		high | 1
+	in a,(c)		; 12		5
+	rra			; 4
+	rl d			; 8
+	out (c),l		; 12		low | 1
+	out (c),h		; 12		high | 1
+	in a,(c)		; 12		6
+	rra			; 4
+	rl d			; 8
+	out (c),l		; 12		low | 1
+	out (c),h		; 12		high | 1
+	in a,(c)		; 12		7
+	rra			; 4
+	rl d			; 8
+	ret
