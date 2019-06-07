9fe43bcc0a4db37703260ec4aa8f0375d8f2b87f
Alan Cox <alan@linux.intel.com>
2019-06-07 21:29:07 +0100

    rc2014: rules.mk for banked RC2014 kernel image

---
+++ Kernel/platform-rc2014/rules.mk
@@ -0,0 +1,17 @@
+CROSS_CCOPTS += --external-banker
+#
+export BANKED=-banked
+
+export CROSS_CC_SEG1=--codeseg CODE1
+export CROSS_CC_SEG2=--codeseg CODE2
+export CROSS_CC_SEG3=--codeseg CODE1
+export CROSS_CC_SEG4=--codeseg CODE1
+export CROSS_CC_VIDEO=--codeseg CODE2
+#
+export CROSS_CC_SYS1=--codeseg CODE1
+export CROSS_CC_SYS2=--codeseg CODE1
+export CROSS_CC_SYS3=--codeseg CODE2
+export CROSS_CC_SYS4=--codeseg CODE2
+export CROSS_CC_SYS5=--codeseg CODE2
+export CROSS_CC_SEGDISC=--codeseg DISCARD
+
