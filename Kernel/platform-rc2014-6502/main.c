143a8bbf15370e2046a553530befa91a8149d8c8
Alan Cox <alan@linux.intel.com>
2019-06-07 11:43:47 +0100

    rc2014-6502: remove dead swap helper

--- Kernel/platform-rc2014-6502/main.c
+++ Kernel/platform-rc2014-6502/main.c
@@ -41,30 +41,6 @@ uint8_t platform_param(char *p)
 {
     return 0;
 }
-
-/*
- *	This function is called for partitioned devices if a partition is found
- *	and marked as swap type. The first one found will be used as swap. We
- *	only support one swap device.
- */
-void platform_swap_found(uint8_t letter, uint8_t m)
-{
-	blkdev_t *blk = blk_op.blkdev;
-	uint16_t n;
-	if (swap_dev != 0xFFFF)
-		return;
-	letter -= 'a';
-	kputs("(swap) ");
-	swap_dev = letter << 4 | m;
-#ifdef SWAPDEV
-	n = blk->lba_count[m - 1] / SWAP_SIZE;
-	if (n > MAX_SWAPS)
-		n = MAX_SWAPS;
-	while (n)
-		swapmap_init(n--);
-#endif
-}
-
 static volatile uint8_t *via = (volatile uint8_t *)0xC0F0;
 
 void device_init(void)
