3a045a585f9598022c3c55063be3bb8e23b02002
Alan Cox <alan@linux.intel.com>
2019-06-17 22:59:41 +0100

    8085: Put all the core discardables in discard
    Now we sort of trust our tooling turn it on properly.

--- Kernel/cpu-8085/rules.mk
+++ Kernel/cpu-8085/rules.mk
@@ -7,7 +7,7 @@ export CROSS_CCOPTS= -mfuzix -c -O4 -S -I$(ROOT_DIR)/cpu-$(CPU) -I$(ROOT_DIR)/pl
 export CROSS_CC_SEG2=
 export CROSS_CC_SEG3=
 export CROSS_CC_SEG4=
-export CROSS_CC_SEGDISC=
+export CROSS_CC_SEGDISC=-Ras=$(ROOT_DIR)/tools/discard85
 export CROSS_CC_FONT=
 export CROSS_CC_VIDEO=
 export CROSS_CC_SYS1=
