6f3574fa80df61b7d93d111db1d7010c4bc8f12a
Alan Cox <alan@linux.intel.com>
2019-06-30 23:37:51 +0100

    taylormade: First cut at a port to Fuzix
    Still a bit big, and probably we want to reprocess the game with one tool
    and run it in another.

---
+++ Applications/games/taylormade.h
@@ -0,0 +1,6 @@
+extern unsigned char WaitCharacter(void);
+extern void LineInput(char *buf, int len);
+extern void DisplayInit(void);
+extern void TopWindow(void);
+extern void BottomWindow(void);
+extern void PrintCharacter(unsigned char c);
