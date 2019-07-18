75f10884ba03e7286555e4cb519b3a27d4ebd7ca
Alan Cox <alan@linux.intel.com>
2019-07-18 23:35:15 +0100

    tty: add the 'tty' command

---
+++ Applications/V7/cmd/tty.c
@@ -0,0 +1,22 @@
+/* UNIX V7 source code: see /COPYRIGHT or www.tuhs.org for details. */
+
+/*
+ * Type tty name
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	const char *p;
+
+	p = ttyname(0);
+	if(argc == 2 && !strcmp(argv[1], "-s"))
+		;
+	else
+		printf("%s\n", (p? p: "not a tty"));
+	exit(p? 0: 1);
+}
