5e94d321fd41ec8f6f7b8001d9a4a88d0a1fef2d
Alan Cox <alan@linux.intel.com>
2019-09-07 16:51:03 +0100

    micropack: remove the tree
    We have more useful real platforms for such work

--- Kernel/platform-micropack/commonmem.s
+++
@@ -1,9 +0,0 @@
-;
-;	Uarea is not in common on pure swap
-;
-        .module commonmem
-
-        .area _UDATA
-
-	.include "../cpu-z80/std-commonmem.s"
-
--- Kernel/platform-micropack/rules.mk
+++
@@ -1,9 +0,0 @@
-#
-#	Use RST8 for call preamble
-#
-CROSS_CCOPTS += --peep-file $(ROOT_DIR)/cpu-z80/rst8.peep
-#export CROSS_CC_SYS1=--codeseg CODE3
-#export CROSS_CC_SYS2=--codeseg CODE4
-export CROSS_CC_SYS3=--codeseg BANK
-export CROSS_CC_SYS4=--codeseg BANK
-export CROSS_CC_SYS5=--codeseg BANK
