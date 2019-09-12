07bbaf7860fccb854ad3b4b73774552c7ad28461
Alan Cox <alan@linux.intel.com>
2019-09-13 00:52:27 +0100

    easy-z80: config.h type changes

--- Kernel/platform-easy-z80/config.h
+++ Kernel/platform-easy-z80/config.h
@@ -62,7 +62,7 @@
 /* Adjust copy_common if you touch the above */
 
 //#define SWAPDEV     (swap_dev)	/* A variable for dynamic, or a device major/minor */
-extern unsigned int swap_dev;
+extern uint16_t swap_dev;
 #define SWAP_SIZE   0x78 	/* Program + udata in blocks */
 #define SWAPBASE    0x0000	/* start at the base of user mem */
 #define SWAPTOP	    0xF000	/* Swap out udata and program */
