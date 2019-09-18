131bef1253202e22cfbe6f11be7575fb81112879
Alan Cox <alan@linux.intel.com>
2019-09-18 21:24:50 +0100

    rc2014: add an initial zxkeyboard driver
    This doesn't yet work but it puts in the pieces we need to debug and wire into
    the platforms.

---
+++ Kernel/dev/rc2014/zxkeyboard.c
@@ -0,0 +1,56 @@
+/*
+ *	High level support for the ZXKey interface. The real work is done
+ *	in the asm code
+ */
+
+#include <kernel.h>
+#include <kdata.h>
+#include <printf.h>
+#include <stdbool.h>
+#include <keycode.h>
+#include <vt.h>
+#include <tty.h>
+#include <input.h>
+#include <devinput.h>
+#include <zxkey.h>
+
+struct vt_repeat keyrepeat = { 50, 5 };
+
+void zxkey_poll(void)
+{
+    unsigned int r = zxkey_scan();
+    uint8_t c = r;
+
+    if (c == 0)
+        return;
+
+
+    switch(keyboard_grab) {
+    case 0:
+        vt_inproc(1, c);
+        break;
+    case 1:
+        if (!input_match_meta(c)) {
+            vt_inproc(1, c);
+            break;
+        }
+        /* Fall through */
+    case 2:
+        queue_input(KEYPRESS_DOWN);
+        queue_input(c);
+        break;
+    case 3:
+	/* Queue an event giving the base key (unshifted)
+	   and the state of shift/ctrl/alt */
+	queue_input(KEYPRESS_DOWN | (r >> 8));
+//	queue_input(FIXME ... need map and byte/bit);
+    }
+}
+
+/* Helper from the asm code */
+void zxkey_queue_key(uint8_t row, uint8_t col)
+{
+    queue_input(KEYPRESS_UP);
+    queue_input(keyboard[row][col]);
+}
+
