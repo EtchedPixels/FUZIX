2d789ee1ccd14bd667923b1cee2bc23415e1cfaf
Alan Cox <alan@linux.intel.com>
2019-11-06 23:31:40 +0000

    gpio: add library header

---
+++ Library/include/sys/gpio.h
@@ -0,0 +1,28 @@
+#ifndef _GPIO_H
+#define _GPIO_H
+
+struct gpioreq {
+    uint8_t pin;
+    uint8_t val;
+};
+
+struct gpio {
+    uint8_t pinmask;
+    uint8_t group;
+    uint8_t flags;
+#define GPIO_BIT_CONFIG	1	/* Per bit direction setting */
+#define GPIO_GRP_CONFIG	2	/* Per group direction setting */
+    uint8_t wmask;
+    uint8_t wdata;
+    uint8_t imask;
+};
+
+#define GPIOC_SETBYTE	0x0530
+#define GPIOC_SET	0x0531
+#define GPIOC_CLR	0x0532
+#define GPIOC_GETBYTE	0x0533
+#define GPIOC_SETRW	0x0534
+#define GPIOC_GETINFO	0x0535
+#define GPIOC_COUNT	0x0536
+
+#endif
