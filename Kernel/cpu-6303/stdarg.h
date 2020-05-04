cce3908ae8ccdbd4d6592a263bf9d6a364cbc506
Alan Cox <alan@linux.intel.com>
2020-05-04 14:29:24 +0100

    6303: Fix kernel stdarg.h
    To match user fix

--- Kernel/cpu-6303/stdarg.h
+++ Kernel/cpu-6303/stdarg.h
@@ -9,7 +9,7 @@ typedef unsigned char *va_list;
 
 #define __typesize(__type)	(sizeof(__type) == 1 ? sizeof(int) : sizeof(__type))
 
-#define va_start(__ap, __last)	((__ap) = (va_list)(&(__ap)))
+#define va_start(__ap, __last)	((__ap) = (va_list)(&(__last)))
 #define va_end(__ap)
 
 #define va_arg(__ap, __type)	(*((__type *)(void *)(__ap -= __typesize(__type))))
