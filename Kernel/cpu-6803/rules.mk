ffde038a66543daf1b38fff28fdb4237ee0406aa
Alan Cox <alan@linux.intel.com>
2020-05-07 18:00:25 +0100

    cpu-6803: basically the 6303 with a different compiler option

---
+++ Kernel/cpu-6803/rules.mk
@@ -0,0 +1,14 @@
+export CROSS_AS=as68
+export CROSS_LD= ld68
+export CROSS_CC = cc68
+export CROSS_CCOPTS= -m6803 -X -c -I$(ROOT_DIR)/cpu-6303 -I$(ROOT_DIR)/platform-$(TARGET) -I$(ROOT_DIR)/include
+export CROSS_CC_SEG1=
+export CROSS_CC_SEG2=
+export CROSS_CC_SEG3=
+export CROSS_CC_SEGDISC= --code-name discard --rodata-name discard
+export CROSS_CC_VIDEO=
+export ASOPTS=
+export ASMEXT = .s
+export BINEXT = .o
+export BITS=16
+
