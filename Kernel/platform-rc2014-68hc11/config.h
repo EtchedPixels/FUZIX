65112adeaf664de1e9e44f2c71c965148019b60b
Alan Cox <alan@linux.intel.com>
2020-05-07 14:47:46 +0100

    68hc11: add the first pieces of 68HC11 platform support
    There is a lot left to do here but it makes sense to begin building this
    in parallel with the 6303 work. There are not many differences except toolchain
    ones. The 68HC11 has another register, which changes stuff like SWI offsets,
    but otherwise it's mostly toolchain differences (_ on names etc, and globl v
    export etc)

---
+++ Kernel/platform-rc2014-68hc11/config.h
@@ -0,0 +1,64 @@
+/* Enable to make ^Z dump the inode table for debug */
+#undef CONFIG_IDUMP
+/* Enable to make ^A drop back into the monitor */
+#undef CONFIG_MONITOR
+/* Profil syscall support (not yet complete) */
+#undef CONFIG_PROFIL
+/* Acct syscall support */
+#undef CONFIG_ACCT
+/* Multiple processes in memory at once */
+#define CONFIG_MULTI
+
+/*
+ *	Currently we are using the 512K/512K RAM card. We could ue the 8085
+ *	style MMU card and linear RAM and that might be better
+ */
+/* 16K reported page size */
+#define CONFIG_PAGE_SIZE	16
+/* We use flexible 16K banks with a fixed common */
+#define CONFIG_BANK16FC
+#define CONFIG_BANKS	4	/* 4 banks 16K page size */
+#define MAX_MAPS	32
+
+/* Permit large I/O requests to bypass cache and go direct to userspace */
+#define CONFIG_LARGE_IO_DIRECT(x)	1
+
+/* Arguments the other way around */
+#define CONFIG_CALL_R2L
+
+#define TICKSPERSEC 20	    /* Ticks per second */
+
+#define MAPBASE	    0x0000  /* We map from 0 */
+#define PROGBASE    0x0000  /* also data base */
+#define PROGLOAD    0x0100
+#define PROGTOP     0xBE00
+                            /* BE00-BFFF is the udata copy */
+
+#define SWAPDEV     (swap_dev)	/* A variable for dynamic, or a device major/minor */
+extern uint16_t swap_dev;
+#define SWAP_SIZE   0x60 	/* 48K in blocks (prog + udata) */
+#define SWAPBASE    0x0000	/* start at the base of user mem */
+#define SWAPTOP	    0xC000	/* Swap out udata and program */
+#define MAX_SWAPS   16	    	/* We will size if from the partition */
+/* Swap will be set up when a suitably labelled partition is seen */
+#define CONFIG_DYNAMIC_SWAP
+#define swap_map(x)	(x)
+
+#define CONFIG_IDE
+#define MAX_BLKDEV 1
+
+#define BOOT_TTY 513        /* Set this to default device for stdio, stderr */
+
+/* We need a tidier way to do this from the loader */
+#define CMDLINE	NULL	  /* Location of root dev name */
+
+/* Device parameters */
+#define NUM_DEV_TTY 1
+#define TTYDEV   BOOT_TTY /* Device used by kernel for messages, panics */
+#define NBUFS    5        /* Number of block buffers */
+#define NMOUNTS	 2	  /* Number of mounts at a time */
+
+#define platform_discard()
+#define platform_copyright()
+
+#define BOOTDEVICENAMES "hd#"
---
+++ Kernel/platform-rc2014-68hc11/main.c
@@ -0,0 +1,74 @@
+#include <kernel.h>
+#include <timer.h>
+#include <kdata.h>
+#include <printf.h>
+#include <blkdev.h>
+#include <devide.h>
+#include <devtty.h>
+
+uint8_t kernel_flag = 1;
+uint8_t need_resched;
+uint16_t swap_dev = 0xFFFF;
+
+/* Onboard I/O */
+static volatile uint8_t *cpuio = (volatile uint8_t *)0;
+
+void platform_idle(void)
+{
+    irqflags_t flags = di();
+    tty_poll();
+    irqrestore(flags);
+}
+
+void do_beep(void)
+{
+}
+
+
+/* FIXME: route serial interrupts directly in asm to a buffer and just
+   queue process here */
+
+static uint8_t tickmod;
+
+void platform_interrupt(void)
+{
+	uint8_t dummy;
+
+	tty_poll();
+
+	/* Reading the register resets the flag if set */
+	if (cpuio[0x08] & 0x20) {
+		cpuio[9];	/* Read tmer to clear interrupt */
+		tickmod++;
+		if (tickmod == 7)
+			tickmod = 0;
+		/* Turn it into a 20Hz timer. For now do a simple fixup
+		   for the 28->20 and ignore the extra 1% or so error */
+		if (tickmod != 2 && tickmod != 6)
+			timer_interrupt();
+	}
+}
+
+/* For now this and the supporting logic don't handle swap */
+
+extern uint8_t hd_map;
+extern void hd_read_data(uint8_t *p);
+extern void hd_write_data(uint8_t *p);
+
+void devide_read_data(void)
+{
+	if (blk_op.is_user)
+		hd_map = 1;
+	else
+		hd_map = 0;
+	hd_read_data(blk_op.addr);	
+}
+
+void devide_write_data(void)
+{
+	if (blk_op.is_user)
+		hd_map = 1;
+	else
+		hd_map = 0;
+	hd_write_data(blk_op.addr);	
+}
