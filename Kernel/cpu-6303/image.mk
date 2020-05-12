f32d85e6f7426289efc72a09ada61e5ab580e43a
Alan Cox <alan@linux.intel.com>
2020-05-12 01:18:05 +0100

    6303/6803: visualize the build

--- Kernel/cpu-6303/image.mk
+++ Kernel/cpu-6303/image.mk
@@ -1,2 +1,6 @@
-fuzix.bin: target $(OBJS)
+
+tools/visualize6800: tools/visualize6800.c
+
+fuzix.bin: target $(OBJS) tools/visualize6800
 	+make -C platform-$(TARGET) image
+	tools/visualize6800 <fuzix.map
