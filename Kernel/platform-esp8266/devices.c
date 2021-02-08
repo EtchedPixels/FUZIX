34861ec42d9b3d272ee3a93acb2eb033e58f125d
David Given <dg@cowlark.com>
2021-02-08 22:39:08 +0100

    Register our own fatal exception handlers.

--- Kernel/platform-esp8266/devices.c
+++ Kernel/platform-esp8266/devices.c
@@ -5,6 +5,8 @@
 #include <blkdev.h>
 #include <tty.h>
 #include <devtty.h>
+#include <dev/devsd.h>
+#include <printf.h>
 #include "globals.h"
 #include "rom.h"
 
@@ -54,6 +56,21 @@ void syscall_handler(struct __exception_frame* ef, int cause)
 	ef->epc += 3; /* skip SYSCALL instruction */
 }
 
+static void fatal_exception_cb(struct __exception_frame* ef, int cause)
+{
+	di();
+	uint32_t vaddr;
+	asm volatile ("rsr.excvaddr %0" : "=a" (vaddr));
+
+	kprintf("FATAL EXCEPTION %d @ %p with %p:\n", cause, ef->epc, vaddr);
+	kprintf("   a0=%p  sp=%p  a2=%p  a3=%p\n", ef->a0, ef->sp, ef->a2, ef->a3);
+	kprintf("   a4=%p  a5=%p  a6=%p  a7=%p\n", ef->a4, ef->a5, ef->a6, ef->a7);
+	kprintf("   a8=%p  a9=%p a10=%p a11=%p\n", ef->a8, ef->a9, ef->a10, ef->a11);
+	kprintf("  a12=%p a13=%p a14=%p a15=%p\n", ef->a12, ef->a13, ef->a14, ef->a15);
+	for (;;)
+		;
+}
+
 static void timer_isr(void* user, struct __exception_frame* ef)
 {
 	const uint32_t clocks_per_tick = (CPU_CLOCK*1000000) / TICKSPERSEC;
@@ -75,6 +92,11 @@ void device_init(void)
 	flash_dev_init();
 	sd_rawinit();
 	devsd_init();
+
+	static const uint8_t fatal_exceptions[] =
+		{ 0, 2, 3, 5, 6, 8, 9, 20, 28, 29 };
+	for (int i=0; i<sizeof(fatal_exceptions)/sizeof(*fatal_exceptions); i++)
+		_xtos_set_exception_handler(fatal_exceptions[i], fatal_exception_cb);
 
 	extern fn_c_exception_handler_t syscall_handler_trampoline;
 	_xtos_set_exception_handler(1, syscall_handler_trampoline);
