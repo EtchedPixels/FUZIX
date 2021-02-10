14b82e72bc308997b512a8191e920bf8c9de7a87
David Given <dg@cowlark.com>
2021-02-11 00:31:31 +0100

    Blink Hello, World works.

---
+++ Kernel/platform-rpipico/main.c
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main(void) {
+	stdio_init_all();
+
+    const uint LED_PIN = 25;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+		puts("Hello, world!");
+
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+    }
+}
