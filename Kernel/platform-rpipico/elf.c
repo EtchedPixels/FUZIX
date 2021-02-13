f50e1fdd302cf9f5131e8089ce622f5fb90275a6
David Given <dg@cowlark.com>
2021-02-13 13:39:33 +0100

    Implement a basic ELF PIE executable loader. Binaries load, and run up to a swi instruction.

---
+++ Kernel/platform-rpipico/elf.c
@@ -0,0 +1,26 @@
+#include <kernel.h>
+#include <kdata.h>
+#include <printf.h>
+#include <stdbool.h>
+#include <elf.h>
+
+int platform_relocate_rel(Elf32_Rel* rel, uaddr_t section_base)
+{
+	switch (ELF32_R_TYPE(rel->r_info))
+	{
+		case R_ARM_RELATIVE:
+		{
+			uaddr_t addr = section_base + rel->r_offset;
+			uaddr_t value = ugetp((void*) addr);
+			value += section_base;
+			uputp(value, (void*) addr);
+			break;
+		}
+
+		default:
+			kprintf("unsupported ARM relocation %d\n", ELF32_R_TYPE(rel->r_info));
+			return 1;
+	}
+	return 0;
+}
+
