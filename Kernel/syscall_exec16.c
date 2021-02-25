a9bed2f930703e5df3ccd08047a46f0d09c40edf
David Given <dg@cowlark.com>
2021-02-25 21:27:35 +0100

    Remove u_top in favour of p_top (there should only be a single source of truth for this).

--- Kernel/syscall_exec16.c
+++ Kernel/syscall_exec16.c
@@ -149,7 +149,6 @@ arg_t _execve(void)
 	else
 		udata.u_flags &= ~U_FLAG_NOCORE;
 #endif
-	udata.u_top = top;
 	udata.u_ptab->p_top = top;
 
 	/* setuid, setgid if executable requires it */
--- Kernel/usermem.c
+++ Kernel/usermem.c
@@ -19,10 +19,10 @@ usize_t valaddr(const uint8_t *base, usize_t size)
 	if (base + size < base)
 		size = MAXUSIZE - (usize_t)base + 1;
 	if (!base || base < (const uint8_t *)PROGBASE ||
-		base > (const uint8_t *)(size_t)udata.u_top)
+		base > (const uint8_t *)(size_t)udata.u_ptab->p_top)
 		size = 0;
-	else if (base + size > (const uint8_t *)(size_t)udata.u_top)
-		size = (uint8_t *)(size_t)udata.u_top - base;
+	else if (base + size > (const uint8_t *)(size_t)udata.u_ptab->p_top)
+		size = (uint8_t *)(size_t)udata.u_ptab->p_top - base;
 	if (size == 0)
 		udata.u_error = EFAULT;
 	return size;
