0971e6f86229ce733bb8a5e591be43c03d82fdf6
David Given <dg@cowlark.com>
2021-03-03 22:24:46 +0100

    Enable signal delivery (which turned out to be really easy).

--- Kernel/lowlevel-armm0.c
+++ Kernel/lowlevel-armm0.c
@@ -3,15 +3,25 @@
 #include <printf.h>
 #include <stdarg.h>
 
-/* Called with interrupts off and on the user stack, immediately before
- * returning into the user program. A signal may longjmp out, so this may not
- * return. */
+/* Called on return from system calls while on the user stack, immediately
+ * before returning into the user program. A signal may longjmp out, so this
+ * may not return. */
 void deliver_signals(void)
 {
-	uint8_t cursig = udata.u_cursig;
-	if (cursig)
+	for (;;)
 	{
-		panic("signals not supported yet");
+		uint8_t cursig = udata.u_cursig;
+		if (!cursig)
+			break;
+
+		typedef int (*sigvec_fn)(int);
+		sigvec_fn fn = udata.u_sigvec[cursig];
+
+		/* Semantics for now: signal delivery clears handler. */
+		udata.u_sigvec[cursig] = 0;
+		udata.u_cursig = 0;
+
+		fn(cursig);
 	}
 }
 
