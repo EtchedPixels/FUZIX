84dad16a81c4ea4bc7c663b285c302fafb65d77d
Alan Cox <alan@linux.intel.com>
2021-05-13 20:41:14 +0100

    newfile: rearrange to keep 68HC11 compiler happy
    The order of the two checks is not that important and it works the other
    way around.

--- Kernel/filesys.c
+++ Kernel/filesys.c
@@ -490,14 +490,16 @@ inoptr newfile(inoptr pino, uint8_t *name)
         goto nogood;
     }
 
-    /* First see if parent is writeable */
-    if (pino->c_flags & CRDONLY) {
-        udata.u_error = EROFS;
+    /* We check getperm before CRDONLY because if you reverse these two
+       it breaks gcc 68hc11 3.4 */
+    if (!(getperm(pino) & OTH_WR)) {
+        udata.u_error = EPERM;
         goto nogood;
     }
 
-    if (!(getperm(pino) & OTH_WR)) {
-        udata.u_error = EPERM;
+    /* First see if parent is writeable */
+    if (pino->c_flags & CRDONLY) {
+        udata.u_error = EROFS;
         goto nogood;
     }
 
