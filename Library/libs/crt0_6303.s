e2359261d7bd190118e1a6d200858f3404772564
Alan Cox <alan@linux.intel.com>
2021-06-12 20:06:45 +0100

    6803/6303: initial cut at signal support code

--- Library/libs/crt0_6303.s
+++ Library/libs/crt0_6303.s
@@ -6,6 +6,7 @@
 	.code
 
 	.export _environ
+	.export head
 
 head:
 	.word	$80A8
@@ -23,7 +24,70 @@ head:
 
 	.word   __sighandler	;	TODO: signals
 
+;
+;	This function is called when we need to deliver a signal. We can't
+;	just blindly stack stuff as we can on big machines because we have
+;	non-reentrancy issues in the compiler temporary and regvar usage
+;
+;	On entry
+;	D = signal number
+;	X = undefined
+;
+;	Return address is the correct route back to the kernel. Above it is
+;	a copy of the vector and an RTI frame.
+;
 __sighandler:
+	; Save compiler temporaries and dp register variables
+	ldx	@tmp
+	pshx
+	ldx	@tmp1
+	pshx
+	ldx	@tmp2
+	pshx
+	ldx	@tmp3
+	pshx
+	ldx	@tmp4
+	pshx
+	ldx	@fp
+	pshx
+	ldx	@reg
+	pshx
+	ldx	@reg+2
+	pshx
+	ldx	@reg+4
+	pshx
+	ldx	@sreg
+	pshx
+	; Arguments signal number and frame pointer
+	pshb	; Save the signal number
+	psha
+	tsx
+	pshx
+	ldx	26,x	; vector
+	jsr	,x
+	; Discard arguments
+	pulx
+	pulx
+	pulx
+	; Restore temporaries
+	stx	@sreg
+	pulx
+	stx	@reg+4
+	pulx
+	stx	@reg+2
+	pulx
+	stx	@reg
+	pulx
+	stx	@fp
+	pulx
+	stx	@tmp4
+	pulx
+	stx	@tmp3
+	pulx
+	stx	@tmp2
+	pulx
+	stx	@tmp
+	; Back to kernel provided address
 	rts
 
 start:
