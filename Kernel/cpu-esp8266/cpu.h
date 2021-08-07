2ab761f8562212f8711870c8084d1761632ea544
Alan Cox <alan@linux.intel.com>
2021-08-07 01:06:44 +0100

    esp8266: remove legacy magic numbers

--- Kernel/cpu-esp8266/cpu.h
+++ Kernel/cpu-esp8266/cpu.h
@@ -58,10 +58,6 @@ typedef union {            /* this structure is endian dependent */
 		wsr %0, ps; isync" \
 		:: "a" (ps) : "memory")
 	
-/* jmp over the Fuzix header. Will need updating if the header size changes */
-#define EMAGIC   0x08
-#define EMAGIC_2 0x3c
-
 #define no_cache_udata()
 
 #define CPUTYPE	CPUTYPE_LX106
