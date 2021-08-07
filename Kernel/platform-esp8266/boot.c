9037cb0e29c277287a5a2216f193450f6a027976
Alan Cox <alan@linux.intel.com>
2021-08-07 01:07:28 +0100

    esp8266: this stuff isn't it turns out too portable

--- Kernel/platform-esp8266/boot.c
+++ Kernel/platform-esp8266/boot.c
@@ -7,23 +7,22 @@ extern int main(void);
 void __main(void)
 {
 	/* See globals.h for the clock speeds. */
-
 	/* Configure peripheral clock. */
-
+#if 0
 	rom_i2c_writeReg(103, 4, 1, 0x88);
 	//rom_i2c_writeReg(103, 4, 2, 0x81); /* 189MHz */
 	rom_i2c_writeReg(103, 4, 2, 0x91); /* 80MHz */
 
 	/* Configure CPU clock. */
-
 	ets_update_cpu_frequency(CPU_CLOCK);
+#endif
 	uart_div_modify(0, (PERIPHERAL_CLOCK * 1e6) / 115200);
-
+#if 0
 	/* Enable SPI flash mapping. */
-
 	SpiFlashCnfig(5, 0);
 	SpiReadModeCnfig(5);
 	Wait_SPI_Idle(sdk_flashchip);
+#endif
 	Cache_Read_Enable(0, 0, 1);
 
 	/* Clear BSS. */
