82edb8b1e80c7ae31d9052b6553550f7ff61f7a7
Alan Cox <alan@linux.intel.com>
2021-08-07 01:08:24 +0100

    esp8266: move to the standard make diskimage layout
    We also move to the dynamic swap handling

--- Kernel/platform-esp8266/config.h
+++ Kernel/platform-esp8266/config.h
@@ -54,8 +54,10 @@ extern uint8_t _code_top[];
 /* We need a tidier way to do this from the loader */
 #define CMDLINE	NULL	  /* Location of root dev name */
 
-#define BOOTDEVICE 0x0012 /* hdb2 */
-#define SWAPDEV    0x0011 /* hdb1 */
+#define BOOTDEVICE 0x0011 /* hdb1 */
+#define SWAPDEV    (swap_dev) /* wherever */
+
+#define CONFIG_DYNAMIC_SWAP
 
 /* Device parameters */
 #define NUM_DEV_TTY 1
