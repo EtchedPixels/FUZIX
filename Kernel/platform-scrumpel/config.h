f40327b42bde3089f5c283a9d0762fa789a1ea8b
Alan Cox <alan@linux.intel.com>
2021-08-15 16:19:12 +0100

    scrumpel: turn on Z180 swapping support
    For now turn off networking until the rework is finished

--- Kernel/platform-scrumpel/config.h
+++ Kernel/platform-scrumpel/config.h
@@ -7,12 +7,12 @@
 /* Multiple processes in memory at once */
 #define CONFIG_MULTI
 /* Fixed banking: 8 x 64K banks, top 4KB is shared with kernel, 60KB-62KB is user memory  */
-#define CONFIG_BANK_FIXED
+#define CONFIG_BANK_SPLIT
 /* Permit large I/O requests to bypass cache and go direct to userspace */
 #define CONFIG_LARGE_IO_DIRECT(x)	1
 /* 8 60K banks, 1 is kernel */
 #define MAX_MAPS	8
-#define MAP_SIZE	PROGTOP    /* WRS: I feel this should be 60KB, but setting it so breaks pagemap_realloc() when exec calls it */
+#define MAP_SPLIT	PROGTOP
 
 /* Banks as reported to user space */
 #define CONFIG_BANKS	1
@@ -20,7 +20,7 @@
 #define TICKSPERSEC 40U     /* Ticks per second */
 #define PROGBASE    0x0000  /* also data base */
 #define PROGLOAD    0x0100  /* also data base */
-#define PROGTOP     0xF600  /* Top of program, base of U_DATA */
+#define PROGTOP     0xF400  /* Top of program, base of U_DATA */
 #define KERNTOP     0xF000  /* Kernel has lower 60KB */
 #define PROC_SIZE   64      /* Memory needed per process */
 
@@ -34,10 +34,6 @@
 #define CONFIG_DYNAMIC_BUFPOOL /* we expand bufpool to overwrite the _DISCARD segment at boot */
 #define NBUFS    5        /* Number of block buffers, keep in line with space reserved in scrumpel.s */
 #define NMOUNTS	 4	  /* Number of mounts at a time */
-
-/* Enable networking and netd support */
-#define CONFIG_NET
-#define CONFIG_NET_NATIVE
 
 /* Hardware parameters : internal hardware at 0x00-0x3F */
 #define Z180_IO_BASE       0x00
@@ -57,14 +53,14 @@
 #define TTYDEV   BOOT_TTY /* Device used by kernel for messages, panics */
 
 /* Z180 does not yet support swap - need to fix that */
-//#define SWAPDEV     (swap_dev)	/* A variable for dynamic, or a device major/minor */
+#define SWAPDEV     (swap_dev)	/* A variable for dynamic, or a device major/minor */
 extern uint16_t swap_dev;
 #define SWAP_SIZE   0x7C 	/* 62K in blocks (prog + udata) */
 #define SWAPBASE    0x0000	/* start at the base of user mem */
-#define SWAPTOP	    0xF800	/* Swap out udata and program */
+#define SWAPTOP	    0xF600	/* Swap out udata and program */
 #define MAX_SWAPS   16	    	/* We will size if from the partition */
 /* Swap will be set up when a suitably labelled partition is seen */
-//#define CONFIG_DYNAMIC_SWAP
+#define CONFIG_DYNAMIC_SWAP
 #define swap_map(x)	((uint8_t *)(x))
 
 #define platform_copyright()		// for now
