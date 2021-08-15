4f0cd0186555f19998f4e9122ac5bf2580b978d4
Alan Cox <alan@linux.intel.com>
2021-08-15 13:39:26 +0100

    z180: add map_kernel_restore to the default map helpers
    It's just another name for map_kernel in our case, but is needed so we can get all the drivers for
    banked and non-banked unified

--- Kernel/cpu-z180/z180.s
+++ Kernel/cpu-z180/z180.s
@@ -32,6 +32,7 @@
         .globl _dofork
 	.globl map_buffers
         .globl map_kernel
+	.globl map_kernel_restore
         .globl map_process_always
         .globl map_kernel_di
         .globl map_process_always_di
@@ -805,6 +806,7 @@ switchinfail:
         call outstring
         jp _platform_monitor
 
+map_kernel_restore:
 map_buffers:
 map_kernel_di:
 map_kernel: ; map the kernel into the low 60K, leaves common memory unchanged
