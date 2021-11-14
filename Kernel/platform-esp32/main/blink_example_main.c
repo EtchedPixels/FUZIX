f728be8e0db7267b860b4ea799503dbbccffc048
David Given <dg@cowlark.com>
2021-11-14 20:06:04 +0100

    Attempt to move IDF out of the memory areas we want to use --- which fails.

--- Kernel/platform-esp32/main/blink_example_main.c
+++ Kernel/platform-esp32/main/blink_example_main.c
@@ -13,40 +13,16 @@
 #include "esp_log.h"
 #include "led_strip.h"
 #include "sdkconfig.h"
+#include "soc/soc_memory_layout.h"
+
+SOC_RESERVE_MEMORY_REGION(0x40080000, 0x400a0000, sram0);
+SOC_RESERVE_MEMORY_REGION(0x3ffc0000, 0x3ffe0000, sram2);
 
 static const char *TAG = "example";
 
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 
 static uint8_t s_led_state = 0;
-
-#ifdef CONFIG_BLINK_LED_RMT
-static led_strip_t *pStrip_a;
-
-static void blink_led(void)
-{
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        pStrip_a->set_pixel(pStrip_a, 0, 16, 16, 16);
-        /* Refresh the strip to send data */
-        pStrip_a->refresh(pStrip_a, 100);
-    } else {
-        /* Set all LED off to clear all pixels */
-        pStrip_a->clear(pStrip_a, 50);
-    }
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
-    /* LED strip initialization with the GPIO and pixels number*/
-    pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 1);
-    /* Set all LED off to clear all pixels */
-    pStrip_a->clear(pStrip_a, 50);
-}
-
-#elif CONFIG_BLINK_LED_GPIO
 
 static void blink_led(void)
 {
@@ -61,8 +37,6 @@ static void configure_led(void)
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
-
-#endif
 
 void app_main(void)
 {
