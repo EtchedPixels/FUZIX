fc31aa7c04a0f3bdc3ed82103959750a2825dfc6
Alan Cox <etchedpixels@gmail.com>
2022-02-03 16:13:16 +0000

    libs: Fix some confused ordering caused by using bits of multiple maths libs

--- Library/libs/copysign.c
+++ Library/libs/copysign.c
@@ -6,8 +6,8 @@
 double copysign(double x, double y) {
 	uint32_t hi;
 	uint32_t his;
-	GET_HIGH_WORD(x, hi);
-	GET_HIGH_WORD(y, his);
+	GET_HIGH_WORD(hi, x);
+	GET_HIGH_WORD(his, y);
 	SET_HIGH_WORD(x, (hi & 0x7FFFFFFFUL) | (his & 0x80000000UL));
 	return x;
 }
--- Library/libs/fabs.c
+++ Library/libs/fabs.c
@@ -7,7 +7,7 @@ double fabs(double x)
 {
 	uint32_t hi;
 
-	GET_HIGH_WORD(x, hi);
+	GET_HIGH_WORD(hi, x);
 	hi &= 0x7FFFFFFFUL;
 	SET_HIGH_WORD(x, hi);
 	return x;
