14e2f03815bb4364a71d10a29e57fa13be017b47
Alan Cox <etchedpixels@gmail.com>
2022-02-04 20:17:04 +0000

    6809; first cut at relocation for 6809

--- Library/libs/crt0nostdio_6809_rel.s
+++ Library/libs/crt0nostdio_6809_rel.s
@@ -24,12 +24,65 @@ start:
 
 		.dw 0				; Patched for reloc ptr
 
+		; We can be at any page aligned address but our base
+		; is passed in Y
+
+		tfr y,x				; Base into both
+		ldd 16,x			; Relocation offset from
+						; our header
+		leay d,y			; To relocation base
+		pshs y				; Save for brk fix
+
+		tfr x,d				; Base into D so we can get
+						; the high byte for
+						; relocating into A
+		;
+		; A is the relocation amount in pages
+		; B is scratch
+		; U is not used
+		; X is the binary as we walk it relocating
+		; Y is the relocation table pointer
+		;
+relocnext:
+		ldb ,y				; Relocation byte
+		clr ,y				; Turn into BSS
+		tstb				; 0 is end marker
+		beq  relocdone
+		cmpb #255			; 255 is a long skip
+		beq reloc254
+		abx				; 1-254 is that many
+						; bytes on and relocate
+		tfr a,b				; Shuffle to keep the
+		addb ,x				; A value unchanged
+		stb ,x				; Relocate the byte
+		bra relocnext
+
+reloc254:	; 255 means move on 254 but do not relocate
+		decb				; We know B is 255
+		abx
+		bra relocnext
+
+		;
+		; Correct the brk base of the binary as we can now discard
+		; the relocation table from memory if it grew the binary
+		; size
+		;
+
+relocdone:
+		; Fix up the BSS base
+		puls x				; BSS base
+		ldd #30				; brk(x)
+		swi				; and syscall
+		;
+		;  This jmp was relocated by the relocation loop above
+		;
 		jmp start2
 
 		.area .text
 
 start2:
 		; we don't clear BSS since the kernel already did
+
 		; pass environ, argc and argv to main
 		; pointers and data stuffed above stack by execve()
 		leax 4,s
