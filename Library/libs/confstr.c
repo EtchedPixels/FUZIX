c344fe71caa512b749d0dad0dec3eaae12d0e9ac
Alan Cox <etchedpixels@gmail.com>
2022-02-07 12:36:15 +0000

    confstr: fix new changes

--- Library/libs/confstr.c
+++ Library/libs/confstr.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 
 size_t confstr(int name, char *buf, size_t len)
 {
   if (name == _CS_PATH)
-    return strlcpy(buf, len, "/bin:/usr/bin");
+    return strlcpy(buf, "/bin:/usr/bin", len);
   errno = EINVAL;
   return 0;
 }
