66cc83649e51ed9b4ad0abcd73c3cc7db027feff
Alan Cox <etchedpixels@gmail.com>
2022-04-05 02:18:06 +0100

    ps2: allow the helpers to be asm or not

--- Kernel/dev/ps2kbd.h
+++ Kernel/dev/ps2kbd.h
@@ -1,8 +1,12 @@
+#ifndef PS2FASTCALL
+#define PS2FASTCALL
+#endif
+
 extern int ps2kbd_init(void);
 extern void ps2kbd_byte(uint_fast8_t byte);
 
 extern uint16_t ps2kbd_get(void);
-extern int ps2kbd_put(uint8_t c) __fastcall;
+extern int ps2kbd_put(uint8_t c) PS2FASTCALL;
 
 extern void ps2kbd_poll(void);
 extern void ps2kbd_conswitch(uint8_t con);
--- Kernel/dev/ps2mouse.h
+++ Kernel/dev/ps2mouse.h
@@ -1,11 +1,17 @@
+
+#ifndef PS2FASTCALL
+#define PS2FASTCALL
+#endif
+
 /* Provided by transport layer */
 extern uint16_t ps2mouse_get(void);
-extern int ps2mouse_put(uint8_t c) __fastcall;
+extern int ps2mouse_put(uint8_t c) PS2FASTCALL;
 
 /* Set up a PS/2 mouse */
 extern int ps2mouse_init(void);
 /* Poll the mouse for data */
 extern void ps2mouse_poll(void);
+extern void ps2mouse_byte(uint8_t c);
 
 /* Platform provided */
 extern void platform_ps2mouse_event(uint8_t *event);
