d846b5134190c00ce98066fea22a6cfdf5a85828
Alan Cox <etchedpixels@gmail.com>
2022-06-02 00:45:27 +0100

    start: update copyright

--- Kernel/start.c
+++ Kernel/start.c
@@ -354,7 +354,7 @@ void fuzix_main(void)
 			"Copyright (c) 1988-2002 by H.F.Bower, D.Braun, S.Nitschke, H.Peraza\n"
 			"Copyright (c) 1997-2001 by Arcady Schekochikhin, Adriano C. R. da Cunha\n"
 			"Copyright (c) 2013-2015 Will Sowerbutts <will@sowerbutts.com>\n"
-			"Copyright (c) 2014-2021 Alan Cox <alan@etchedpixels.co.uk>\nDevboot\n",
+			"Copyright (c) 2014-2022 Alan Cox <alan@etchedpixels.co.uk>\nDevboot\n",
 			sysinfo.uname);
 
 	set_cpu_type();
