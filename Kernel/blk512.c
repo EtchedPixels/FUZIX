7eda2a9823c8f410fef5d631ba09deb48da05fd6
Alan Cox <etchedpixels@gmail.com>
2022-06-11 14:22:51 +0100

    kernel: add pieces split out for 512 byte specific file systems

---
+++ Kernel/blk512.c
@@ -0,0 +1,127 @@
+#include "kernel.h"
+
+#if (BLKSIZE == 512)
+
+/*
+ *	File system routines for the usual 512 byte block size
+ */
+
+/* Return the number of blocks an inode occupies assuming all blocks present */
+blkno_t inode_blocks(inoptr i)
+{
+    return (i->c_node.i_size + BLKMASK) >> BLKSHIFT;
+}
+
+/* Read an inode */
+uint_fast8_t breadi(uint16_t dev, uint16_t ino, void *ptr)
+{
+    struct blkbuf *buf = bread(dev, (ino >> 3) + 2, 0);
+    if (buf == NULL)
+        return 1;
+    blktok(ptr, buf, 64 * (ino & 7), 64);
+    brelse(buf);
+    return 0;
+}
+
+/* Write an inode */
+uint_fast8_t bwritei(inoptr ino)
+{
+    blkno_t blkno = (ino->c_num >> 3) + 2;
+    struct blkbuf *buf = bread(ino->c_dev, blkno, 0);
+    if (buf == NULL)
+        return 1;
+    blkfromk(&ino->c_node, buf, sizeof(struct dinode) * (ino->c_num & 0x07),
+            sizeof(struct dinode));
+    bfree(buf, 2);
+    return 0;
+}
+
+/*
+ * Bmap defines the structure of file system storage by returning
+ * the physical block number on a device given the inode and the
+ * logical block number in a file.  The block is zeroed if created.
+ */
+blkno_t bmap(inoptr ip, blkno_t bn, unsigned int rwflg)
+{
+    int i;
+    bufptr bp;
+    int j;
+    blkno_t nb;
+    int sh;
+    uint16_t dev;
+
+    if(getmode(ip) == MODE_R(F_BDEV))
+        return(bn);
+
+    dev = ip->c_dev;
+
+    /* blocks 0..17 are direct blocks
+    */
+    if(bn < 18) {
+        if(nb == 0) {
+            if(rwflg ||(nb = blk_alloc(dev))==0)
+                return(NULLBLK);
+            ip->c_node.i_addr[bn] = nb;
+            ip->c_flags |= CDIRTY;
+        }
+        return(nb);
+    }
+
+    /* TODO BLKSIZE: this assumes 512 byte blocks */
+
+    /* addresses 18 and 19 have single and double indirect blocks.
+     * the first step is to determine how many levels of indirection.
+     */
+    bn -= 18;
+    sh = 0;
+    j = 2;
+    if(bn & 0xff00){       /* bn > 255  so double indirect */
+        sh = 8;
+        bn -= 256;
+        j = 1;
+    }
+
+    /* fetch the address from the inode
+     * Create the first indirect block if needed.
+     */
+    if(!(nb = ip->c_node.i_addr[20-j]))
+    {
+        if(rwflg || !(nb = blk_alloc(dev)))
+            return(NULLBLK);
+        ip->c_node.i_addr[20-j] = nb;
+        ip->c_flags |= CDIRTY;
+    }
+
+    /* fetch through the indirect blocks
+    */
+    for(; j<=2; j++) {
+        bp = bread(dev, nb, 0);
+        if (bp == NULL) {
+            corrupt_fs(ip->c_dev);
+            return 0;
+        }
+        /******
+          if(bp->bf_error) {
+          brelse(bp);
+          return((blkno_t)0);
+          }
+         ******/
+        i = (bn >> sh) & 0xff;
+        nb = *(blkno_t *)blkptr(bp, (sizeof(blkno_t)) * i, sizeof(blkno_t));
+        if (nb)
+            brelse(bp);
+        else
+        {
+            if(rwflg || !(nb = blk_alloc(dev))) {
+                brelse(bp);
+                return(NULLBLK);
+            }
+            blkfromk(&nb, bp, i * sizeof(blkno_t), sizeof(blkno_t));
+            bawrite(bp);
+        }
+        sh -= 8;
+    }
+    return(nb);
+}
+
+#endif
---
+++ Kernel/include/blk512.h
@@ -0,0 +1,12 @@
+/*
+ *	Defines for the usual 512 byte file system layout
+ */
+
+#define BLKSIZE		512
+#define INO_PER_BLOCK	8
+#define BLKSHIFT	9
+#define BLOCK(x)	((x) >> BLKSHIFT)
+#define BLKMASK		511
+
+/* Help the 8bit compilers out by preventing any 32bit promotions */
+#define BLKOFF(x)	(((uint16_t)(x)) & BLKMASK)
