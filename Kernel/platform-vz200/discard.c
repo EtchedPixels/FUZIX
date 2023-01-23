460e10dfb8dcf8905d2fd81478943fcadb151d71
Alan Cox <etchedpixels@gmail.com>
2023-01-23 01:06:13 +0000

    vz200: SDHC fixes

--- Kernel/platform-vz200/discard.c
+++ Kernel/platform-vz200/discard.c
@@ -13,7 +13,7 @@ void map_init(void)
 
 void device_init(void)
 {
-	sd_shift[0] = *((uint8_t *)0x89FF) == 3 ? 1 : 9;
+	sd_shift[0] = *((uint8_t *)0x89FF) == 3 ? 0 : 9;
 	sd_spi_clock(1);
 	/* Install a single tinysd interface */
 	td_register(sd_xfer, 1);
