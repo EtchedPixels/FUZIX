5f2f76e8be4a6c7cc3c0af0a2c5d3904f40661cb
Alan Cox <etchedpixels@gmail.com>
2023-01-28 21:53:53 +0000

    sbcv2: switch to the common fast usermem

--- Kernel/platform-sbcv2/tricks.s
+++ Kernel/platform-sbcv2/tricks.s
@@ -23,6 +23,12 @@
 ;
 	.include "../lib/z80fixedbank-core.s"
 
+;	We can also use the standard fast user copiers because all the
+;	kernel objects we need to copy to/from userspace exist in the
+;	user mapping.
+;
+	.include "../lib/z80user1.s"
+
 ;
 ;	We want to provide our own optimized direct 32K bank to bank
 ;	copy. This is slightly crazy stuff. The fastest Z80 copy is to use
@@ -118,144 +124,4 @@ spcache:
 copyct:
 	.byte 0
 
-;
-;	The second set of very performance sensitive routines are accesses
-;	to user space. We thus provide our own modified versions of these
-;	for speed
-;
-;	This works because user space occupies 0000-7FFF and we carefully
-;	pack the kernel up so that only kernel code and vectors live below
-;	8000. In other words there is no case where we need to copy between
-;	the low 32K of user and the low 32K of kernel. This in turn means
-;	we know we can always map it all and ldir.
-;
 
-        ; exported symbols
-        .globl __uget
-        .globl __ugetc
-        .globl __ugetw
-
-	.globl outcharhex
-	.globl outhl
-
-        .globl __uput
-        .globl __uputc
-        .globl __uputw
-        .globl __uzero
-
-	.globl  map_process_always
-	.globl  map_kernel
-;
-;	We need these in common as they bank switch
-;
-        .area _COMMONMEM
-
-;
-;	The basic operations are copied from the standard one. Only the
-;	blk transfers are different. uputget is a bit different as we are
-;	not doing 8bit loop pairs.
-;
-uputget:
-        ; load DE with the byte count
-        ld c, 8(ix) ; byte count
-        ld b, 9(ix)
-	ld a, b
-	or c
-	ret z		; no work
-        ; load HL with the source address
-        ld l, 4(ix) ; src address
-        ld h, 5(ix)
-        ; load DE with destination address (in userspace)
-        ld e, 6(ix)
-        ld d, 7(ix)
-	ret	; 	Z is still false
-
-__uputc:
-	pop bc	;	return
-	pop de	;	char
-	pop hl	;	dest
-	push hl
-	push de
-	push bc
-	call map_process_always
-	ld (hl), e
-uputc_out:
-	jp map_kernel			; map the kernel back below common
-
-__uputw:
-	pop bc	;	return
-	pop de	;	word
-	pop hl	;	dest
-	push hl
-	push de
-	push bc
-	call map_process_always
-	ld (hl), e
-	inc hl
-	ld (hl), d
-	jp map_kernel
-
-;
-;	ugetc and ugetw are fastcall so the address we need is already in
-;	HL
-;
-__ugetc:
-	call map_process_always
-        ld l, (hl)
-	ld h, #0
-	jp map_kernel
-
-__ugetw:
-	call map_process_always
-        ld a, (hl)
-	inc hl
-	ld h, (hl)
-	ld l, a
-	jp map_kernel
-
-__uput:
-	push ix
-	ld ix, #0
-	add ix, sp
-	call uputget			; source in HL dest in DE, count in BC
-	jr z, uput_out			; but count is at this point magic
-	call map_process_always
-	ldir
-uput_out:
-	call map_kernel
-	pop ix
-	ld hl, #0
-	ret
-
-__uget:
-	push ix
-	ld ix, #0
-	add ix, sp
-	call uputget			; source in HL dest in DE, count in BC
-	jr z, uput_out			; but count is at this point magic
-	call map_process_always
-	ldir
-	jr uput_out
-
-;
-__uzero:
-	pop de	; return
-	pop hl	; address
-	pop bc	; size
-	push bc
-	push hl	
-	push de
-	ld a, b	; check for 0 copy
-	or c
-	ret z
-	call map_process_always
-	ld (hl), #0
-	dec bc
-	ld a, b
-	or c
-	jp z, uputc_out
-	ld e, l
-	ld d, h
-	inc de
-	ldir
-	jp uputc_out
