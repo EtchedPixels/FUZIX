ecffc682021b434b9eb0aef7ee6f2ba4339099bf
Alan Cox <etchedpixels@gmail.com>
2023-01-31 21:16:26 +0000

    ds1302: rename as per RC2014 trademark rules

---
+++ Kernel/dev/ds1302_rcbus.s
@@ -0,0 +1,11 @@
+	.area _CODE
+
+PIN_CE          = 0x10
+PIN_DATA_HIZ    = 0x20
+PIN_CLK         = 0x40
+PIN_DATA_OUT    = 0x80
+PIN_DATA_IN     = 0x01
+PIN_OTHER	= 0x00
+
+
+	.include "ds1302_common.s"
