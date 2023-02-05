5c011d03d0eff74dc50e1097e7ee0c34b79d5687
Alan Cox <etchedpixels@gmail.com>
2023-02-05 22:36:36 +0000

    vz200: finish off the lpr bits

--- Kernel/platform-vz200/devlpr.c
+++ Kernel/platform-vz200/devlpr.c
@@ -3,12 +3,20 @@
 #include <kdata.h>
 #include <devlpr.h>
 
+__sfr __at 0x00 status;
+__sfr __at 0x0E data;
+
 int lpr_open(uint_fast8_t minor, uint16_t flag)
 {
-	if (!minor)
+	if (minor == 0)
 		return 0;
 	udata.u_error = ENODEV;
 	return -1;
+}
+
+int lpr_close(uint_fast8_t minor)
+{
+	return 0;
 }
 
 static int iopoll(int sofar)
@@ -30,14 +38,15 @@ int lpr_write(uint_fast8_t minor, uint_fast8_t rawflag, uint_fast8_t flag)
 	uint8_t *p = udata.u_base;
 	uint8_t *pe = p + udata.u_count;
 	int n;
+	irqflags_t irq;
 
 	while (p < pe) {
 		/* Printer busy ? */
-		while (lpstatus & 1) {
+		while (status & 1) {
 			if ((n = iopoll(pe - p)) != 0)
 				return n;
 		}
-		lpdata = ugec(p++);
+		data = ugetc(p++);
 	}
 	return pe - p;
 }
