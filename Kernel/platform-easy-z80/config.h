70e28d1df6129ca5e676290ce7d9607190aa75ea
Alan Cox <etchedpixels@gmail.com>
2023-02-08 22:01:12 +0000

    easy-z80: update docs to reflect other target

--- Kernel/platform-easy-z80/config.h
+++ Kernel/platform-easy-z80/config.h
@@ -2,14 +2,10 @@
 
 /* Set this if you have the RC2014 CF adapter at 0x10/0x90 */
 #define CONFIG_RC2014_CF
-/* Set this to be able to do networking over the second serial port */
-/* Need to do the banked network rework before this will work */
-#undef CONFIG_RC2014_NET
 /* Set this if you have the 8255 IDE adapter (mutually exclusive of RC2014_CF) */
 #undef CONFIG_RC2014_PPIDE
 /* Set this if you have the floppy interface */
 #undef CONFIG_RC2014_FLOPPY
-
 
 /*
  *	Turn selections into system level defines
@@ -21,12 +17,6 @@
 #ifdef CONFIG_RC2014_PPIDE
 #define CONFIG_IDE
 #define CONFIG_PPIDE
-#endif
-#ifdef CONFIG_RC2014_NET
-/* Core Networking support */
-#define CONFIG_NET
-/* User mode uIP TCP/IP daemon */
-#define CONFIG_NET_NATIVE
 #endif
 #ifdef CONFIG_RC2014_FLOPPY
 #define CONFIG_FLOPPY
