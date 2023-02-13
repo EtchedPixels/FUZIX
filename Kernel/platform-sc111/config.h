9fc5dd8631130251d298a8daf577ff4896474e47
Alan Cox <etchedpixels@gmail.com>
2023-02-13 15:18:42 +0000

    sc111: bring the Z180 swap support over into SC111

--- Kernel/platform-sc111/config.h
+++ Kernel/platform-sc111/config.h
@@ -7,12 +7,12 @@
 /* Multiple processes in memory at once */
 #define CONFIG_MULTI
 /* Fixed banking: 8 x 64K banks, top 4KB is shared with kernel, 60KB-62KB is user memory  */
-#define CONFIG_BANK_FIXED
+#define CONFIG_BANK_SPLIT
 /* Permit large I/O requests to bypass cache and go direct to userspace */
 #define CONFIG_LARGE_IO_DIRECT(x)	1
 /* 8 60K banks, 1 is kernel */
 #define MAX_MAPS	8
-#define MAP_SIZE	PROGTOP    /* WRS: I feel this should be 60KB, but setting it so breaks pagemap_realloc() when exec calls it */
+#define MAP_SPLIT	PROGTOP
 
 /* Banks as reported to user space */
 #define CONFIG_BANKS	1
@@ -20,7 +20,7 @@
 #define TICKSPERSEC 40U     /* Ticks per second */
 #define PROGBASE    0x0000  /* also data base */
 #define PROGLOAD    0x0100  /* also data base */
-#define PROGTOP     0xF800  /* Top of program, base of U_DATA */
+#define PROGTOP     0xF600  /* Top of program, base of U_DATA */
 #define KERNTOP     0xF000  /* Kernel has lower 60KB */
 #define PROC_SIZE   64      /* Memory needed per process */
 
@@ -47,14 +47,14 @@
 #define TTYDEV   BOOT_TTY /* Device used by kernel for messages, panics */
 
 /* Z180 does not yet support swap - need to fix that */
-//#define SWAPDEV     (swap_dev)	/* A variable for dynamic, or a device major/minor */
+#define SWAPDEV     (swap_dev)	/* A variable for dynamic, or a device major/minor */
 extern uint16_t swap_dev;
-#define SWAP_SIZE   0x7D 	/* 62.5K in blocks (prog + udata) */
+#define SWAP_SIZE   0x7C 	/* 62K in blocks (prog + udata) */
 #define SWAPBASE    0x0000	/* start at the base of user mem */
-#define SWAPTOP	    0xFA00	/* Swap out udata and program */
+#define SWAPTOP	    0xF800	/* Swap out udata and program */
 #define MAX_SWAPS   16	    	/* We will size if from the partition */
 /* Swap will be set up when a suitably labelled partition is seen */
-//#define CONFIG_DYNAMIC_SWAP
+#define CONFIG_DYNAMIC_SWAP
 #define swap_map(x)	((uint8_t *)(x))
 
 #define plt_copyright()		// for now
