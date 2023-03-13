998505e44c0e2456dc4b4dad5928793e84557b80
Alan Cox <etchedpixels@gmail.com>
2023-03-13 14:32:20 +0000

    syscall_ns32k: add the fork magic

--- Library/tools/syscall_ns32k.c
+++ Library/tools/syscall_ns32k.c
@@ -21,21 +21,36 @@ static void write_call(int n)
   }
   /*
    *	We define _fork() as trashing all the registers except the return
-   *	values *TBD*. The userspace ick here allows us to avoid saving
+   *	values and r6. The userspace ick here allows us to avoid saving
    *	all the registers on syscall entry just in case we are doing a fork and
-   *	have multiple udata blocks. TODO FORK
-   *
-   *	Note: this does mean if you want a register global you need to use TODO
-   *	or ensure you never take a signal in the fork() area - doable but ugly.
+   *	have multiple udata blocks.
    */
+
   fprintf(fp, "\t.text\n\n"
 	      "\t.globl _%1$s\n"
 	      "\t.type _%1$s, @function\n"
 	      "\t.align 2\n\n"
 	      "_%1$s:\n", syscall_name[n]);
+
+  if (strcmp(syscall_name[n], "_fork") == 0) {
+    /* Save r2-r7 and fp */
+    fprintf(fp, "\tenter [r2,r3,r4,r5,r7],0\n");
+    /* Fix up argument to _fork() */
+    fprintf(fp, "\tmovd 8(fp),tos\n");
+    fprintf(fp, "\tmovd 0,tos\n");
+    saves = 1;
+  }
+  /*
+   *	Note: this does mean if you want a register global you need to use r6
+   *	or ensure you never take a signal in the fork() area - doable but ugly.
+   */
   fprintf(fp, "\tmovw %d,r0\n", n);
   fprintf(fp, "\tsvc\n");
   fprintf(fp, "\tmovd r1,_errno(pc)\n");
+  if (saves) {
+    fprintf(fp, "\tadjspd -8\n");
+    fprintf(fp, "\texit [r2,r3,r4,r5,r7]\n");
+  }
   fprintf(fp, "\tret 0\n");
 
   fclose(fp);
