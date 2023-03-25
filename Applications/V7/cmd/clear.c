ee098ea239ff5838bfabb434b62fa932b07ff7a2
Alan Cox <etchedpixels@gmail.com>
2023-03-25 23:15:17 +0000

    clear: clean up warnings

--- Applications/V7/cmd/clear.c
+++ Applications/V7/cmd/clear.c
@@ -1,4 +1,4 @@
-static char *sccsid = "@(#)clear.c	4.1 (Berkeley) 10/1/80";
+/*static char *sccsid = "@(#)clear.c	4.1 (Berkeley) 10/1/80"; */
 /* load me with -ltermlib */
 /* #include <retrofit.h> on version 6 */
 /*
