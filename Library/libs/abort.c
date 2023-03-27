33e8ca9588867dbb7f2ddbb7d51bc0ce8bb848c2
Alan Cox <etchedpixels@gmail.com>
2023-03-27 14:40:44 +0100

    abort: fix for POSIX behaviour
    Lets try that again whilst awake.

--- Library/libs/abort.c
+++ Library/libs/abort.c
@@ -1,9 +1,3 @@
-/*
- *	abort(): unblock SIGABRT, send it and then if that doesn't do
- *	anything set it to default and send it again - which will.
- *
- *	Does not fflush anything as POSIX does not require that.
- */
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -11,10 +5,19 @@
 
 void abort(void)
 {
+	/* POSIX requires this sequence of behaviouir
+		- unblock the signal
+		- raise it
+		- if we end up back here set the disposition to default
+		- raise it again
+	
+	   If you take another signal in the middle of this and
+	   change ABRT then we can end up at the _exit. POSIX has nothing
+	   to say about what happens in such cases so you get to keep all
+	   the pieces */
 	sigrelse(SIGABRT);
-	signal(SIGABRT, SIG_DFL);
 	kill(getpid(),SIGABRT);		/* Correct one */
-	signal(SIGKILL, SIG_DFL);
-	kill(SIGABRT, getpid());
+	signal(SIGABRT, SIG_DFL);
+	kill(getpid(), SIGABRT);
 	_exit(255);			/* WHAT!! */
 }
