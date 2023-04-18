9e5e71eff516c869beb440df93b6d22ebae280eb
Alan Cox <etchedpixels@gmail.com>
2023-04-18 13:08:20 +0100

    i2c: fix silly bug

--- Kernel/platform-rc2014/i2c.c
+++ Kernel/platform-rc2014/i2c.c
@@ -7,7 +7,7 @@ int plt_i2c_msg(struct i2c_msg *m, uint8_t *kbuf)
     if (m->bus)
         return -ENODEV;
     if (m->addr & 1)
-        return pcf8584_tx_msg(m->addr, kbuf, m->len);
+        return pcf8584_rx_msg(m->addr, kbuf, m->len);
     else
         return pcf8584_tx_msg(m->addr, kbuf, m->len);
 }
