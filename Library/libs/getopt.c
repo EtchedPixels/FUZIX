EtchedPixels/FUZIX

commit 2fd69c83e411135acab329f28d8402bddba9705c

getopt: fix warning

Library/libs/getopt.c
@@ -41,8 +41,8 @@ static int sp = 1;		/* position within argument */
 int getopt(int argc, char *const argv[], const char *optstring)
 				/* returns letter, '?', EOF */
 {
-   register int osp;		/* saved `sp' for param test */
 #ifndef STRICT
+   register int osp;		/* saved `sp' for param test */
    register int oind;		/* saved `optind' for param test */
 #endif
    register int c;		/* option letter */
@@ -64,7 +64,9 @@ int getopt(int argc, char *const argv[], const char *optstring)
    }
 
    c = argv[optind][sp];	/* option letter */
+#ifdef STRICT
    osp = sp++;			/* get ready for next letter */
+#endif
 
 #ifndef STRICT
    oind = optind;		/* save optind for param test */

commit fde70411246300b0dbd3db9b25679c65957fedee

getopt: Fix infinite loop regression
Regression from commit 2fd69c8

Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

Library/libs/getopt.c
@@ -67,6 +67,8 @@ int getopt(int argc, char *const argv[], const char *optstring)
    c = argv[optind][sp];	/* option letter */
 #ifdef STRICT
    osp = sp++;			/* get ready for next letter */
+#else
+   sp++;
 #endif
 
 #ifndef STRICT