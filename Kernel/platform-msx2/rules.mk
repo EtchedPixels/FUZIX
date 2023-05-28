680bb18e28bad42e8910622a383f3fa4469c0b0e
Alan Cox <etchedpixels@gmail.com>
2023-05-28 13:30:32 +0100

    msx2: add forgotten rules.mk

---
+++ Kernel/platform-msx2/rules.mk
@@ -0,0 +1,13 @@
+export CROSS_CC_SEG1=--codeseg CODE2
+export CROSS_CC_SEG2=--codeseg CODE2
+export CROSS_CC_SEG3=--codeseg CODE2
+export CROSS_CC_SEG4=--codeseg CODE2
+export CROSS_CC_VIDEO=--codeseg CODE2
+export CROSS_CC_FONT=--constseg FONT
+#
+export CROSS_CC_SYS1=--codeseg CODE2
+export CROSS_CC_SYS2=--codeseg CODE2
+export CROSS_CC_SYS3=--codeseg CODE2
+export CROSS_CC_SYS4=--codeseg CODE2
+export CROSS_CC_SYS5=--codeseg CODE2
+export CROSS_CC_SEGDISC=--codeseg DISCARD --constseg DISCARD
