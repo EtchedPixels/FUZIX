34d12e7e374a424c5d6a2477a1b62ea17ce9e4ce
Alan Cox <etchedpixels@gmail.com>
2023-06-15 23:21:00 +0100

    8080: adapt the bank helpers to the new compiler

--- Kernel/lib/8080fixedbank-core.s
+++ Kernel/lib/8080fixedbank-core.s
@@ -1,140 +1,144 @@
-!
-!	For a purely bank based architecture this code is common and can be
-!	used by most platforms
-!
-!	The caller needs to provide the standard map routines along with
-!	map_kernel_a which maps in the kernel bank in a. This code assumes
-!	that the bank can be encoded in 8bits.
-!
+;
+;	For a purely bank based architecture this code is common and can be
+;	used by most platforms
+;
+;	The caller needs to provide the standard map routines along with
+;	map_kernel_a which maps in the kernel bank in a. This code assumes
+;	that the bank can be encoded in 8bits.
+;
 
-.sect .common
+	.common
 
-.define _plt_switchout
+	.export _plt_switchout
 
-!
-!	The ABI requires we preserve BC
-!
+;
+;	The ABI requires we preserve BC
+;
 
-!
-!	Switch a process out and run the next one
-!
+;
+;	Switch a process out and run the next one
+;
 _plt_switchout:
 	push b
 	lxi h,0
-	push h			! Save a 0 argment
-	lhld .retadr
+	push h			; Save a 0 argment
+	lhld __tmp
 	push h
-	lhld .bcreg
+	lhld __hireg
 	push h
-	lhld .tmp1
+	lhld __tmp2
 	push h
-	lhld .areg
+	lhld __tmp2+2
 	push h
-	! Do we need to save block1->block3+2 (ie do we do 32bit * and / ???
+	lhld __tmp3
+	push h
+	lhld __tmp3+2
+	push h
+	; Do we need to save block1->block3+2 (ie do we do 32bit * and / ???
 	lxi h,0
 	dad sp
-	shld U_DATA__U_SP	! Save the sp for a switch in
-	call map_process_always_di
-	!
-	!	Save the udata into process state
-	!
-	!	FIXME: can we skip this if the process is defunct or would
-	!	it even make sense to defer it (with magic in swapout) ?
-	!
-	lxi h,U_DATA
+	shld U_DATA__U_SP	; Save the sp for a switch in
+	call map_proc_always_di
+	;
+	;	Save the udata into process state
+	;
+	;	FIXME: can we skip this if the process is defunct or would
+	;	it even make sense to defer it (with magic in swapout) ?
+	;
+	lxi h,_udata
 	lxi d,U_DATA_STASH
-	call copy512		! No ldir 8(
-	! Back to kernel
+	call copy512		; No ldir 8(
+	; Back to kernel
 	call map_kernel_di
-	call _getproc		! Who goes next
+	call _getproc		; Who goes next
 	push d
-	call _switchin		! Run it
-	! Should never hit this
+	call _switchin		; Run it
+	; Should never hit this
 	call _plt_monitor
 
-!
-!	Switch a process back in
-!
-.define _switchin
+;
+;	Switch a process back in
+;
+	.export _switchin
 
-!
-!	Switch to a given process
-!
+;
+;	Switch to a given process
+;
 _switchin:
 	di
 	pop b
-	pop d			! DE is now our process to run
+	pop d			; DE is now our process to run
 	push d
 	push b
 
 	call map_kernel_di
 
-	!
-	!	Is it swapped (page 0)
-	!
+	;
+	;	Is it swapped (page 0)
+	;
 	lxi h,P_TAB__P_PAGE_OFFSET
 	dad d
 
 	lxi sp,_swapstack
-	mov a,m			! A is now our bank
+	mov a,m			; A is now our bank
 	ora a
 	jnz not_swapped
-	!
-	!	Swapping time. Interrupts back on
-	!
+	;
+	;	Swapping time. Interrupts back on
+	;
 	ei
 	xra a
 	sta _int_disabled
-	!
-	!	Can we rely on the compiler not mashing the stacked var ?
-	!
+	;
+	;	Can we rely on the compiler not mashing the stacked var ?
+	;
 	push h
 	push d
 	call _swapper
 	pop d
 	pop h
-	!
-	!	It should now be back in memory
-	!
+	;
+	;	It should now be back in memory
+	;
 	mvi a,1
 	sta _int_disabled
 	di
 	mov a,m
-	!
-	!	Check if we need to recover the udata (we were last to run)
-	!
+	;
+	;	Check if we need to recover the udata (we were last to run)
+	;
 not_swapped:
 	mov c,a
-	! DE is the process to run, get the udata process into HL
+	; DE is the process to run, get the udata process into HL
 	lhld U_DATA__U_PTAB
-	! See if our udata is live - this is common, if we sleep and we are
-	! the next to run for example.
+	; See if our udata is live - this is common, if we sleep and we are
+	; the next to run for example.
 	mov a,h
 	cmp d
 	jnz copyback
 	mov a,l
 	cmp e
 	jz skip_copyback
-	!
-	!	Recover the udata
-	!
+	;
+	;	Recover the udata
+	;
 copyback:
 	mov a,c
-	call map_process_a
+	call map_proc_a
 
 	push d
 	lxi h,U_DATA_STASH
-	lxi d,U_DATA
+	lxi d,_udata
 	call copy512
 	pop d
 
-	lhld U_DATA__U_SP	! A valid stack to map on
+	lhld U_DATA__U_SP	; A valid stack to map on
 	sphl
 
 	call map_kernel
-	!
-	!	Did we find the right process ?
-	!
+	;
+	;	Did we find the right process ?
+	;
 	lhld U_DATA__U_PTAB
 	mov a,h
 	cmp d
@@ -144,10 +148,10 @@ copyback:
 	jnz switchinfail
 
 skip_copyback:
-	!
-	!	Mark us as running, clear our preemption counter
-	!	and set the interrupt flags
-	!
+	;
+	;	Mark us as running, clear our preemption counter
+	;	and set the interrupt flags
+	;
 	lhld U_DATA__U_PTAB
 	mvi a,P_RUNNING
 	mov m,a
@@ -159,23 +163,26 @@ skip_copyback:
 	shld _runticks
 	lhld U_DATA__U_SP
 	sphl
-	!
-	!	Recover our parent frame pointer and return code
-	!
-	!	probably we still need to save block1-block3 (12 bytes)
-	!	used by .mli4/.dvi4. Would be good to make them do their
-	!	own saves so the cost is paid in the right place
-	!
+	;
+	;	Recover our parent frame pointer and return code
+	;
+	;	probably we still need to save block1-block3 (12 bytes)
+	;	used by .mli4/.dvi4. Would be good to make them do their
+	;	own saves so the cost is paid in the right place
+	;
 	pop h
-	mov a,l
-	sta .areg	! FIXME: add a pad byte to .areg instead
+	shld __tmp3+2
 	pop h
-	shld .tmp1
+	shld __tmp3
 	pop h
-	shld .bcreg
+	shld __tmp2+2
 	pop h
-	shld .retadr
-	pop d
+	shld __tmp2
+	pop h
+	shld __hireg
+	pop h
+	shld __tmp
+	pop h
 	pop b
 	lda U_DATA__U_ININTERRUPT
 	sta _int_disabled
@@ -191,19 +198,20 @@ switchinfail:
 	call _plt_monitor
 
 badswitchmsg:
-	.asciz 'badsw'
+	.ascii "badsw"
+	.byte 0
 
 
 fork_proc_ptr:
-	.data2 0
+	.word 0
 
-.define _dofork
-!
-!	The heart of fork
-!
+	.export _dofork
+;
+;	The heart of fork
+;
 _dofork:
 	pop d
-	pop h		! new process
+	pop h		; new process
 	push h
 	push d
 
@@ -215,26 +223,30 @@ _dofork:
 	inx h
 	mov h,m
 	mov l,a
-	!
-	! We don't have any state to save but the pid and framepointer 
-	! and (alas) a pile of compiler non-reentrant crap
-	!
+	;
+	; We don't have any state to save but the pid and bc
+	; and (alas) a pile of compiler non-reentrant crap
+	;
 	push b
 	push h
-	lhld .retadr
+	lhld __tmp
 	push h
-	lhld .bcreg
+	lhld __hireg
 	push h
-	lhld .tmp1
+	lhld __tmp2
 	push h
-	lhld .areg
+	lhld __tmp2+1
+	push h
+	lhld __tmp3
+	push h
+	lhld __tmp3+1
 	push h
 	lxi h,0
 	dad sp
 	shld U_DATA__U_SP
-	!
-	! We are now in a safe state to work
-	!
+	;
+	; We are now in a safe state to work
+	;
 	lhld fork_proc_ptr
 	lxi d,P_TAB__P_PAGE_OFFSET
 	dad d
@@ -243,26 +255,26 @@ _dofork:
 
 	call bankfork
 
-	call map_process_always
-	!
-	!	Copy the parent udata and stack into the parent stash
-	!	The live udata becomes that of the child
-	!
-	lxi h,U_DATA
+	call map_proc_always
+	;
+	;	Copy the parent udata and stack into the parent stash
+	;	The live udata becomes that of the child
+	;
+	lxi h,_udata
 	lxi d,U_DATA_STASH
 	call copy512
 
 	call map_kernel
 
-	pop h		! Get rid of saved pid
-	pop h		! and C runtime state
+	pop h		; Get rid of saved pid
+	pop h		; and C runtime state
 	pop h
 	pop h
 	pop h
 
-	!
-	!	Manufacture the child udata state
-	!
+	;
+	;	Manufacture the child udata state
+	;
 	lxi h,_udata
 	push h
 	lhld fork_proc_ptr
@@ -270,26 +282,26 @@ _dofork:
 	call _makeproc
 	pop b
 	pop b
-	!
-	!	Timer ticks
-	!
+	;
+	;	Timer ticks
+	;
 	lxi h,0
 	shld _runticks
-	!
-	!	Frame pointer
-	!
-	xchg			! return 0 in DE for child
+	;
+	;	Frame pointer
+	;
+	; return 0 in HL for child
 	pop b
 	ret
 
-.define bouncebuffer
-.define _swapstack
+	.export bouncebuffer
+	.export _swapstack
 
 bouncebuffer:
-	.space 256		! Do we really need 256 ?
+	.ds 256		; Do we really need 256 ?
 _swapstack:
 
-.define _need_resched
+	.export _need_resched
 
 _need_resched:
-	.data1 0
+	.byte 0
--- Kernel/lib/8080fixedbank.s
+++ Kernel/lib/8080fixedbank.s
@@ -1,10 +1,8 @@
-#
-
 #include "../lib/8080fixedbank-core.s"
 
-!
-!	Fast copy 512 bytes from H to D
-!
+;
+;	Fast copy 512 bytes from H to D
+;
 copy512:
 	mvi b,64
 copy8:
