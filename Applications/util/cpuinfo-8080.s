732ce0bd6d87a6493401ad08bae92a8c47237c2d
Alan Cox <etchedpixels@gmail.com>
2023-06-15 17:55:25 +0100

    8080@ continue switch to new compiler

--- Applications/util/cpuinfo-8080.s
+++ Applications/util/cpuinfo-8080.s
@@ -1,14 +1,10 @@
-.sect text
-.sect rom
-.sect data
-.sect bss
+	.code
+	.setcpu 8080
 
-.sect text
-
-.define _cpu_identify
+.export _cpu_identify
 
 _cpu_identify:
-	! Ok start with the flags
+	; Ok start with the flags
 	mvi a,255
 	inr a
 	push psw
@@ -19,39 +15,39 @@ _cpu_identify:
 	jz lr35902
 	ora a
 	jnz is808x
-	lxi d,3		! Z80: we don't care which kind.
+	lxi d,3		; Z80: we don't care which kind.
 	ret
 lr35902:
-	lxi d,4		! also not allowed
+	lxi d,4		; also not allowed
 	ret
 is808x:
 	xra a
-	rim		! no-op on 8080
+	.db 0x20	; no-op on 8080 RIM on 8085
 	ora a
-	jnz is8085	! it changed must be an 8085
-	!
-	!	But it could really be 0
-	!
+	jnz is8085	; it changed must be an 8085
+	;
+	;	But it could really be 0
+	;
 	inr a
-	rim
+	.db 0x20	; no-op or RIM
 	ora a
 	jz is8085
-	!
-	!	TODO: check for KP580M1
-	!
+	;
+	;	TODO: check for KP580M1
+	;
 	lxi d,0
-	!
-	!	But wait it might be a 9080
-	!
+	;
+	;	But wait it might be a 9080
+	;
 	mvi a,255
 	ani 255
 	push psw
 	pop h
 	mov a,l
-	ani 0x10	! half carry is zero on AMD
+	ani 0x10	; half carry is zero on AMD
 	rnz
-	inx d		! 1 = AMD 9080A
+	inx d		; 1 = AMD 9080A
 	ret
 is8085:
-	lxi d,2		! 8085
+	lxi d,2		; 8085
 	ret
