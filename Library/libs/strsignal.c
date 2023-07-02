79cd59fa7ce4418ec51418b6248f8de2c7bbfaaf
Tormod Volden <debian.tormod@gmail.com>
2023-07-02 09:13:12 +0200

    Library/libs/strsignal.c: Add numbered comments for easier navigation
    Inspired by Library/tools/liberror.c
    
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

--- Library/libs/strsignal.c
+++ Library/libs/strsignal.c
@@ -6,36 +6,46 @@
 #include <unistd.h>
 
 const char *sys_siglist[NSIGS] = {
+/* 0 */
   "Unknown signal",
   "Hangup",
   "Interrupt",
   "Quit",
   "Illegal instruction",
+
   "BPT trace/trap",
   "I/O trap/ABORT instruction",
   "Bus error",
   "Floating point exception",
   "Killed",
+
+ /* 10 */
   "User signal 1",
   "Segmentation fault",
   "User signal 2",
   "Broken pipe",
   "Alarm clock",
+
   "Terminated",
   "Stack fault",
   "Child death or stop",
   "Continue",
   "Stopped",
+
+ /* 20 */
   "Stopped (signal)",
   "Stopped (tty input)",
   "Stopped (tty output)",
   "Urgent IO condition",
   "CPU limit",
+
   "File limit",
   "Alarm (virtual)",
   "Alarm (profile)",
   "Window changed",
   "I/O ready",
+
+/* 30 */
   "Power",
   "Bad system call"
 };
