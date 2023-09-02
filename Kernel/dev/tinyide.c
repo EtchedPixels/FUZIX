f97a61b23fa2bc4835ca89aefb5695ea3202e351
Alan Cox <etchedpixels@gmail.com>
2023-09-02 23:11:01 +0100

    tinyide: adapt to map_proc changes

--- Kernel/dev/tinyide.c
+++ Kernel/dev/tinyide.c
@@ -82,7 +82,7 @@ not_swapin:
 #endif
             or a                                    ; test is_user
             jr z, rd_kernel
-            call map_process_always  	            ; map user memory first if required
+            call map_proc_always  	            ; map user memory first if required
             jr doread
 rd_kernel:
             call map_buffers
@@ -113,7 +113,7 @@ not_swapout:
 #endif
             or a                                    ; test is_user
             jr z, wr_kernel
-            call map_process_always                 ; else map user memory first if required
+            call map_proc_always                 ; else map user memory first if required
             jr dowrite
 wr_kernel:
             call map_buffers
