7903f1659f229ef76aa99770ad3f12cdd991b211
Alan Cox <etchedpixels@gmail.com>
2023-09-04 12:13:28 +0100

    tbblue: kill - this offers nothing over the standard zxuno

--- Kernel/platform-tbblue/commonmem.s
+++
@@ -1,8 +0,0 @@
-;
-; Multiple app sizes and the fact the kernel and apps share the same banks
-; means we need to put this somewhere low
-;
-        .module commonmem
-        .area _COMMONDATA
-
-	.include "../cpu-z80/std-commonmem.s"
