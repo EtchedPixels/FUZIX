9f60c877fb1a1c7d8bd9a670c586d1861e40e25c
Alan Cox <etchedpixels@gmail.com>
2023-09-04 12:11:01 +0100

    divide: move to tinydisk

--- Kernel/dev/zx/divide.c
+++ Kernel/dev/zx/divide.c
@@ -1,33 +1,35 @@
 #include <kernel.h>
 #include <kdata.h>
 #include <printf.h>
-#include <devide.h>
-#include <blkdev.h>
-#include <platform_ide.h>
+#include <tinyide.h>
+#include <plt_ide.h>
 #include <devtty.h>
 
 /* We have to provide slightly custom methods here because of the banked
    kernel */
 COMMON_MEMORY
 
-void devide_read_data(void) __naked
+void devide_read_data(uint8_t *p) __naked
 {
     __asm
-            ld a, (_blk_op+BLKPARAM_IS_USER_OFFSET) ; blkparam.is_user
-            ld hl, (_blk_op+BLKPARAM_ADDR_OFFSET)   ; blkparam.addr
+            pop de
+            pop hl
+            push hl
+            push de
+            ld a, (_td_raw) 			    ; I/O type ?
             ld bc, #IDE_REG_DATA                    ; setup port number
                                                     ; and count
             push af
 #ifdef SWAPDEV
 	    cp #2
             jr nz, not_swapin
-            ld a, (_blk_op+BLKPARAM_SWAP_PAGE)	    ; blkparam.swap_page
+            ld a, (_td_page)	 		   ; swap page
             call map_for_swap
             jr doread
 not_swapin:
 #endif
             or a                                    ; test is_user
-            call nz, map_process_always  	    ; map user memory first if required
+            call nz, map_proc_always		    ; map user memory first if required
 doread:
             ld a,#0x05
             out (0xfe),a
@@ -44,24 +46,27 @@ doread:
     __endasm;
 }
 
-void devide_write_data(void) __naked
+void devide_write_data(uint8_t *p) __naked
 {
     __asm
-            ld a, (_blk_op+BLKPARAM_IS_USER_OFFSET) ; blkparam.is_user
-            ld hl, (_blk_op+BLKPARAM_ADDR_OFFSET)   ; blkparam.addr
+            pop de
+            pop hl
+            push hl
+            push de
+            ld a, (_td_raw) ;			    ; I/O type
             ld bc, #IDE_REG_DATA                    ; setup port number
                                                     ; and count
             push af
 #ifdef SWAPDEV
 	    cp #2
             jr nz, not_swapout
-            ld a, (_blk_op+BLKPARAM_SWAP_PAGE)	    ; blkparam.swap_page
+            ld a, (_td_page)			    ; page to swap
             call map_for_swap
             jr dowrite
 not_swapout:
 #endif
             or a                                    ; test is_user
-            call nz, map_process_always             ; else map user memory first if required
+            call nz, map_proc_always                ; else map user memory first if required
 dowrite:
             ld a,#0x05
             out (0xfe),a
