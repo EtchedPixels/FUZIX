d0adeea9caa6b1d078c196b9050e868b19a072bb
Alan Cox <etchedpixels@gmail.com>
2023-09-04 10:23:16 +0100

    zrc: fix map_proc memalloc_none and correct accidental banking enable

--- Kernel/platform-zrc/rules.mk
+++ Kernel/platform-zrc/rules.mk
@@ -1,19 +1,1 @@
-#
 CROSS_CCOPTS += --peep-file $(FUZIX_ROOT)/Kernel/cpu-z80/rst.peep
-CROSS_CCOPTS += --external-banker
-#
-# Tell the core code we are using the banked helpers
-#
-export BANKED=-banked
-export CROSS_CC_SEG1=--codeseg CODE1
-export CROSS_CC_SEG2=--codeseg CODE2
-export CROSS_CC_SEG3=--codeseg CODE1
-export CROSS_CC_SEG4=--codeseg CODE1
-export CROSS_CC_VIDEO=--codeseg CODE2
-#
-export CROSS_CC_SYS1=--codeseg CODE1
-export CROSS_CC_SYS2=--codeseg CODE1
-export CROSS_CC_SYS3=--codeseg CODE1
-export CROSS_CC_SYS4=--codeseg CODE2
-export CROSS_CC_SYS5=--codeseg CODE2
-export CROSS_CC_SEGDISC=--codeseg DISCARD2
