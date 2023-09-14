17589a7c8741393d66da9d34896464fd2cdd35dd
Alan Cox <etchedpixels@gmail.com>
2023-09-14 22:08:53 +0100

    ch375: allow platform level methods
    We need this for z88dk fast call and inline port access or mmio on some
    targets

--- Kernel/dev/ch375.h
+++ Kernel/dev/ch375.h
@@ -1,9 +1,4 @@
+#include "plt_ch375.h"
+
 extern uint_fast8_t ch375_probe(void);
 
-extern uint8_t ch375_rdata(void);
-extern uint8_t ch375_rstatus(void);
-extern void ch375_wdata(uint_fast8_t data);
-extern void ch375_wcmd(uint_fast8_t cmd);
-extern void ch375_rblock(uint8_t *ptr);
-extern void ch375_wblock(uint8_t *ptr);
-extern void nap20(void);
