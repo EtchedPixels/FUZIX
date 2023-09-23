716e2defb20eb7fd38a43fae93d25e5da7f0f6a9
Alan Cox <etchedpixels@gmail.com>
2023-09-23 20:13:06 +0100

    rcbus-8085: reformat tty code ready for further vdp work

--- Kernel/platform-rcbus-8085/devtty.c
+++ Kernel/platform-rcbus-8085/devtty.c
@@ -10,10 +10,10 @@
 static unsigned char tbuf1[TTYSIZ];
 static unsigned char tbuf2[TTYSIZ];
 
-struct  s_queue  ttyinq[NUM_DEV_TTY+1] = {       /* ttyinq[0] is never used */
-    {   NULL,    NULL,    NULL,    0,        0,       0    },
-    {   tbuf1,   tbuf1,   tbuf1,   TTYSIZ,   0,   TTYSIZ/2 },
-    {   tbuf2,   tbuf2,   tbuf2,   TTYSIZ,   0,   TTYSIZ/2 },
+struct s_queue ttyinq[NUM_DEV_TTY + 1] = {	/* ttyinq[0] is never used */
+	{ NULL, NULL, NULL, 0, 0, 0 },
+	{ tbuf1, tbuf1, tbuf1, TTYSIZ, 0, TTYSIZ / 2 },
+	{ tbuf2, tbuf2, tbuf2, TTYSIZ, 0, TTYSIZ / 2 },
 };
 
 static uint_fast8_t acia_minor;
@@ -28,7 +28,7 @@ uint8_t vidmode;
 
 tcflag_t termios_mask[NUM_DEV_TTY + 1] = {
 	0,
-	/*CSIZE|CSTOPB|PARENB|PARODD|*/_CSYS,
+	/*CSIZE|CSTOPB|PARENB|PARODD| */ _CSYS,
 	_CSYS
 };
 
@@ -41,34 +41,34 @@ static void calc_acia_setup(void)
 	/* No CS5/CS6 CS7 must have parity enabled */
 	if (r <= CS7) {
 		t->c_cflag &= ~CSIZE;
-		t->c_cflag |= CS7|PARENB;
+		t->c_cflag |= CS7 | PARENB;
 	}
 	/* No CS8 parity and 2 stop bits */
 	if (r == CS8 && (t->c_cflag & PARENB))
 		t->c_cflag &= ~CSTOPB;
 	/* There is no obvious logic to this */
-	switch(t->c_cflag & (CSIZE|PARENB|PARODD|CSTOPB)) {
-	case CS7|PARENB:
+	switch (t->c_cflag & (CSIZE | PARENB | PARODD | CSTOPB)) {
+	case CS7 | PARENB:
 		r = 0x8A;
 		break;
-	case CS7|PARENB|PARODD:
+	case CS7 | PARENB | PARODD:
 		r = 0x8E;
 		break;
-	case CS7|PARENB|CSTOPB:
+	case CS7 | PARENB | CSTOPB:
 		r = 0x82;
-	case CS7|PARENB|PARODD|CSTOPB:
+	case CS7 | PARENB | PARODD | CSTOPB:
 		r = 0x86;
-	case CS8|CSTOPB:
+	case CS8 | CSTOPB:
 		r = 0x92;
 		break;
 	default:
 	case CS8:
 		r = 0x96;
 		break;
-	case CS8|PARENB:
+	case CS8 | PARENB:
 		r = 0x9A;
 		break;
-	case CS8|PARENB|PARODD:
+	case CS8 | PARENB | PARODD:
 		r = 0x9E;
 		break;
 	}
@@ -80,7 +80,7 @@ static void calc_acia_setup(void)
  *	to stuff into the chip.
  */
 static const uint16_t clocks[] = {
-	12,		/* Not a real rate */
+	12,			/* Not a real rate */
 	2304,
 	1536,
 	1047,
@@ -104,36 +104,36 @@ static void calc_16x50_setup(void)
 	uint16_t w;
 	struct termios *t = &ttydata[uart_minor].termios;
 
-	d = 0x80;	/* DLAB (so we can write the speed) */
+	d = 0x80;		/* DLAB (so we can write the speed) */
 	d |= (t->c_cflag & CSIZE) >> 4;
-	if(t->c_cflag & CSTOPB)
+	if (t->c_cflag & CSTOPB)
 		d |= 0x04;
 	if (t->c_cflag & PARENB)
 		d |= 0x08;
 	if (!(t->c_cflag & PARODD))
 		d |= 0x10;
-	out(0xC3, d);	/* LCR */
+	out(0xC3, d);		/* LCR */
 	w = clocks[t->c_cflag & CBAUD];
 	out(0XC0, w);		/* Set the DL */
 	out(0xC1, w >> 8);
-	if (w >> 8)	/* Low speeds interrupt every byte for latency */
+	if (w >> 8)		/* Low speeds interrupt every byte for latency */
 		out(0xC2, 0x00);
-	else		/* High speeds set our interrupt quite early
-			   as our latency is poor, turn on 64 byte if
-			   we have a 16C750 */
+	else			/* High speeds set our interrupt quite early
+				   as our latency is poor, turn on 64 byte if
+				   we have a 16C750 */
 		out(0xC2, 0x51);
 	out(0xC3, d & 0x7F);
 	/* FIXME: CTS/RTS support */
-	out(0xC4, 0x03); /* DTR RTS */
-	out(0xC1, 0x0D); /* We don't use tx ints */
+	out(0xC4, 0x03);	/* DTR RTS */
+	out(0xC1, 0x0D);	/* We don't use tx ints */
 }
 
 void tty_setup(uint_fast8_t minor, uint_fast8_t flags)
 {
 	if (minor == acia_minor) {
-	  calc_acia_setup();
+		calc_acia_setup();
 	} else {
-	  calc_16x50_setup();
+		calc_16x50_setup();
 	}
 }
 
@@ -145,14 +145,14 @@ int tty_carrier(uint_fast8_t minor)
 
 void tty_putc(uint_fast8_t minor, uint_fast8_t c)
 {
-  uint8_t ch = c;
-  /* Shadow the first console on the TMS 9918A */
-  if (minor == 1 && tms9918a_present)
-	vtoutput(&ch, 1);
-  if (minor == acia_minor)
-    ttyout_acia(c);
-  else
-    ttyout_uart(c);
+	uint8_t ch = c;
+	/* Shadow the first console on the TMS 9918A */
+	if (minor == 1 && tms9918a_present)
+		vtoutput(&ch, 1);
+	if (minor == acia_minor)
+		ttyout_acia(c);
+	else
+		ttyout_uart(c);
 }
 
 void tty_sleeping(uint_fast8_t minor)
@@ -165,33 +165,33 @@ void tty_data_consumed(uint_fast8_t minor)
 
 ttyready_t tty_writeready(uint_fast8_t minor)
 {
-    uint_fast8_t r;
-    if (minor == acia_minor)
-        r = acia_ready();
-    else
-        r = uart_ready();
-    if (r)
-        return TTY_READY_NOW;
-    return TTY_READY_SOON;
+	uint_fast8_t r;
+	if (minor == acia_minor)
+		r = acia_ready();
+	else
+		r = uart_ready();
+	if (r)
+		return TTY_READY_NOW;
+	return TTY_READY_SOON;
 }
 
 void tty_poll(void)
 {
-    uint16_t r;
-    if (uart_minor)
-      while((r = uart_poll()) != 0xFFFF)
-          tty_inproc(uart_minor, r);
-    if (acia_minor)
-      while((r = acia_poll()) != 0xFFFF)
-          tty_inproc(acia_minor, r);
+	uint16_t r;
+	if (uart_minor)
+		while ((r = uart_poll()) != 0xFFFF)
+			tty_inproc(uart_minor, r);
+	if (acia_minor)
+		while ((r = acia_poll()) != 0xFFFF)
+			tty_inproc(acia_minor, r);
 }
 
 /* kernel writes to system console -- never sleep! */
 void kputchar(uint_fast8_t c)
 {
-    tty_putc(TTYDEV & 0xFF, c);
-    if(c == '\n')
-        tty_putc(TTYDEV & 0xFF, '\r');
+	tty_putc(TTYDEV & 0xFF, c);
+	if (c == '\n')
+		tty_putc(TTYDEV & 0xFF, '\r');
 }
 
 /*
@@ -210,30 +210,30 @@ static const struct videomap tms_map = {
 /* FIXME: we need a way of reporting CPU speed/TMS delay info as unlike the
    ports so far we need delays on the RCBUS */
 static const struct display tms_mode = {
-    1,
-    256, 192,
-    256, 192,
-    255, 255,
-    FMT_VDP,
-    HW_VDP_9918A,
-    GFX_MULTIMODE|GFX_MAPPABLE|GFX_TEXT,
-    16,
-    0
+	1,
+	256, 192,
+	256, 192,
+	255, 255,
+	FMT_VDP,
+	HW_VDP_9918A,
+	GFX_MULTIMODE | GFX_MAPPABLE | GFX_TEXT,
+	16,
+	0
 };
 
 int rctty_ioctl(uint_fast8_t minor, uarg_t arg, char *ptr)
 {
-  if (minor < 3 && tms9918a_present) {
- 	switch(arg) {
- 	case GFXIOC_GETINFO:
- 		return uput(&tms_mode, ptr, sizeof(struct display));
-	case GFXIOC_MAP:
-	        return uput(&tms_map, ptr, sizeof(struct display));
-	case GFXIOC_UNMAP:
-		return 0;
+	if (minor < 3 && tms9918a_present) {
+		switch (arg) {
+		case GFXIOC_GETINFO:
+			return uput(&tms_mode, ptr, sizeof(struct display));
+		case GFXIOC_MAP:
+			return uput(&tms_map, ptr, sizeof(struct display));
+		case GFXIOC_UNMAP:
+			return 0;
+		}
 	}
-  }
-  return tty_ioctl(minor, arg, ptr);
+	return tty_ioctl(minor, arg, ptr);
 }
 
 int rctty_open(uint_fast8_t minor, uint16_t flag)
@@ -246,14 +246,14 @@ int rctty_open(uint_fast8_t minor, uint16_t flag)
 
 void rctty_init(void)
 {
-  if (acia_present) {
-    acia_minor = ++num_uart;
-    termios_mask[1] = _CSYS|CSIZE|CSTOPB|PARENB|PARODD;
-  }
-  if (uart_present) {
-    uart_minor = ++num_uart;
-    /* TODO: set up and termios mask */
-  }
+	if (acia_present) {
+		acia_minor = ++num_uart;
+		termios_mask[1] = _CSYS | CSIZE | CSTOPB | PARENB | PARODD;
+	}
+	if (uart_present) {
+		uart_minor = ++num_uart;
+		/* TODO: set up and termios mask */
+	}
 }
 
 void do_beep(void)
