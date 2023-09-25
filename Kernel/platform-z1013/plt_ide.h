8037bef40241285249d3fe4364ae499944db1104
Alan Cox <etchedpixels@gmail.com>
2023-09-25 19:29:25 +0100

    z1013: add missed new file for plt_ide.h

---
+++ Kernel/platform-z1013/plt_ide.h
@@ -0,0 +1,11 @@
+__sfr __at 0x48 data;
+__sfr __at 0x49 error;
+__sfr __at 0x4A count;
+__sfr __at 0x4B sec;
+__sfr __at 0x4C cyll;
+__sfr __at 0x4D cylh;
+__sfr __at 0x4E devh;
+__sfr __at 0x4F cmd;
+__sfr __at 0x4F status;
+
+#define IDE_REG_DATA	0x0048
