1783f9c91b111f903059b58b9f13644635fa6e77
Alan Cox <etchedpixels@gmail.com>
2023-10-05 22:41:27 +0100

    6800: update kernel code to match new tree

--- Kernel/cpu-6800/image.mk
+++ Kernel/cpu-6800/image.mk
@@ -2,5 +2,5 @@
 tools/visualize6800: tools/visualize6800.c
 
 fuzix.bin: target $(OBJS) tools/visualize6800
-	+make -C platform-$(TARGET) image
+	+make -C platform/platform-$(TARGET) image
 	tools/visualize6800 <fuzix.map
