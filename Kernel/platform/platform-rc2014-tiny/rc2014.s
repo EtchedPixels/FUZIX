8e37c7bb82069a2e344ef10d2a3b4cac4be928c6
Alan Cox <etchedpixels@gmail.com>
2023-10-05 16:03:24 +0100

    rc2014-tiny: get building with the tree changes

--- Kernel/platform/platform-rc2014-tiny/rc2014.s
+++ Kernel/platform/platform-rc2014-tiny/rc2014.s
@@ -41,7 +41,7 @@
 	.globl outchar
 
         .include "kernel.def"
-        .include "../kernel-z80.def"
+        .include "../../cpu-z80/kernel-z80.def"
 
 ;=========================================================================
 ; Constants
