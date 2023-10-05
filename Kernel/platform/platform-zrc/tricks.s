54196f31d75f632f46628c7e1dd60353ab038a2b
Alan Cox <etchedpixels@gmail.com>
2023-10-05 19:16:12 +0100

    zrc: update for new tree layout

--- Kernel/platform/platform-zrc/tricks.s
+++ Kernel/platform/platform-zrc/tricks.s
@@ -4,13 +4,13 @@
 ;	bank to bank copier will give vastly better fork() performance.
 ;
 	.include "kernel.def"
-	.include "../kernel-z80.def"
+	.include "../../cpu-z80/kernel-z80.def"
 
 ;
 ;	All of the fixed bank support is available as a library routine,
 ;	however it is a performance sensitive area. Start with
 ;
-;	.include "../lib/z80fixedbank.s"
+;	.include "../../lib/z80fixedbank.s"
 ;
 ;	As well as using "usermem_std-z80.rel" in your link file for the
 ;	userspace access operations.
@@ -21,13 +21,13 @@
 ;
 ;	Firstly we still want the core of the fixed bank support
 ;
-	.include "../lib/z80fixedbank-core.s"
+	.include "../../lib/z80fixedbank-core.s"
 
 ;	We can also use the standard fast user copiers because all the
 ;	kernel objects we need to copy to/from userspace exist in the
 ;	user mapping.
 ;
-	.include "../lib/z80user1.s"
+	.include "../../lib/z80user1.s"
 
 ;
 ;	We want to provide our own optimized direct 32K bank to bank
