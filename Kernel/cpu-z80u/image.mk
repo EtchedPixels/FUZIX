06f94cb5caa832d41575b9cc90e5a5e8f37b5526
Alan Cox <etchedpixels@gmail.com>
2023-10-17 18:27:18 +0100

    cpu-z80u: rules and support for building kernel with ccz80

---
+++ Kernel/cpu-z80u/image.mk
@@ -0,0 +1,9 @@
+tools/visualize6800: tools/visualize6800.c
+
+tools/pack85: tools/pack85.c
+
+tools/doubleup: tools/doubleup.c
+
+fuzix.bin: target $(OBJS) tools/pack85 tools/visualize6800 tools/doubleup
+	+$(MAKE) -C platform/platform-$(TARGET) image
+	tools/visualize6800 <fuzix.map
