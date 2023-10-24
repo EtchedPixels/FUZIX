851a685adbe477e65921ed5e078787f8d50163a0
Alan Cox <etchedpixels@gmail.com>
2023-10-24 22:18:52 +0100

    z80: turn on the changes to the core compiler for relocatable binaries

--- Library/libs/crt0_z80.s
+++ Library/libs/crt0_z80.s
@@ -5,11 +5,11 @@
 __text:
 		.word 0x80A8	; Fuzix executable
 		.byte 1			; 8080 series
-		.byte 1			; Z80 feature set
-		.byte 1			; Base 0x100
+		.byte 0x02		; Z80 feature set
+		.byte 0			; Base 0x0000
 		.byte 0			; No hints
 
-		.word __data-0x100	; code
+		.word __data		; code + literal (0 based)
 		.word __data_size	; data
 		.word __bss_size	; bss size
 
@@ -21,6 +21,54 @@ __text:
 		.word __sighandler		; signal handler vector
 
 start2:
+;
+;	On entry we are page aligned and de is our base
+;
+;	s__DATA is the BSS base computed by the compiler. It will get
+;	modified by the relocatable binary maker in the header but not
+;	in the code. Thus this is actually a pointer to our relocation
+;	bytestream.
+;
+		push de			; base for relocation in DE
+		exx			; get it into DE and DE'
+		pop de			;
+		ld hl,__bss		; will be pre-reloc value (0 based)
+		add hl,de		; hl is now the relocations
+					; de is the code base
+		ld b,#0			; on the code base bits
+		ex de,hl		; de is relocations as loop swaps
+relnext:
+		; Read each relocation byte and zero it (because it's really
+		; stolen BSS so should start zero)
+		ex de,hl
+		ld a,(hl)
+		ld (hl),0
+		inc hl
+		ex de,hl
+		; 0 means done, 255 means skip 254, 254 or less means
+		; skip that many and relocate  (runs are 255,255,....)
+		or a		; 0 ?
+		jr z, relocdone
+		ld c,a
+		inc a		; 255 ?
+		jr z, relocskip
+		add hl,bc
+		ld a,(hl)
+		exx
+		add a,d
+		exx
+		ld (hl),a
+		jr relnext
+relocskip:	add hl,bc
+		dec hl
+		jr relnext
+relocdone:
+		ld hl,__bss		; Where our BSS should start
+		ld de,__bss_size	; The size
+		add hl,de
+		push hl
+		call _brk		; Break back to the right place
+		pop af
 		call ___stdio_init_vars
 		ld hl,4
 		add hl,sp
