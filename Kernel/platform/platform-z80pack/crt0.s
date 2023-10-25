493fa7a718e35f9ff4410a3ff4f39964bf2bfad8
Alan Cox <etchedpixels@gmail.com>
2023-10-25 22:46:11 +0100

    z80pack: move to new compiler

--- Kernel/platform/platform-z80pack/crt0.s
+++ Kernel/platform/platform-z80pack/crt0.s
@@ -1,79 +1,45 @@
-; 2013-12-18 William R Sowerbutts
-
-        .module crt0
-
-        ; Ordering of segments for the linker.
-        ; WRS: Note we list all our segments here, even though
-        ; we don't use them all, because their ordering is set
-        ; when they are first seen.
-        .area _CODE
-        .area _CODE2
-        .area _CONST
-        .area _INITIALIZED
-        .area _DATA
-        .area _BSEG
-        .area _BSS
-        .area _HEAP
-        ; note that areas below here may be overwritten by the heap at runtime, so
-        ; put initialisation stuff in here
-        .area _INITIALIZER
-        .area _GSINIT
-        .area _GSFINAL
-	.area _DISCARD
-        .area _COMMONMEM
-        .area _COMMONDATA
-
-        ; imported symbols
-        .globl _fuzix_main
-        .globl init_early
-        .globl init_hardware
-        .globl s__INITIALIZER
-        .globl s__COMMONMEM
-        .globl l__COMMONMEM
-        .globl s__COMMONDATA
-        .globl l__COMMONDATA
-        .globl s__DISCARD
-        .globl l__DISCARD
-        .globl s__DATA
-        .globl l__DATA
-        .globl kstack_top
-
+# 0 "crt0.S"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 0 "<command-line>" 2
+# 1 "crt0.S"
         ; startup code
-        .area _CODE
+ .code
+
 init:
         di
-        ld sp, #kstack_top
+        ld sp, kstack_top
 
         ; Configure memory map
         call init_early
 
-	; move the common memory where it belongs    
-	ld hl, #s__DATA
-	ld de, #s__COMMONMEM
-	ld bc, #l__COMMONMEM
-	ldir
-	ld de, #s__COMMONDATA
-	ld bc, #l__COMMONDATA
-	ldir
-	; and the discard
-	ld de, #s__DISCARD
-	ld bc, #l__DISCARD
-	ldir
-	; then zero the data area
-	ld hl, #s__DATA
-	ld de, #s__DATA + 1
-	ld bc, #l__DATA - 1
-	ld (hl), #0
-	ldir
+ ; move the common memory where it belongs
+ ld hl, __bss
+ ld de, __common
+ ld bc, __common_size
+ ldir
+ ld de, __commondata
+ ld bc, __commondata_size
+ ldir
+ ; and the discard
+ ld de, __discard
+ ld bc, __discard_size
+ ldir
+ ; then zero the data area
+ ld hl, __bss
+ ld de, __bss + 1
+ ld bc, __bss_size - 1
+ ld (hl), 0
+ ldir
 
         ; Hardware setup
         call init_hardware
 
         ; Call the C main routine
         call _fuzix_main
-    
+
         ; main shouldn't return, but if it does...
         di
-stop:   halt
+stop: halt
         jr stop
-
