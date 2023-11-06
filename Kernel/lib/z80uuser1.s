9f8b722af25271897bba677f44e79b0a89f394e5
Alan Cox <etchedpixels@gmail.com>
2023-11-06 16:41:36 +0000

    z80uuser: fix uzero()

--- Kernel/lib/z80uuser1.s
+++ Kernel/lib/z80uuser1.s
@@ -51,7 +51,6 @@ __uputc:
 	ld l,a
 	call map_proc_always
 	ld (hl), e
-uputc_out:
 	jp map_kernel			; map the kernel back below common
 
 __uputw:
@@ -134,7 +133,7 @@ __uzero:
 	ld b,(hl)
 	ld a, b	; check for 0 copy
 	or c
-	ret z
+	jr z, popout
 	call map_proc_always
 	ld l,e
 	ld h,d
@@ -142,7 +141,9 @@ __uzero:
 	dec bc
 	ld a, b
 	or c
-	jp z, uputc_out
+	jr z, popout
 	inc de
 	ldir
-	jp uputc_out
+popout:
+	pop bc
+	jp map_kernel
