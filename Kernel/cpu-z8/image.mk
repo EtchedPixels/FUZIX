01b1e0698f54a9fc0fcf354821c67651c421f009
Alan Cox <etchedpixels@gmail.com>
2024-02-15 16:07:59 +0000

    z8: Further work
    Add split id support for the kernel image. Userspace will need a load
    of things dealing with including binary format and also lower level changes
    so that valaddr can do the right thing for signals (where it's a code pointer)
    and disk I/O can be told to load into code or data space.
    
    (and then there's swap to worry about too)

--- Kernel/cpu-z8/image.mk
+++ Kernel/cpu-z8/image.mk
@@ -1,7 +1,5 @@
-tools/visualize6800: tools/visualize6800.c
+tools/visualize_splitid: tools/visualize_splitid.c
 
-tools/pack85: tools/pack85.c
-
-fuzix.bin: target $(OBJS) tools/pack85 tools/visualize6800
+fuzix.bin: target $(OBJS) tools/visualize_splitid
 	+$(MAKE) -C platform/platform-$(TARGET) image
-	tools/visualize6800 <fuzix.map
+	tools/visualize_splitid <fuzix.map
--- Kernel/platform/platform-rcbus-z8/discard.c
+++ Kernel/platform/platform-rcbus-z8/discard.c
@@ -3,11 +3,17 @@
 /* Add memory */
 void pagemap_init(void)
 {
-    register unsigned i;
-    /* Simple banking to get us going */
-    for (i = 35; i <= 63; i+=3)
-        pagemap_add(i);
-    /* TODO: do we need 35 last ? */
+	/* Low 0 is kernel map, high 0 unused except at boot */
+	/* 0xC3 is the kernel data */
+	/* pagemap_add(0xC3); */
+	pagemap_add(0x33);
+	pagemap_add(0xF3);
+	pagemap_add(0x0F);
+	pagemap_add(0xCF);
+	pagemap_add(0x3F);
+	/* This one weill be picked up by init and must be first to
+	   match crt0.S */
+	pagemap_add(0xFF);
 }
 
 void map_init(void)
