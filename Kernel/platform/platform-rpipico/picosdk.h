557c6fb0cec4ad8bdeb8ead05e6be8702d6088bc
Yaroslav Veremenko <yaroslav@veremenko.info>
2024-04-03 16:29:40 -0600

    Save about 6K of RAM

--- Kernel/platform/platform-rpipico/picosdk.h
+++ Kernel/platform/platform-rpipico/picosdk.h
@@ -1,7 +1,9 @@
 #define MANGLED 0
 #include "mangle.h"
 
-#include "pico/stdlib.h"
+#include <pico/platform.h>
+#include <hardware/gpio.h>
+#include <hardware/regs/addressmap.h>
 
 #define MANGLED 1
 #include "mangle.h"
