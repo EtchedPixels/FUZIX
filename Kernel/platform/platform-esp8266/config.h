ccde82b9728890c5b081072ad22d3752c7f0f385
Pierre Alain <65669679+palainp@users.noreply.github.com>
2024-11-24 21:25:23 +0100

    do not default boot on flash

--- Kernel/platform/platform-esp8266/config.h
+++ Kernel/platform/platform-esp8266/config.h
@@ -92,7 +92,7 @@ extern uint8_t _code_top[];
 /* We need a tidier way to do this from the loader */
 #define CMDLINE	NULL	  /* Location of root dev name */
 
-#define BOOTDEVICE 0x0000 /* hda */
+//#define BOOTDEVICE 0x0000 /* hda */
 //#define BOOTDEVICE 0x0011 /* hdb1 */
 #ifndef BOOTDEVICE
 #define BOOTDEVICE 0x0011 /* hdb1 */
