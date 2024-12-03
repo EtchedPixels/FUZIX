89adef0068d138abf8c10a37a9f5c15a9244bcc2
Alan Cox <etchedpixels@gmail.com>
2024-12-03 17:41:46 +0000

    ar: clean up // usage

--- Applications/ar/strmode.c
+++ Applications/ar/strmode.c
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-// static char sccsid[] = "@(#)strmode.c	8.1 (Berkeley) 6/4/93";
+/* static char sccsid[] = "@(#)strmode.c	8.1 (Berkeley) 6/4/93"; */
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
