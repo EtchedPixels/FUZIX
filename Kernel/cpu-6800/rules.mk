8ddc8ffa85b8591b889e7b074ff6e525f73b15bb
Alan Cox <etchedpixels@gmail.com>
2024-12-04 12:21:38 +0000

    6800: add kernel rules and mods to use fcc

--- Kernel/cpu-6800/rules.mk
+++ Kernel/cpu-6800/rules.mk
@@ -1,7 +1,7 @@
-export CROSS_AS=fcc -x -m6800 -c
+export CROSS_AS=fcc -m6800 -c
 export CROSS_LD=ld6800
 export CROSS_CC=fcc
-export CROSS_CCOPTS= -m6800 -X -c -I$(ROOT_DIR)/cpu-6800 -I$(ROOT_DIR)/platform/platform-$(TARGET) -I$(ROOT_DIR)/include
+export CROSS_CCOPTS= -m6800 -Os -X -c -I$(ROOT_DIR)/cpu-6800 -I$(ROOT_DIR)/platform/platform-$(TARGET) -I$(ROOT_DIR)/include
 export CROSS_CC_SEG1=
 export CROSS_CC_SEG2=
 export CROSS_CC_SEG3=
