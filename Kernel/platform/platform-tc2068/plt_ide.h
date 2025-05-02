86845cb1488940e760d9f233f011003973950868
Alan Cox <etchedpixels@gmail.com>
2025-05-03 00:08:42 +0100

    tc2068: add needed plt_ide.h

---
+++ Kernel/platform/platform-tc2068/plt_ide.h
@@ -0,0 +1,11 @@
+__sfr __at 0xA3 data;
+__sfr __at 0xA7 error;
+__sfr __at 0xA8 count;
+__sfr __at 0xAF sec;
+__sfr __at 0xB3 cyll;
+__sfr __at 0xB7 cylh;
+__sfr __at 0xBB devh;
+__sfr __at 0xBF cmd;
+__sfr __at 0xBF status;
+
+#define IDE_REG_DATA	0x00A3
