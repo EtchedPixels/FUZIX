630ca9501dde7f115549b96bf71f72a1797a18f2
Alan Cox <etchedpixels@gmail.com>
2025-05-03 00:08:11 +0100

    sam: add needed devatom.h

---
+++ Kernel/platform/platform-sam/devatom.h
@@ -0,0 +1,12 @@
+#ifndef __DEVATOM_H
+#define __DEVATOM_H
+
+extern uint8_t atom_type;
+
+#define ATOM_NONE	0
+#define ATOM_16BIT	1
+#define ATOM_LITE	2
+
+extern uint8_t atom_probe(void);
+
+#endif
