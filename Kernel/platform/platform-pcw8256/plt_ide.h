8cb9e7535ef441353575e692aacb7cba70776eda
Alan Cox <etchedpixels@gmail.com>
2025-05-03 00:07:39 +0100

    pcw8256: add needed plt_ide.h

---
+++ Kernel/platform/platform-pcw8256/plt_ide.h
@@ -0,0 +1,11 @@
+__sfr __at 0xC8 data;
+__sfr __at 0xC9 error;
+__sfr __at 0xCA count;
+__sfr __at 0xCB sec;
+__sfr __at 0xCC cyll;
+__sfr __at 0xCD cylh;
+__sfr __at 0xCE devh;
+__sfr __at 0xCF cmd;
+__sfr __at 0xCF status;
+
+#define IDE_REG_DATA	0x00C8
